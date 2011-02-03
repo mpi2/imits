12a448742e7c102c51d566c8d50778d7a26722f4
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-03 16:13:35 +0000

    First commit - rails project setting up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kermits2::Application.config.secret_token = 'b208a870d61184d29132c78ac25d5236d7e1ae9d4dc549643940a0b6d1f6103c17a4a14c2314dcccc44c7ab450b49e8503942b0f750d6bfed5d1d936b5a32160'

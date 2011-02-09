7652a12faccc16c05f603659289b786b0f5af916
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-09 16:43:17 +0000

    EmiAttempt#emi_event, EmiEvent#emi_clone, EmiAttempt#emi_clone

--- test/unit/emi_attempt_test.rb
+++ test/unit/emi_attempt_test.rb
@@ -4,4 +4,12 @@ class EmiAttemptTest < ActiveSupport::TestCase
   should 'use table "emi_attempt"' do
     assert_equal 'emi_attempt', EmiAttempt.table_name
   end
+
+  should 'belong to emi_event' do
+    assert_equal emi_event('EPD0127_4_E01'), emi_attempt('EPD0127_4_E01__1').emi_event
+  end
+
+  should 'belong to emi clone t hrough emi event' do
+    assert_equal emi_clone('EPD0127_4_E01'), emi_attempt('EPD0127_4_E01__1').emi_clone
+  end
 end

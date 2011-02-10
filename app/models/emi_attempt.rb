782abdc2c76b6f19faa0ead2c4edd42d9d607b60
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-10 10:53:57 +0000

    EmiAttempt#emi_event association no longer includes emi_clone lazily
    Postgres ignores it and oracle breaks on it: fix it later

--- app/models/emi_attempt.rb
+++ app/models/emi_attempt.rb
@@ -1,8 +1,10 @@
 class EmiAttempt < ActiveRecord::Base
   set_table_name 'emi_attempt'
 
+  # The include does not work in postgres, which seems to ignore it, and breaks
+  # on oracle
   belongs_to :emi_event, :class_name => 'EmiEvent',
-          :foreign_key => :event_id, :include => [:emi_clone]
+          :foreign_key => :event_id # , :include => [:emi_clone]
 
   def emi_clone; emi_event.emi_clone; end
 

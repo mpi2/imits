b84e3de0bfd33dc43c91fee81ade562fd00a0084
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-10 10:15:19 +0000

    Add scope EmiAttempt::by_clone_name

--- test/unit/emi_attempt_test.rb
+++ test/unit/emi_attempt_test.rb
@@ -9,7 +9,16 @@ class EmiAttemptTest < ActiveSupport::TestCase
     assert_equal emi_event('EPD0127_4_E01'), emi_attempt('EPD0127_4_E01__1').emi_event
   end
 
-  should 'belong to emi clone t hrough emi event' do
+  should 'belong to emi clone through emi event' do
     assert_equal emi_clone('EPD0127_4_E01'), emi_attempt('EPD0127_4_E01__1').emi_clone
   end
+
+  should '::by_clone_name works' do
+    results = EmiAttempt.by_clone_name('EPD0127_4_E01')
+    assert_equal 3, results.size
+    assert results.include? emi_attempt('EPD0127_4_E01__1')
+    assert results.include? emi_attempt('EPD0127_4_E01__2')
+    assert results.include? emi_attempt('EPD0127_4_E01__3')
+  end
+
 end

b8f7ee4798788eebcb8311305595419f64b57430
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-10 12:42:31 +0000

    Search for and display some emi attempt info for a clone name

--- test/integration/search_for_emi_attempts_by_clone_name_test.rb
+++ test/integration/search_for_emi_attempts_by_clone_name_test.rb
@@ -7,8 +7,13 @@ class SearchForEmiAttemptsByCloneNameTest < ActionDispatch::IntegrationTest
       fill_in 'clone_names', :with => 'EPD0127_4_E01'
       click_button 'Search'
       assert_match %r{http://www\.example\.com/emi_clones\?clone_names=EPD0127_4_E01$}, current_url
-      assert page.has_css? 'td', :text => 'EPD0127_4_E01'
-      save_and_open_page
+
+      assert page.has_css? 'tr:nth-child(2) td', :text => 'EPD0127_4_E01'
+      assert page.has_css? 'tr:nth-child(2) td', :text => 'Trafd1'
+      assert page.has_css? 'tr:nth-child(2) td', :text => 'Trafd1<sup>tm1a(EUCOMM)Wtsi</sup>'
+      assert page.has_css? 'tr:nth-child(2) td', :text => '29 July 2008'
+      assert page.has_css? 'tr:nth-child(2) td', :text => '30 July 2008'
+      assert page.has_css? 'tr:nth-child(2) td', :text => 'MBSS'
     end
   end
 end

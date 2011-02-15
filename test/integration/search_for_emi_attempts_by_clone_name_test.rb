175e8cd89dc03dcded1c8141e0c13f5e0ad67070
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-15 17:18:11 +0000

    Display some more data in emi attempt search results

--- test/integration/search_for_emi_attempts_by_clone_name_test.rb
+++ test/integration/search_for_emi_attempts_by_clone_name_test.rb
@@ -2,21 +2,32 @@ require 'test_helper'
 
 class SearchForEmiAttemptsByCloneNameTest < ActionDispatch::IntegrationTest
 
-  context 'searching for emi attempts by clone name' do
-    should 'work with a single clone name' do
-      visit '/'
-      fill_in 'clone_names', :with => 'EPD0127_4_E01'
-      click_button 'Search'
-      sleep 3
-      assert_match %r{^http://[^/]+/emi_attempts\?clone_names=EPD0127_4_E01$}, current_url
+  def selector_for_table_cell(table_row)
+    ".x-grid3-body .x-grid3-row:nth-child(#{table_row}) .x-grid3-cell-inner"
+  end
 
-      #y find('.x-grid3-body .x-grid3-row:nth-child(1)').text
-      assert page.has_css? '.x-grid3-body .x-grid3-row:nth-child(1) .x-grid3-cell-inner', :text => 'EPD0127_4_E01'
-      assert page.has_css? 'tr:nth-child(2) td', :text => 'Trafd1'
-      assert page.has_css? 'tr:nth-child(2) td', :text => 'Trafd1<sup>tm1a(EUCOMM)Wtsi</sup>'
-      assert page.has_css? 'tr:nth-child(2) td', :text => '29 July 2008'
-      assert page.has_css? 'tr:nth-child(2) td', :text => '30 July 2008'
-      assert page.has_css? 'tr:nth-child(2) td', :text => 'MBSS'
+  context 'searching for emi attempts by clone name' do
+    context 'with a single clone' do
+      setup do
+        visit '/'
+        fill_in 'clone_names', :with => 'EPD0127_4_E01'
+        click_button 'Search'
+        sleep 3
+        assert_match %r{^http://[^/]+/emi_attempts\?clone_names=EPD0127_4_E01$}, current_url
+      end
+
+      should 'show all data for that clone' do
+        assert page.has_css? selector_for_table_cell(1), :text => 'EPD0127_4_E01'
+        assert page.has_css? selector_for_table_cell(1), :text => 'Trafd1'
+        assert page.has_css? selector_for_table_cell(1), :text => 'Trafd1tm1a(EUCOMM)Wtsi'
+        assert page.has_css? selector_for_table_cell(1), :text => '29 July 2008'
+        assert page.has_css? selector_for_table_cell(1), :text => '30 July 2008'
+        assert page.has_css? selector_for_table_cell(1), :text => 'MBSS'
+      end
+
+      should 'not show data for other clones' do
+        assert(!page.has_css?('.x-grid3-cell-inner', :text => 'EPD0343_1_H06'))
+      end
     end
 
     should 'work with a multiple clone names' do
@@ -24,8 +35,8 @@ class SearchForEmiAttemptsByCloneNameTest < ActionDispatch::IntegrationTest
       fill_in 'clone_names', :with => "EPD0127_4_E01\nEPD0343_1_H06"
       click_button 'Search'
 
-      assert page.has_css? 'tr:nth-child(2) td', :text => 'EPD0127_4_E01'
-      assert page.has_css? 'tr:nth-child(5) td', :text => 'EPD0343_1_H06'
+      assert page.has_css? selector_for_table_cell(2), :text => 'EPD0127_4_E01'
+      assert page.has_css? selector_for_table_cell(5), :text => 'EPD0343_1_H06'
     end
 
     should 'work if whitespace around clone names' do

54d936294450b155f78013c08ce39aef4ff51424
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-17 17:33:44 +0000

    Display distribution centre in MI Attempt results

--- app/helpers/emi_attempts_helper.rb
+++ app/helpers/emi_attempts_helper.rb
@@ -1,23 +1,32 @@
 module EmiAttemptsHelper
 
-  def emi_attempts_table(clone_names)
-    netzke(:emi_attempts,
-      :class_name => "Basepack::GridPanel", :model => "EmiAttempt",
-      :columns => [
-        {:name => :clone_name, :header => 'Clone'},
-        {:name => :gene_symbol, :header => 'Gene'},
-        {:name => :allele_name, :header => 'Allele'},
-        {:name => :formatted_proposed_mi_date, :header => 'Proposed MI Date'},
-        {:name => :formatted_actual_mi_date, :header => 'Actual MI Date'},
-        {:name => :colony_name, :header => 'Colony'},
-      ],
-      :prohibit_create => true,
-      :prohibit_update => true,
-      :prohibit_delete => true,
-      :enable_edit_in_form => false,
-      :enable_extended_search => false,
-      :scope => [:by_clone_names, *clone_names ]
-    )
+  class Grid < Netzke::Basepack::GridPanel
+    def configuration
+      config_up_to_now = super
+      clone_names = config_up_to_now.fetch(:clone_names)
+      config_up_to_now.merge(
+        :model => 'EmiAttempt',
+        :columns => [
+          :clone_name,
+          {:name => :gene_symbol, :header => 'Gene'},
+          {:name => :allele_name, :header => 'Allele'},
+          {:name => :formatted_proposed_mi_date, :header => 'Proposed MI Date'},
+          {:name => :formatted_actual_mi_date, :header => 'Actual MI Date'},
+          {:name => :colony_name, :header => 'Colony'},
+          {:header => 'Distribution Centre', :getter => lambda {|mi_attempt| mi_attempt.distribution_centre.name} }
+        ],
+        :prohibit_create => true,
+        :prohibit_update => true,
+        :prohibit_delete => true,
+        :enable_edit_in_form => false,
+        :enable_extended_search => false,
+        :scope => [:by_clone_names, *clone_names ]
+      )
+    end
+  end
 
+  def emi_attempts_table(clone_names)
+    netzke(:micro_injection_attempts, :class_name => "EmiAttemptsHelper::Grid",
+      :clone_names => clone_names)
   end
 end

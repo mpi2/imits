f2cac0b893e4dc77035f38c48972338cec7efd4c
Asfand Qazi <aq2@sanger.ac.uk>
2011-02-25 08:56:24 +0000

    Leave MI attempt editing integration tests as pending
    Since we cannot test them without jumping through hoops

--- test/integration/edit_mi_attempts_test.rb
+++ test/integration/edit_mi_attempts_test.rb
@@ -6,16 +6,9 @@ class EditMiAttemptsTest < ActionDispatch::IntegrationTest
   context 'Edit MI Attempt' do
 
     context 'Distribution Centre' do
-      should 'get list of distribution centres from all centres in the DB' do
-        visit '/emi_attempts?clone_names=EPD0343_1_H06'
+      should 'get list of distribution centres from all centres in the DB'
 
-        assert_equal 'WTSI', find('.x-grid3-body .x-grid3-row:nth-child(1) .x-grid3-col-distribution_centre_name.x-grid3-cell-inner').text
-        flunk 'Selenium/Capybara do not support double click testing'
-      end
-
-      should 'work' do
-        flunk 'Selenium/Capybara do not support double click testing'
-      end
+      should 'work'
     end
   end
 

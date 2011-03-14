c01727bdcbdd23c2d780ce5a7818da71c20bca0d
Asfand Qazi <aq2@sanger.ac.uk>
2011-03-14 16:17:45 +0000

    Make flash notices pretty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def flash_message(message_type)
     if ! flash[message_type].blank?
       content_tag(:div, :class => ['flash', message_type]) do
-        content_tag :p, flash[message_type]
+        content_tag(:p, message_type.capitalize, :class => 'header') +
+                content_tag(:p, flash[message_type], :class => 'body')
       end
     end
   end

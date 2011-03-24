7fc64517c2eac9be667bcf249ae8a20a9e4ad730
Asfand Qazi <aq2@sanger.ac.uk>
2011-03-24 13:07:28 +0000

    Loading mask while MI attempt columns are reticulated

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,17 +14,30 @@ function setInitialFocus() {
 function toggleMiAttemptsSwitchViewButton(button, pressed) {
     if(!pressed) {return;}
 
-    var grid = Netzke.page.microInjectionAttemptsWidget.grid;
-    columnModel = grid.getColumnModel();
+    var mask = new Ext.LoadMask(Ext.get('micro_injection_attempts_widget'),
+        {msg: 'Reticulating columns...', removeMask: true});
 
-    var columnsToShow = MI_ATTEMPTS_VIEW_CONFIG[button.viewName];
+    function intensiveOperation() {
+        var grid = Netzke.page.microInjectionAttemptsWidget.grid;
+        columnModel = grid.getColumnModel();
 
-    for(var idx = 0; idx < columnModel.getColumnCount(); ++idx) {
-        var columnId = columnModel.getColumnId(idx);
-        if(columnsToShow.indexOf(columnId) == -1) {
-            columnModel.setHidden(idx, true);
-        } else {
-            columnModel.setHidden(idx, false);
+        var columnsToShow = MI_ATTEMPTS_VIEW_CONFIG[button.viewName];
+
+        for(var idx = 0; idx < columnModel.getColumnCount(); ++idx) {
+            var columnId = columnModel.getColumnId(idx);
+            if(columnsToShow.indexOf(columnId) == -1) {
+                columnModel.setHidden(idx, true);
+            } else {
+                columnModel.setHidden(idx, false);
+            }
         }
+
+        mask.hide();
+        Ext.getBody().removeClass('wait');
     }
+
+    Ext.getBody().addClass('wait');
+    mask.show();
+
+    setTimeout(intensiveOperation, 500);
 }

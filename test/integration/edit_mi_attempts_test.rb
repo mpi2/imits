7314c5faf62b3aa729647f33b0bfc074a3f9a278
Asfand Qazi <aq2@sanger.ac.uk>
2011-03-31 10:40:30 +0100

    Do a bit more testing for auditing

--- test/integration/edit_mi_attempts_test.rb
+++ test/integration/edit_mi_attempts_test.rb
@@ -3,31 +3,49 @@
 require 'test_helper'
 
 class EditMiAttemptsTest < ActionDispatch::IntegrationTest
-  should 'audit MiAttempt on changes' do
-    mi_attempt = emi_attempt('EPD0343_1_H06__1')
-    assert_equal 'vvi', mi_attempt.edited_by
+
+  def default_mi_attempt
+    @default_mi_attempt ||= emi_attempt('EPD0343_1_H06__1')
+  end
+
+  def assert_mi_attempt_was_audited
+    assert_equal 'vvi', default_mi_attempt.edited_by
 
     login
-    visit '/mi_attempts'
-    fill_in 'search_terms', :with => 'EPD0343_1_H06'
+    visit '/mi_attempts?search_terms=EPD0343_1_H06'
     click_button 'Search'
 
-    find('.x-grid3-col-emma_status').click # The cell containing EMMA status
-    find('.x-editor .x-form-trigger').click # The combo box down arrow
-    find('.x-combo-list-item:nth-child(4)').click # 'Unsuitable for EMMA - STICKY'
+    yield
 
     click_button 'Save Changes'
 
     sleep 6
 
-    mi_attempt.reload
-    assert_in_delta Time.now, mi_attempt.edit_date, 60.seconds
-    assert_equal 'zz99', mi_attempt.edited_by
+    default_mi_attempt.reload
+    assert_in_delta Time.now, default_mi_attempt.edit_date, 60.seconds
+    assert_equal 'zz99', default_mi_attempt.edited_by
   end
 
-  should 'audit EmiEvent on changes' do
-    mi_attempt = emi_attempt('EPD0343_1_H06__1')
-    assert_equal 'vvi', mi_attempt.emi_event.edited_by
+  should 'audit MiAttempt when emma status changes' do
+    assert_mi_attempt_was_audited do
+      find('.x-grid3-col-emma_status').click # The cell containing EMMA status
+      find('.x-editor .x-form-trigger').click # The combo box down arrow
+      find('.x-combo-list-item:nth-child(4)').click # 'Unsuitable for EMMA - STICKY'
+    end
+    assert_equal :unsuitable_sticky, default_mi_attempt.emma_status
+  end
+
+  should 'audit MiAttempt when simple numeric field changes' do
+    assert_mi_attempt_was_audited do
+      find('.x-grid3-col-number_born').click # The cell containing 'Total Pups Born'
+      find('.x-editor input.x-form-text[@type=text]').set('12')
+      click_button 'Save Changes' # Make text-box lose focus.. will not submit
+    end
+    assert_equal 12, default_mi_attempt.number_born
+  end
+
+  should 'audit EmiEvent when distribution center changes' do
+    assert_equal 'vvi', default_mi_attempt.emi_event.edited_by
 
     login
     visit '/mi_attempts'
@@ -42,8 +60,9 @@ class EditMiAttemptsTest < ActionDispatch::IntegrationTest
 
     sleep 6
 
-    mi_attempt.reload
-    assert_in_delta Time.now, mi_attempt.emi_event.edit_date, 60.seconds
-    assert_equal 'zz99', mi_attempt.emi_event.edited_by
+    default_mi_attempt.reload
+    assert_in_delta Time.now, default_mi_attempt.emi_event.edit_date, 60.seconds
+    assert_equal 'zz99', default_mi_attempt.emi_event.edited_by
   end
+
 end

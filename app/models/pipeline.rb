a7b37a7e5b6ccd09b943e4d68ece01e06c0f2749
Asfand Qazi <aq2@sanger.ac.uk>
2011-04-19 13:43:51 +0100

    Whitespace

--- app/models/pipeline.rb
+++ app/models/pipeline.rb
@@ -1,4 +1,4 @@
 class Pipeline < ActiveRecord::Base
-  validates :name, :presence => true, :uniqueness => true
+  validates :name       , :presence => true, :uniqueness => true
   validates :description, :presence => true
 end

fa2e98d6eebba7cab5002b2db6c39e5731f4ac6f
Asfand Qazi <aq2@sanger.ac.uk>
2011-04-20 16:25:08 +0100

    Add short-cuts to both statuses

--- app/models/mi_attempt_status.rb
+++ app/models/mi_attempt_status.rb
@@ -1,3 +1,6 @@
 class MiAttemptStatus < ActiveRecord::Base
-  validates :description, :presence => true
+  IN_PROGRESS = self.find_by_description!('In progress').freeze
+  GOOD        = self.find_by_description!('Good').freeze
+
+  validates :description, :presence => true, :uniqueness => true
 end
--- test/unit/mi_attempt_status_test.rb
+++ test/unit/mi_attempt_status_test.rb
@@ -2,11 +2,22 @@ require 'test_helper'
 
 class MiAttemptStatusTest < ActiveSupport::TestCase
   context 'MiAttemptStatus' do
-    setup do
-      Factory.create :mi_attempt_status
-    end
 
     should have_db_column(:description).with_options(:null => false)
     should validate_presence_of :description
+    should validate_uniqueness_of :description
+
+    context 'easy-access constants' do
+      should 'include IN_PROGRESS' do
+        assert_equal 'In progress', MiAttemptStatus::IN_PROGRESS.description
+        assert_true MiAttemptStatus::IN_PROGRESS.frozen?
+      end
+
+      should 'include GOOD' do
+        assert_equal 'Good', MiAttemptStatus::GOOD.description
+        assert_true MiAttemptStatus::GOOD.frozen?
+      end
+    end
+
   end
 end

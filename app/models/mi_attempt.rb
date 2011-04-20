c6fdadbfa628f7270dac7ba1b7a8deaed3ef8858
Asfand Qazi <aq2@sanger.ac.uk>
2011-04-20 14:07:04 +0100

    Centre associations added to MiAttempt

--- app/models/mi_attempt.rb
+++ app/models/mi_attempt.rb
@@ -1,4 +1,7 @@
 class MiAttempt < ActiveRecord::Base
   belongs_to :clone
   validates :clone, :presence => true
+
+  belongs_to :centre
+  belongs_to :distribution_centre, :class_name => 'Centre'
 end
--- test/unit/mi_attempt_test.rb
+++ test/unit/mi_attempt_test.rb
@@ -9,5 +9,11 @@ class MiAttemptTest < ActiveSupport::TestCase
     should have_db_column(:clone_id).with_options(:null => false)
     should belong_to :clone
     should validate_presence_of :clone
+
+    should have_db_column(:centre_id)
+    should belong_to :centre
+
+    should have_db_column(:distribution_centre_id)
+    should belong_to :distribution_centre
   end
 end

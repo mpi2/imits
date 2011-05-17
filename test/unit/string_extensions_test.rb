0a7dcf1f2f24d5e9b896b7515a6a2ec8ea9db91b
Asfand Qazi <aq2@sanger.ac.uk>
2011-05-17 16:20:53 +0100

    Wrap should tests in context

--- test/unit/string_extensions_test.rb
+++ test/unit/string_extensions_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class StringExtensionsTest < ActiveSupport::TestCase
-  should '#to_json_variable' do
-    assert_equal ActiveSupport::JSON::Variable.new('foo'), 'foo'.to_json_variable
+  context 'String' do
+    should '#to_json_variable' do
+      assert_equal ActiveSupport::JSON::Variable.new('foo'), 'foo'.to_json_variable
+    end
   end
 end

55a4ee2fb3664b033c92a1eecaacee8bc86bebef
Asfand Qazi <aq2@sanger.ac.uk>
2011-05-18 15:50:59 +0100

    Start migrating; migrate a few mi attempt fields and centres

--- app/models/kermits2/migration.rb
+++ app/models/kermits2/migration.rb
@@ -1,7 +1,44 @@
 class Kermits2::Migration
-  def self.run
+  def self.run(params)
+    params.symbolize_keys!
+
+    migrate_centres
+
+    migrate_mi_attempts_and_their_clones(params)
+  end
+
+  private
+
+  def self.migrate_centres
     Old::Centre.all.each do |old_centre|
       Centre.create!(:name => old_centre.name)
+    end
+  end
+
+  def self.migrate_mi_attempts_and_their_clones(params)
+    mi_attempt_ids = params[:mi_attempt_ids]
+
+    mi_attempt_ids.each do |mi_attempt_id|
+      old_mi_attempt = Old::MiAttempt.find(mi_attempt_id)
+
+      pipeline = Pipeline.find(:first)
+
+      clone = Clone.create!(:marker_symbol => 'tmp',
+        :allele_name_superscript => 'tm1(TMP)tmp',
+        :clone_name => "RAND_#{rand 999999999999}",
+        :pipeline => pipeline)
+
+      mi_attempt = MiAttempt.new(
+        :clone => clone,
+        :production_centre => Centre.first,
+
+        # Transfer details
+        :total_blasts_injected => old_mi_attempt.num_blasts,
+        :total_transferred => old_mi_attempt.num_transferred,
+        :number_surrogates_receiving => old_mi_attempt.num_recipients,
+      )
+
+      mi_attempt.save!
     end
   end
 end
--- test/unit/kermits2/migration_test.rb
+++ test/unit/kermits2/migration_test.rb
@@ -1,20 +1,61 @@
+# encoding: utf-8
+
 require 'test_helper'
 
 class Kermits2::MigrationTest < ActiveSupport::TestCase
 
   context 'Kermits2::Migration' do
+    setup do
+      Centre.destroy_all
+    end
+
     should 'work from the script' do
       flunk
     end
 
     should 'migrate across centres' do
-      Centre.destroy_all
-      Kermits2::Migration.run
+      Kermits2::Migration.run(:mi_attempt_ids => [])
 
       centre_names = Centre.all.collect(&:name)
       assert_include centre_names, 'ICS'
       assert_include centre_names, 'WTSI'
       assert_include centre_names, 'Monterotondo'
+    end
+
+    should 'migrate across pipelines' do
+      flunk
+    end
+
+    context 'migrating an mi attempt' do
+      should 'migrate across the clone using data from marts if it does not already exist' do
+        old_mi_attempt = Old::MiAttempt.find(11029)
+        assert_not_nil old_mi_attempt
+
+        Kermits2::Migration.run(:mi_attempt_ids => [11029])
+        assert_equal 1, MiAttempt.count
+
+        mi_attempt = MiAttempt.first
+        clone = mi_attempt.clone
+
+        assert_equal 'EPD0127_4_E01', clone.clone_name
+      end
+
+      should 'migrate centres' do
+        flunk
+      end
+
+      should 'migrate numeric fields from transfer details' do
+        old_mi_attempt = Old::MiAttempt.find(5268)
+        assert_not_nil old_mi_attempt
+
+        Kermits2::Migration.run(:mi_attempt_ids => [5268])
+        assert_equal 1, MiAttempt.count
+
+        mi_attempt = MiAttempt.first
+        assert_equal 40, mi_attempt.total_blasts_injected
+        assert_equal 80, mi_attempt.total_transferred
+        assert_equal nil, mi_attempt.number_surrogates_receiving
+      end
     end
 
   end

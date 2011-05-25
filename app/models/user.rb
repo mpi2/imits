f8950f2d62dbcfebb2859eba82e9952a65c1fed6
Asfand Qazi <aq2@sanger.ac.uk>
2011-05-25 13:03:42 +0100

    Do not set remember_me in model any more

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,5 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation
-
-  before_save :remember_me_defaults_to_true
-
-  protected
-
-  def remember_me_defaults_to_true
-    self.remember_me = true
-  end
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,16 +2,15 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   context 'User' do
+
     subject { Factory.create :user }
 
     should 'have accessible attributes' do
       assert_include User.accessible_attributes, :email
       assert_include User.accessible_attributes, :password
       assert_include User.accessible_attributes, :password_confirmation
+      assert_include User.accessible_attributes, :remember_me
     end
 
-    should 'default remember_me to true' do
-      assert_equal true, subject.remember_me
-    end
   end
 end

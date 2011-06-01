21e10c4955ed151e43ed6698614d8b7f33fad6a7
Asfand Qazi <aq2@sanger.ac.uk>
2011-06-01 13:30:40 +0100

    Centre default scope orders by name

--- app/models/centre.rb
+++ app/models/centre.rb
@@ -1,5 +1,7 @@
 class Centre < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
+
+  default_scope :order => 'name ASC'
 end
 
 # == Schema Information
--- test/unit/centre_test.rb
+++ test/unit/centre_test.rb
@@ -2,13 +2,24 @@ require 'test_helper'
 
 class CentreTest < ActiveSupport::TestCase
   context 'Centre' do
-    setup do
-      Factory.create :centre
+    context '(misc. tests)' do
+      setup do
+        Factory.create :centre
+      end
+
+      should have_db_column(:name).of_type(:text).with_options(:null => false)
+      should have_db_index(:name).unique(true)
+      should validate_presence_of :name
+      should validate_uniqueness_of :name
     end
 
-    should have_db_column(:name).of_type(:text).with_options(:null => false)
-    should have_db_index(:name).unique(true)
-    should validate_presence_of :name
-    should validate_uniqueness_of :name
+    should 'order by name by default' do
+      Factory.create :centre, :name => 'ZZ2'
+      Factory.create :centre, :name => 'ZZ1'
+      Factory.create :centre, :name => 'AA9'
+      Factory.create :centre, :name => 'AA1'
+
+      assert_equal %w{AA1 AA9 ICS WTSI ZZ1 ZZ2}, Centre.all.map(&:name)
+    end
   end
 end

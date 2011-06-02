034fa000ba1961a4d60fa4b6e0e91dfa9761819c
Asfand Qazi <aq2@sanger.ac.uk>
2011-06-02 11:49:15 +0100

    Abandon Rails form - use ExtJS form unfortunately

--- test/integration/create_mi_attempts_test.rb
+++ test/integration/create_mi_attempts_test.rb
@@ -27,8 +27,67 @@ class CreateMiAttemptsTest < ActionDispatch::IntegrationTest
       mi_attempt = MiAttempt.first
       assert_not_nil mi_attempt
 
+
+      # Important fields
+      # TODO clone
+      assert_equal '2011-05-31', mi_attempt.mi_date.to_s
+      assert_equal MiAttemptStatus.micro_injection_in_progress, mi_attempt.mi_attempt_status
+      assert_nil mi_attempt.colony_name
       assert_equal 'WTSI', mi_attempt.distribution_centre.name
       assert_equal 'ICS', mi_attempt.production_centre.name
+
+      # Transfer details
+      assert_nil mi_attempt.blast_strain
+      assert_nil mi_attempt.total_blasts_injected
+      assert_nil mi_attempt.total_transferred
+      assert_nil mi_attempt.number_surrogates_receiving
+
+      # Litter details
+      assert_nil mi_attempt.total_pups_born
+      assert_nil mi_attempt.total_female_chimeras
+      assert_nil mi_attempt.total_male_chimeras
+      assert_equal 0, mi_attempt.total_chimeras
+      assert_nil mi_attempt.number_of_males_with_0_to_39_percent_chimerism
+      assert_nil mi_attempt.number_of_males_with_40_to_79_percent_chimerism
+      assert_nil mi_attempt.number_of_males_with_80_to_99_percent_chimerism
+      assert_nil mi_attempt.number_of_males_with_100_percent_chimerism
+
+      # Chimera mating details
+      assert_equal :unsuitable, mi_attempt.emma_status
+      assert_nil mi_attempt.test_cross_strain_id
+      assert_nil mi_attempt.colony_background_strain_id
+      assert_equal '2011-05-30', mi_attempt.date_chimeras_mated
+      assert_nil mi_attempt.number_of_chimera_matings_attempted
+      assert_nil mi_attempt.number_of_chimera_matings_successful
+      assert_nil mi_attempt.number_of_chimeras_with_glt_from_cct
+      assert_nil mi_attempt.number_of_chimeras_with_glt_from_genotyping
+      assert_nil mi_attempt.number_of_chimeras_with_0_to_9_percent_glt
+      assert_nil mi_attempt.number_of_chimeras_with_10_to_49_percent_glt
+      assert_nil mi_attempt.number_of_chimeras_with_50_to_99_percent_glt
+      assert_nil mi_attempt.number_of_chimeras_with_100_percent_glt
+      assert_nil mi_attempt.total_f1_mice_from_matings
+      assert_nil mi_attempt.number_of_cct_offspring
+      assert_nil mi_attempt.number_of_het_offspring
+      assert_nil mi_attempt.number_of_live_glt_offspring
+      assert_nil mi_attempt.mouse_allele_type
+
+      # QC details
+      assert_equal 'na', mi_attempt.qc_southern_blot.description
+      assert_equal 'na', mi_attempt.qc_five_prime_lr_pcr.description
+      assert_equal 'na', mi_attempt.qc_five_prime_cassette_integrity.description
+      assert_equal 'na', mi_attempt.qc_tv_backbone_assay.description
+      assert_equal 'na', mi_attempt.qc_neo_count_qpcr.description
+      assert_equal 'na', mi_attempt.qc_neo_sr_pcr.description
+      assert_equal 'na', mi_attempt.qc_loa_qpcr.description
+      assert_equal 'na', mi_attempt.qc_homozygous_loa_sr_pcr.description
+      assert_equal 'na', mi_attempt.qc_lacz_sr_pcr.description
+      assert_equal 'na', mi_attempt.qc_mutant_specific_sr_pcr.description
+      assert_equal 'na', mi_attempt.qc_loxp_confirmation.description
+      assert_equal 'na', mi_attempt.qc_three_prime_lr_pcr.description
+      assert_true  mi_attempt.should_export_to_mart
+      assert_true  mi_attempt.is_active
+      assert_false mi_attempt.is_released_from_genotyping
+
 
       # should 'enter numeric fields'
 

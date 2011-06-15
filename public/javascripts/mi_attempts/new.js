4414258d39bdc91ba06ddefe9578ef1f95d386b3
Asfand Qazi <aq2@sanger.ac.uk>
2011-06-15 17:39:39 +0100

    During MI creation, pass in clone_name for the selected clone

--- public/javascripts/mi_attempts/new.js
+++ public/javascripts/mi_attempts/new.js
@@ -1,107 +1,3 @@
-function oldOnMiAttemptsNew() {
-    var form = Ext.select('form.new.mi-attempt', true).first();
-    if(! form) {return;}
-    form.dom.onsubmit = function() {return false;};
-
-    var restOfForm = Ext.get('rest-of-form');
-    restOfForm.hide(false);
-
-    var genes = [];
-    Ext.each(Kermits2.propertyNames(window.allClonesPartitionedByMarkerSymbol), function(gene) {
-        if(gene == '') {
-            genes.push([gene, '[All]']);
-        } else {
-            genes.push([gene, gene]);
-        }
-    });
-
-    var geneCombo = new Ext.form.ComboBox({
-        forceSelection: true,
-        triggerAction: 'all',
-        mode: 'local',
-        lazyRender: true,
-        store: genes,
-        autoSelect: true,
-        id: 'gene-combo'
-    });
-
-    geneCombo.onSelectListener = function(combo, record) {
-        var gene = record.data[combo.valueField];
-        cloneCombo.loadDataForGene(gene);
-    }
-
-    geneCombo.addListener('select', geneCombo.onSelectListener);
-
-    var cloneCombo = new Ext.form.ComboBox({
-        hiddenName: 'mi_attempt[clone_id]',
-        hiddenId: 'mi_attempt_clone_id',
-        lazyRender: true,
-        forceSelection: true,
-        triggerAction: 'all',
-        mode: 'local',
-        store: new Ext.data.JsonStore({
-            root: 'rows',
-            fields: ['id', 'clone_name']
-        }),
-        valueField: 'id',
-        displayField: 'clone_name'
-    });
-    cloneCombo.loadDataForGene = function(gene) {
-        this.store.loadData({
-            rows: window.allClonesPartitionedByMarkerSymbol[gene]
-        });
-        this.setValue('');
-        restOfForm.hide(false);
-        form.dom.onsubmit = function() {return false;}
-    }
-
-    cloneCombo.addListener('select', function() {
-        restOfForm.show(true);
-        form.dom.onsubmit = null;
-    });
-
-    cloneCombo.loadDataForGene('');
-
-    var clonePanel = new Ext.Panel({
-        layout: 'vbox',
-        renderTo: 'mi_attempt_clone_id_combo',
-        height: 90,
-        border: false,
-        items: [
-        {
-            xtype: 'panel',
-            layout: 'vbox',
-            height: 40,
-            border: false,
-            margins: '0 0 10 0',
-            items: [
-            {
-                xtype: 'label',
-                text: 'Marker symbol'
-            },
-            geneCombo
-            ]
-        },
-        {
-            xtype: 'panel',
-            layout: 'vbox',
-            height: 40,
-            border: false,
-            items: [
-            {
-                xtype: 'label',
-                text: 'ES cell clone name',
-                forId: 'mi_attempt_clone_id'
-            },
-            cloneCombo
-            ]
-        }
-        ]
-    });
-
-    geneCombo.getEl().dom.value = '[All]';
-}
-
 Kermits2.CloneSelectorWindow = Ext.extend(Ext.Window, {
     title: 'Search for clones',
     resizable: false,
@@ -189,8 +85,9 @@ Kermits2.CloneSelectorWindow = Ext.extend(Ext.Window, {
                             }
                             var cloneName = listView.getStore().getAt(indices[0]).data['clone_name'];
                             this.cloneSelectorForm.cloneNameTextField.setValue(cloneName);
-
+                            Kermits2.restOfForm.setCloneName(cloneName);
                             this.hideAfterSelection();
+                            Kermits2.restOfForm.showIfHidden();
                         },
                         scope: this
                     }
@@ -225,7 +122,6 @@ Kermits2.CloneSelectorWindow = Ext.extend(Ext.Window, {
         this.cloneSearchTab.cloneNameField.setValue('');
         this.cloneSearchTab.clonesList.getStore().removeAll();
         this.hide();
-        Kermits2.restOfForm.showIfHidden();
     }
 });
 
@@ -283,28 +179,32 @@ Kermits2.CloneSelectorForm = Ext.extend(Ext.Panel, {
         });
     },
 
-    onRender: function(container) {
+    onRender: function() {
         Kermits2.CloneSelectorForm.superclass.onRender.apply(this, arguments);
         this.cloneSelectorWindow.show();
     }
 });
 
-
-function onMiAttemptsNew() {
-    var form = Ext.select('form.new.mi-attempt', true).first();
-    if(! form) {return;}
-    form.dom.onsubmit = function() {return false;};
-
+function processRestOfForm() {
     Kermits2.restOfForm = Ext.get('rest-of-form');
     Kermits2.restOfForm.hide(false);
     Kermits2.restOfForm.hidden = true;
     Kermits2.restOfForm.showIfHidden = function() {
-        console.log(this);
         if(this.hidden == true) {
             this.show(true);
             this.hidden = false;
         }
     }
+    Kermits2.restOfForm.setCloneName = function(cloneName) {
+        var cloneNameField = this.child('input[name="mi_attempt[clone_name]"]');
+        cloneNameField.set({
+            value: cloneName
+        });
+    }
+}
+
+function onMiAttemptsNew() {
+    processRestOfForm();
 
     var panel = new Kermits2.CloneSelectorForm({
         renderTo: 'clone-selector'

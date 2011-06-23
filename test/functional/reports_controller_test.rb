201aae9bbd00c605fa539c872ccf4629d98715f4
Darren Oakley <do2@sanger.ac.uk>
2011-06-23 15:43:56 +0100

    Small refactor of the reports controller.  Added in some functional tests, more to do... refs #4692 #4932 #4941

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -1,8 +1,33 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'The reports controller' do
+    should 'require authentication' do
+      get :index
+      assert_false response.success?
+      assert_redirected_to new_user_session_path
+    end
+    
+    context 'when authorised' do
+      setup do
+        create_common_test_objects
+        sign_in default_user
+      end
+      
+      context 'on the /microinjection_list report' do
+        should 'be blank without parameters' do
+          get :microinjection_list
+          assert response.success?
+          assert_nil assigns(:report)
+        end
+        
+        should 'generate a full report with parameters' do
+          get :microinjection_list, 'commit' => 'Go'
+          assert response.success?
+          assert assigns(:report)
+          assert assigns(:report).is_a? Ruport::Data::Table
+        end
+      end
+    end
   end
 end

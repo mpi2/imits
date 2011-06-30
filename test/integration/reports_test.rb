9bbedc6b606e9544e1749b1581f522e182c4d630
Darren Oakley <do2@sanger.ac.uk>
2011-06-30 14:10:04 +0100

    Added in basic integration tests for the reports controller.
    closes #4942 #4941 #4932 #4962

---
+++ test/integration/reports_test.rb
@@ -0,0 +1,71 @@
+require 'test_helper'
+
+class ReportsTest < ActionDispatch::IntegrationTest
+  context 'The reports pages' do
+    should 'require the user to be logged in' do
+      visit '/reports'
+      assert_login_page
+    end
+    
+    context 'once logged in' do
+      setup do
+        create_common_test_objects
+        visit '/users/logout'
+        login
+      end
+      
+      should 'allow users to visit the reports "home" page' do
+        visit '/reports'
+        assert_match reports_path, current_url
+      end
+      
+      should 'allow users to get reports of all MIs in the system' do
+        visit '/reports'
+        click_link 'All Microinjections'
+        
+        assert_match '/reports/microinjection_list', current_url
+        assert page.has_css?('form')
+        assert page.has_css?('form select#grouping')
+        
+        select 'WTSI', :from => 'production_centre_id[]'
+        click_button 'Generate Report'
+        
+        assert_match '/reports/microinjection_list', current_url
+        assert_match 'production_centre_id', current_url
+        assert page.has_css?('#report table')
+      end
+      
+      should 'allow users to get production summary reports' do
+        visit '/reports'
+        click_link 'Month-by-Month Production Summary'
+        
+        assert_match '/reports/production_summary', current_url
+        assert page.has_css?('form')
+        assert_false page.has_css?('form select#grouping')
+        
+        select 'WTSI', :from => 'production_centre_id[]'
+        click_button 'Generate Report'
+        
+        assert_match '/reports/production_summary', current_url
+        assert_match 'production_centre_id', current_url
+        assert page.has_css?('#report table')
+      end
+      
+      should 'allow users to get gene summary reports' do
+        visit '/reports'
+        click_link 'Gene Summary'
+        
+        assert_match '/reports/gene_summary', current_url
+        assert page.has_css?('form')
+        assert_false page.has_css?('form select#grouping')
+        
+        select 'WTSI', :from => 'production_centre_id[]'
+        click_button 'Generate Report'
+        
+        assert_match '/reports/gene_summary', current_url
+        assert_match 'production_centre_id', current_url
+        assert page.has_css?('#report table')
+      end
+    end
+  end
+end

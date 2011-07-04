7f2979fb679361d2427b7335f5d4b255582b4057
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-04 12:21:46 +0100

    Add User mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default :from => 'htgt@sanger.ac.uk'
+
+  def email(params)
+    params = params.symbolize_keys
+    @email_body = params[:body]
+    mail(:to => params[:user].email, :subject => params[:subject])
+  end
+end

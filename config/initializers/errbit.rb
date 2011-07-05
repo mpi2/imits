6b8bc955ee9bb60a01eed178880a1fc7d1043a0e
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-05 11:43:28 +0100

    Enable hoptoad/errbit

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '5ccaa96909a939f3ef2438c888eb032c'
+  config.host    = 'htgt.internal.sanger.ac.uk'
+  config.port    = 4007
+  config.secure  = config.port == 443
+end

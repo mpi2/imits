1bc4857bb5b98194ac2ac272c39868afaf88c32c
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-20 10:30:55 +0100

    script/cron/clone_production_and_reset_passwords.rb actually does what it says

--- script/cron/clone_production_and_reset_passwords.rb
+++ script/cron/clone_production_and_reset_passwords.rb
@@ -6,6 +6,8 @@ if ! Object.constants.include?(:Rails)
 end
 raise 'Cannot run in production environment' if Rails.env.production?
 
+system "cd #{Rails.root} && RAILS_ENV=#{Rails.env} rake db:clone_production"
+
 User.all.each do |user|
   user.update_attributes!(:password => 'password')
 end

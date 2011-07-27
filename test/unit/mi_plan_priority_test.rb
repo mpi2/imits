a74e15ae1419e330fdab46f69dbaafbca4db1d4b
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-27 15:55:46 +0100

    MiPlanPriority migration/model tweaks

--- test/unit/mi_plan_priority_test.rb
+++ test/unit/mi_plan_priority_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MiPlanPriorityTest < ActiveSupport::TestCase
-  should validate_presence_of :name
-  should validate_uniqueness_of :name
-  should have_many :mi_plans
+  context 'MiPlanPriority' do
+
+    should validate_presence_of :name
+    should validate_uniqueness_of :name
+    should have_many :mi_plans
+
+    should have_db_column(:name).with_options(:null => false)
+    should have_db_index(:name).unique(true)
+
+  end
 end

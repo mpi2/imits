06f714b3bc3cfbf4c9d398b3781d25ce9350a5fd
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-28 12:37:26 +0100

    Write script to load formatted MI interest data

---
+++ script/load_interest_data.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+if ! Object.constants.include?(:Rails)
+  require File.expand_path('../../config/environment', __FILE__)
+end
+
+RANK_PRIORITY_MAP = {
+  1 => MiPlanPriority.find_by_name!('High'),
+  2 => MiPlanPriority.find_by_name!('Medium'),
+  3 => MiPlanPriority.find_by_name!('Low')
+}.freeze
+
+rows = CSV.read(ARGV[0])
+
+rows.each do |consortium_name, production_centre_name, mgi_accession_id, rank, start_point|
+  next unless start_point == 'breeding'
+  MiPlan.create!(
+    :gene => Gene.find_or_create_from_marts_by_mgi_accession_id(mgi_accession_id),
+    :mi_plan_status => MiPlanStatus.find_by_name!('Interest'),
+    :consortium => Consortium.find_by_name!(consortium_name),
+    :production_centre => Centre.find_by_name!(production_centre_name),
+    :mi_plan_priority => RANK_PRIORITY_MAP[rank.to_i]
+  )
+end
---
+++ test/functional/script/load_interest_data_test.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+require 'test_helper'
+
+class LoadInterestDataTest < ExternalScriptTestCase
+  context './script/load_interest_data' do
+
+    def sample_data_file(name)
+      return File.expand_path(File.join(Rails.root, "test/sample_input_data/#{name}.csv"))
+    end
+
+    should 'work' do
+      assert_equal 0, Gene.count
+      assert_equal 0, MiPlan.count
+
+      output = run_script "./script/load_interest_data.rb #{sample_data_file 'conflict_report/mrc'}"
+      assert output.blank?
+
+      gene = Gene.find_by_mgi_accession_id!('MGI:88289')
+      assert_equal 'Cbx2', gene.marker_symbol
+
+      assert_equal 1, gene.mi_plans.size
+      mi_plan = gene.mi_plans.first
+      assert_equal 'Interest', mi_plan.mi_plan_status.name
+      assert_equal 'BASH', mi_plan.consortium.name
+      assert_equal 'MRC - Harwell', mi_plan.production_centre.name
+      assert_equal 'High', mi_plan.mi_plan_priority.name
+
+      assert_equal 2, Gene.count
+      assert_equal 2, MiPlan.count
+    end
+
+    should 'set priority correctly' do
+      output = run_script "./script/load_interest_data.rb #{sample_data_file 'conflict_report/wtsi'}"
+      assert output.blank?
+
+      mi_plan = Gene.find_by_mgi_accession_id!('MGI:1338859').mi_plans.first
+      assert_equal 'Medium', mi_plan.mi_plan_priority.name
+
+      mi_plan = Gene.find_by_mgi_accession_id!('MGI:1346877').mi_plans.first
+      assert_equal 'Low', mi_plan.mi_plan_priority.name
+    end
+
+    should 'ignore non-breeding start points' do
+      assert_equal 0, MiPlan.count
+      output = run_script "./script/load_interest_data.rb #{sample_data_file 'conflict_report/wtsi'}"
+      assert output.blank?
+
+      assert_nil Gene.find_by_mgi_accession_id('MGI:1352750')
+      assert_equal 4, MiPlan.count
+    end
+
+  end
+end

a76747d3754424d123f4d921a53981ad74d2299d
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-29 16:39:06 +0100

    Improve consortium detection in rake one_time:back_fill_mi_plans_from_mi_attempts

--- lib/tasks/one_time.rake
+++ lib/tasks/one_time.rake
@@ -1,26 +1,28 @@
 namespace :one_time do
+
   desc 'Back-fill MiPlans from MiAttempt data'
   task :back_fill_mi_plans_from_mi_attempts => :environment do
-    MiAttempt.all.each do |mi_attempt|
-      if mi_attempt.production_centre.name == 'WTSI'
-        if mi_attempt.es_cell.pipeline.name == 'EUCOMM'
-          consortium = Consortium.find_by_name!('EUCOMM-EUMODIC')
-        elsif mi_attempt.es_cell.pipeline.name == 'KOMP-CSD'
-          consortium = Consortium.find_by_name!('MGP')
+    MiAttempt.transaction do
+      MiAttempt.all.each do |mi_attempt|
+        if mi_attempt.production_centre.name == 'WTSI'
+          if mi_attempt.es_cell.pipeline.name == 'EUCOMM'
+            consortium = Consortium.find_by_name!('EUCOMM-EUMODIC')
+          else
+            consortium = Consortium.find_by_name!('MGP')
+          end
         else
-          raise "Cannot deduce pipeline for mi_attempt(production_centre: #{mi_attempt.production_centre.name}, pipeline: #{mi_attempt.cs_cell.pipeline.name})"
+          consortium = Consortium.find_by_name!('EUCOMM-EUMODIC')
         end
-      else
-        consortium = Consortium.find_by_name!('EUCOMM-EUMODIC')
-      end
 
-      MiPlan.create!(
-        :gene => mi_attempt.es_cell.gene,
-        :consortium => consortium,
-        :production_centre => mi_attempt.production_centre,
-        :mi_plan_status => MiPlanStatus.find_by_name!('Assigned'),
-        :mi_plan_priority => MiPlanPriority.first
-      )
+        MiPlan.create!(
+          :gene => mi_attempt.es_cell.gene,
+          :consortium => consortium,
+          :production_centre => mi_attempt.production_centre,
+          :mi_plan_status => MiPlanStatus.find_by_name!('Assigned'),
+          :mi_plan_priority => MiPlanPriority.first
+        )
+      end
     end
-  end
+  end # :back_fill_mi_plans_from_mi_attempts
+
 end
--- test/integration/rake/one_time/back_fill_mi_plans_from_mi_attempts_test.rb
+++ test/integration/rake/one_time/back_fill_mi_plans_from_mi_attempts_test.rb
@@ -10,7 +10,8 @@ module Rake
         should 'work' do
           gene_cbx1 = Factory.create :gene_cbx1
           gene_trafd1 = Factory.create :gene_trafd1
-          gene_cbx2 = Factory.create :gene, :marker_symbol => 'Cbx2', :mgi_accession_id => 'MGI:88289'
+          gene_cbx2 = Factory.create :gene, :marker_symbol => 'Cbx2'
+          gene_cbx7 = Factory.create :gene, :marker_symbol => 'Cbx7'
 
           Factory.create :mi_attempt,
                   :es_cell => Factory.create(:es_cell, :gene => gene_cbx1),
@@ -21,28 +22,34 @@ module Rake
           Factory.create :mi_attempt,
                   :es_cell => Factory.create(:es_cell, :gene => gene_cbx2, :pipeline => Pipeline.find_by_name!('EUCOMM')),
                   :production_centre => Centre.find_by_name!('WTSI')
+          Factory.create :mi_attempt,
+                  :es_cell => Factory.create(:es_cell, :gene => gene_cbx7, :pipeline => Pipeline.find_by_name!('SANGER_FACULTY')),
+                  :production_centre => Centre.find_by_name!('WTSI')
 
           assert_equal 0, MiPlan.count
           run_script 'rake --trace one_time:back_fill_mi_plans_from_mi_attempts'
-          assert_equal 3, MiPlan.count
+          assert_equal 4, MiPlan.count
 
           mi_plan_eucomm_eumodic_1 = MiPlan.find_by_gene_id!(Gene.find_by_marker_symbol!('Cbx1'))
-          mi_plan_mgp = MiPlan.find_by_gene_id!(Gene.find_by_marker_symbol!('Trafd1'))
+          mi_plan_mgp_1 = MiPlan.find_by_gene_id!(Gene.find_by_marker_symbol!('Trafd1'))
           mi_plan_eucomm_eumodic_2 = MiPlan.find_by_gene_id!(Gene.find_by_marker_symbol!('Cbx2'))
+          mi_plan_mgp_2 = MiPlan.find_by_gene_id!(Gene.find_by_marker_symbol!('Cbx7'))
 
           assert_equal 'EUCOMM-EUMODIC', mi_plan_eucomm_eumodic_1.consortium.name
-          assert_equal 'MGP', mi_plan_mgp.consortium.name
+          assert_equal 'MGP', mi_plan_mgp_1.consortium.name
           assert_equal 'EUCOMM-EUMODIC', mi_plan_eucomm_eumodic_2.consortium.name
+          assert_equal 'MGP', mi_plan_mgp_2.consortium.name
 
           assert_equal 'ICS', mi_plan_eucomm_eumodic_1.production_centre.name
-          assert_equal 'WTSI', mi_plan_mgp.production_centre.name
+          assert_equal 'WTSI', mi_plan_mgp_1.production_centre.name
           assert_equal 'WTSI', mi_plan_eucomm_eumodic_2.production_centre.name
+          assert_equal 'WTSI', mi_plan_mgp_2.production_centre.name
 
-          assert_equal ['Assigned', 'Assigned', 'Assigned'],
-                  [mi_plan_eucomm_eumodic_1.mi_plan_status.name, mi_plan_mgp.mi_plan_status.name, mi_plan_eucomm_eumodic_2.mi_plan_status.name]
+          assert_equal ['Assigned', 'Assigned', 'Assigned', 'Assigned'],
+                  [mi_plan_eucomm_eumodic_1, mi_plan_mgp_1, mi_plan_eucomm_eumodic_2, mi_plan_mgp_2].map {|i| i.mi_plan_status.name}
 
-          assert_equal ['High', 'High', 'High'],
-                  [mi_plan_eucomm_eumodic_1.mi_plan_priority.name, mi_plan_mgp.mi_plan_priority.name, mi_plan_eucomm_eumodic_2.mi_plan_priority.name]
+          assert_equal ['High', 'High', 'High', 'High'],
+                  [mi_plan_eucomm_eumodic_1, mi_plan_mgp_1, mi_plan_eucomm_eumodic_2, mi_plan_mgp_2].map {|i| i.mi_plan_priority.name}
         end
 
       end

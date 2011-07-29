2a9609d79fb2c5c23665b84daea0aca631dd7625
Asfand Qazi <aq2@sanger.ac.uk>
2011-07-29 12:30:00 +0100

    MiPlan::assign_genes_and_mark_conflicts preliminarily complete

--- app/models/mi_plan.rb
+++ app/models/mi_plan.rb
@@ -18,16 +18,38 @@ class MiPlan < ActiveRecord::Base
   validates_uniqueness_of :gene_id, :scope => [:consortium_id, :production_centre_id]
 
   def self.assign_genes_and_mark_conflicts
-    grouped_by_gene = self.all.group_by {|i| i.gene.mgi_accession_id}
+    conflict_status = MiPlanStatus.find_by_name!('Conflict')
+    declined_status = MiPlanStatus.find_by_name!('Declined')
 
-    grouped_by_gene.each do |mgi_accession_id, mi_plans|
-      mi_plans.each do |mi_plan|
-        mi_plan.mi_plan_status = MiPlanStatus.find_by_name!('Assigned')
-        mi_plan.save!
+    self.all_grouped_by_mgi_accession_id_then_by_status_name.each do
+      |mgi_accession_id, mi_plans_by_status|
+
+      interested = mi_plans_by_status['Interest']
+
+      if ! mi_plans_by_status['Assigned'].blank?
+        interested.each do |mi_plan|
+          mi_plan.mi_plan_status = declined_status
+          mi_plan.save!
+        end
+      elsif ! mi_plans_by_status['Conflict'].blank? or interested.size != 1
+        interested.each do |mi_plan|
+          mi_plan.mi_plan_status = conflict_status
+          mi_plan.save!
+        end
+      else
+        interested.first.mi_plan_status = MiPlanStatus.find_by_name!('Assigned')
+        interested.first.save!
       end
     end
   end
 
+  def self.all_grouped_by_mgi_accession_id_then_by_status_name
+    mi_plans = self.all.group_by {|i| i.gene.mgi_accession_id}
+    mi_plans = mi_plans.each do |mgi_accession_id, all_for_gene|
+      mi_plans[mgi_accession_id] = all_for_gene.group_by {|i| i.mi_plan_status.name}
+    end
+    return mi_plans
+  end
 end
 
 # == Schema Information
--- test/unit/mi_plan_test.rb
+++ test/unit/mi_plan_test.rb
@@ -5,55 +5,74 @@ require 'test_helper'
 class MiPlanTest < ActiveSupport::TestCase
   context 'MiPlan' do
 
-    setup do
-      Factory.create :mi_plan
-    end
+    context '(misc. tests):' do
+      setup do
+        Factory.create :mi_plan
+      end
 
-    should belong_to :gene
-    should belong_to :consortium
-    should belong_to :production_centre
-    should belong_to :mi_plan_status
-    should belong_to :mi_plan_priority
+      should belong_to :gene
+      should belong_to :consortium
+      should belong_to :production_centre
+      should belong_to :mi_plan_status
+      should belong_to :mi_plan_priority
 
-    should validate_presence_of :gene
-    should validate_presence_of :consortium
-    should validate_presence_of :mi_plan_status
-    should validate_presence_of :mi_plan_priority
+      should validate_presence_of :gene
+      should validate_presence_of :consortium
+      should validate_presence_of :mi_plan_status
+      should validate_presence_of :mi_plan_priority
 
-    should 'validate the uniqueness of gene_id scoped to consortium_id and production_centre_id' do
-      mip = Factory.build :mi_plan
-      assert mip.save
-      assert mip.valid?
+      should 'validate the uniqueness of gene_id scoped to consortium_id and production_centre_id' do
+        mip = Factory.build :mi_plan
+        assert mip.save
+        assert mip.valid?
 
-      mip2 = MiPlan.new( :gene => mip.gene, :consortium => mip.consortium )
-      assert_false mip2.save
-      assert_false mip2.valid?
-      assert ! mip2.errors['gene_id'].blank?
+        mip2 = MiPlan.new( :gene => mip.gene, :consortium => mip.consortium )
+        assert_false mip2.save
+        assert_false mip2.valid?
+        assert ! mip2.errors['gene_id'].blank?
 
-      mip.production_centre = Centre.find_by_name('WTSI')
-      assert mip.save
-      assert mip.valid?
+        mip.production_centre = Centre.find_by_name('WTSI')
+        assert mip.save
+        assert mip.valid?
 
-      mip2.production_centre = mip.production_centre
-      assert_false mip2.save
-      assert_false mip2.valid?
-      assert ! mip2.errors['gene_id'].blank?
+        mip2.production_centre = mip.production_centre
+        assert_false mip2.save
+        assert_false mip2.valid?
+        assert ! mip2.errors['gene_id'].blank?
 
-      # TODO: Need to account for the inevitable... we're gonna get MiP's that have
-      #       a gene and consortium then nil for production_centre, and a duplicate
-      #       with the same gene and consortium BUT with a production_centre assigned.
-      #       Really, the fist should be updated to become the second (i.e. not produce a duplicate).
+        # TODO: Need to account for the inevitable... we're gonna get MiP's that have
+        #       a gene and consortium then nil for production_centre, and a duplicate
+        #       with the same gene and consortium BUT with a production_centre assigned.
+        #       Really, the fist should be updated to become the second (i.e. not produce a duplicate).
+      end
     end
 
     context '::assign_genes_and_mark_conflicts' do
-      should 'set Interested MiPlan to Assigned status if no other Interested or Assigned MiPlan for the same gene exists' do
+      def setup_for_set_one_to_assigned
         gene = Factory.create :gene_cbx1
-        only_interest_mi_plan = Factory.create :mi_plan, :gene => gene, :consortium => Consortium.find_by_name!('BASH')
+        @only_interest_mi_plan = Factory.create :mi_plan, :gene => gene, :consortium => Consortium.find_by_name!('BASH')
+        @declined_mi_plans = [
+          Factory.create(:mi_plan, :gene => gene,
+            :consortium => Consortium.find_by_name!('MGP'),
+            :mi_plan_status => MiPlanStatus.find_by_name!('Declined')),
+          Factory.create(:mi_plan, :gene => gene,
+            :consortium => Consortium.find_by_name!('EUCOMM-EUMODIC'),
+            :mi_plan_status => MiPlanStatus.find_by_name!('Declined'))
+        ]
 
         MiPlan.assign_genes_and_mark_conflicts
+        @only_interest_mi_plan.reload
+        @declined_mi_plans.each(&:reload)
+      end
 
-        only_interest_mi_plan.reload
-        assert_equal 'Assigned', only_interest_mi_plan.mi_plan_status.name
+      should 'set Interested MiPlan to Assigned status if no other Interested or Assigned MiPlan for the same gene exists' do
+        setup_for_set_one_to_assigned
+        assert_equal 'Assigned', @only_interest_mi_plan.mi_plan_status.name
+      end
+
+      should 'not affect non-Interested MiPlans when setting Interested ones to Assigned' do
+        setup_for_set_one_to_assigned
+        assert_equal ['Declined', 'Declined'], @declined_mi_plans.map{|i| i.mi_plan_status.name}
       end
 
       should 'set all Interested MiPlans that have the same gene to Conflict' do
@@ -69,7 +88,65 @@ class MiPlanTest < ActiveSupport::TestCase
                 mi_plans.map {|i| i.mi_plan_status.name }
       end
 
-      should 'set Interested MiPlan to Assigned if only Declined MiPlans exist for that gene'
+      should 'set all Interested MiPlans to Conflict if other MiPlans for the same gene are in Conflict' do
+        gene = Factory.create :gene_cbx1
+        mi_plans = ['MGP', 'EUCOMM-EUMODIC'].map do |consortium_name|
+          Factory.create :mi_plan, :gene => gene,
+                  :consortium => Consortium.find_by_name!(consortium_name),
+                  :mi_plan_status => MiPlanStatus.find_by_name!('Conflict')
+        end
+
+        interested_mi_plan = Factory.create :mi_plan,
+                :gene => gene, :consortium => Consortium.find_by_name!('BASH')
+
+        MiPlan.assign_genes_and_mark_conflicts
+        interested_mi_plan.reload
+
+        assert_equal 'Conflict', interested_mi_plan.mi_plan_status.name
+      end
+
+      should 'set all Interested MiPlans to Declined if other MiPlans for the same gene are already Assigned' do
+        gene = Factory.create :gene_cbx1
+        Factory.create :mi_plan, :gene => gene,
+                :consortium => Consortium.find_by_name!('BASH'),
+                :mi_plan_status => MiPlanStatus.find_by_name!('Assigned')
+        mi_plans = ['MGP', 'EUCOMM-EUMODIC'].map do |consortium_name|
+          Factory.create :mi_plan, :gene => gene, :consortium => Consortium.find_by_name!(consortium_name)
+        end
+
+        MiPlan.assign_genes_and_mark_conflicts
+        mi_plans.each(&:reload)
+
+        assert_equal ['Declined', 'Declined'],
+                mi_plans.map {|i| i.mi_plan_status.name }
+      end
+
+    end # ::assign_genes_and_mark_conflicts
+
+    context '::all_grouped_by_mgi_accession_id_then_by_status_name' do
+      should 'work' do
+        Factory.create :consortium, :name => 'Consortium X'
+        gene1 = Factory.create :gene_cbx1
+        gene2 = Factory.create :gene_trafd1
+        bash = Factory.create :mi_plan, :gene => gene1,
+                :consortium => Consortium.find_by_name!('BASH'),
+                :mi_plan_status => MiPlanStatus.find_by_name!('Interest')
+        consortium_x = Factory.create :mi_plan, :gene => gene1,
+                :consortium => Consortium.find_by_name!('Consortium X'),
+                :mi_plan_status => MiPlanStatus.find_by_name!('Interest')
+        mgp = Factory.create :mi_plan, :gene => gene1,
+                :consortium => Consortium.find_by_name!('MGP'),
+                :mi_plan_status => MiPlanStatus.find_by_name!('Assigned')
+        eucomm = Factory.create :mi_plan, :gene => gene2,
+                :consortium => Consortium.find_by_name!('EUCOMM-EUMODIC'),
+                :mi_plan_status => MiPlanStatus.find_by_name!('Declined')
+
+        result = MiPlan.all_grouped_by_mgi_accession_id_then_by_status_name
+
+        assert_equal [bash, consortium_x].sort, result[gene1.mgi_accession_id]['Interest'].sort
+        assert_equal [mgp], result[gene1.mgi_accession_id]['Assigned']
+        assert_equal [eucomm], result[gene2.mgi_accession_id]['Declined']
+      end
     end
 
   end

ff3f04c09eaa874619103913d8ba92235cabeae1
Asfand Qazi <aq2@sanger.ac.uk>
2011-08-02 11:09:07 +0100

    ExtJS4-ify text/date fields

--- public/javascripts/mi_attempts/shared.js
+++ public/javascripts/mi_attempts/shared.js
@@ -1,44 +1,43 @@
-/*
 function replaceTextFieldWithExtField(selector, replacementCreationFunction) {
     Ext.select(selector).each(function(textField) {
-        var renderDiv = Ext.DomHelper.createDom({tag: 'div'});
         var name = textField.dom.name;
         var defaultValue = textField.dom.value;
-        textField.replaceWith(renderDiv);
+        var renderDiv = new Ext.Element(Ext.core.DomHelper.createDom({tag: 'div'}));
+        renderDiv.replace(textField);
 
         replacementCreationFunction(renderDiv, name, defaultValue);
     });
 }
 
-function initDateFields() {
-    replaceTextFieldWithExtField('.date-field', function(renderDiv, name, defaultValue) {
-        new Ext.form.DateField({
-            cls: 'date-field',
-            renderTo: renderDiv,
-            name: name,
-            editable: false,
-            format: 'd/m/Y',
-            value: defaultValue
-        });
-    });
-}
-
 function initNumberFields() {
     replaceTextFieldWithExtField('.number-field', function(renderDiv, name, defaultValue) {
-        new Ext.form.NumberField({
+        new Ext.form.field.Number({
             cls: 'number-field',
             renderTo: renderDiv,
             name: name,
-            allowDecimals: false,
-            allowNegative: false,
+            value: defaultValue,
             width: 40,
-            value: defaultValue
+            allowDecimals: false,
+            minValue: 0,
+            hideTrigger: true,
+            keyNavEnabled: false,
+            mouseWheelEnabled: false
         });
     });
 }
 
-Ext.onReady(function() {
-    initDateFields();
-    initNumberFields();
-});
-*/
+function initDateFields() {
+    replaceTextFieldWithExtField('.date-field', function(renderDiv, name, defaultValue) {
+        new Ext.form.field.Date({
+            cls: 'date-field',
+            renderTo: renderDiv,
+            name: name,
+            value: defaultValue,
+            editable: false,
+            format: 'd/m/Y'
+        });
+    });
+}
+
+Ext.onReady(initNumberFields);
+Ext.onReady(initDateFields);

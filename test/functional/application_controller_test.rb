83265401f67f19732528976c6f747ae5d5c945aa
Darren Oakley <daz.oakley@gmail.com>
2011-09-13 16:21:57 +0100

    Flesh out the application controller tests to test for when an ExtJS data store tries to pass through filter parameters

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -45,6 +45,30 @@ class ApplicationControllerTest < ActionController::TestCase
         assert_equal({'submit' => 'blank', 'attr1_eq' => 'blank', 'attr2_eq' => 'blank'},
           @controller.send(:params_cleaned_for_search, dirty_params))
       end
+
+      should 'process the "filter" sub-array' do
+        expected_params = {
+          'submit'    => 'blank',
+          'attr1_eq'  => 'weee',
+          'attr2_in'  => ['foo','bar','baz']
+        }
+
+        dirty_params = {
+          'controller'  => 'blank',
+          'submit'      => 'blank',
+          'filter'      => [
+            { 'property' => 'attr1_eq', 'value' => 'weee' },
+            { 'property' => 'attr2_in', 'value' => "foo\nbar \nbaz" }
+          ]
+        }
+
+        assert_equal expected_params, @controller.send(:params_cleaned_for_search, dirty_params)
+
+        dirty_params['filter'] = dirty_params['filter'].to_json
+
+        assert dirty_params['filter'].is_a?(String)
+        assert_equal expected_params, @controller.send(:params_cleaned_for_search, dirty_params)
+      end
     end
 
   end

d363eabd8a6f7b9fd4bb53befa6ce50610e83ef5
Asfand Qazi <aq2@sanger.ac.uk>
2011-09-27 11:12:59 +0100

    Revert "Import ExtJS auto-loader stuff from gene-selection"
    This reverts commit 98df8664c308137cae967956146caaf31b308579.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -99,8 +99,15 @@ Ext.util.Format.safeTextRenderer = function(value) {
 Ext.Loader.setConfig({
     enabled: true,
     paths: {
-        'Imits': window.basePath + '/javascripts/Imits',
-        'Ext': window.basePath + '/extjs',
-        'Ext.ux': window.basePath + '/extjs/examples/ux'
+        'Imits': window.basePath + '/javascripts/Imits'
     }
 });
+Ext.Loader.setPath('Ext', window.basePath + '/extjs');
+Ext.Loader.setPath('Ext.ux', window.basePath + '/extjs/examples/ux');
+
+// TODO Why?!?!
+Ext.require([
+  'Imits.model.MiAttempt',
+  'Imits.model.Gene'
+]);
+

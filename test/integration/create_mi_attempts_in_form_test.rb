7c441f0524c71a4b15649e9387a9b7bf79bf33e6
Asfand Qazi <aq2@sanger.ac.uk>
2011-10-03 09:37:11 +0100

    Make create_mi_attempts_in_form_test.rb work

--- test/integration/create_mi_attempts_in_form_test.rb
+++ test/integration/create_mi_attempts_in_form_test.rb
@@ -11,7 +11,7 @@ class CreateMiAttemptsInFormTest < ActionDispatch::IntegrationTest
       fill_in 'marker_symbol-search-box', :with => marker_symbol
       click_button 'Search'
       sleep 5
-      find(:xpath, '//em[text()="' + es_cell_name + '"]').click
+      find(:xpath, '//td/div[text()="' + es_cell_name + '"]').click
     end
 
     setup do

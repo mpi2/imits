d9b2d86cb665fed42af154bb2cbcbd10fe4cbd10
Asfand Qazi <aq2@sanger.ac.uk>
2011-10-11 15:45:33 +0100

    Make choose_es_cell_from_list method in test_helper more robust

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -134,8 +134,7 @@ class Kermits2::JsIntegrationTest < ActionDispatch::IntegrationTest
 
   def choose_es_cell_from_list(marker_symbol = 'Cbx1', es_cell_name = 'EPD0027_2_A01')
     fill_in 'marker_symbol-search-box', :with => marker_symbol
-    click_button 'Search'
-    sleep 5
+    find(:xpath, '//button/span[text()="Search"]').click
     find(:xpath, '//td/div[text()="' + es_cell_name + '"]').click
   end
   

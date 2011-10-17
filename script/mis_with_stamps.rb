8784042e020481ec93157794112301df11f89268
Asfand Qazi <aq2@sanger.ac.uk>
2011-10-17 11:53:47 +0100

    Order MIs by created_at in script/mis_with_stamps.rb

--- script/mis_with_stamps.rb
+++ script/mis_with_stamps.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-data = MiAttempt.order(:id).all.map do |mi|
+data = MiAttempt.order('created_at asc').all.map do |mi|
   [
     "#{mi.colony_name} #{mi.es_cell_name}",
     mi.mi_plan.status_stamps.order(:created_at).map {|i| "#{i.created_at.strftime('%F %T')} #{i.name}"},

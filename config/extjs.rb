683ede7a000617838e3c8b16e3fc8eee43942c3c
Asfand Qazi <aq2@sanger.ac.uk>
2011-10-24 08:10:43 +0100

    Upgrade master to ExtJS 4.0.7

--- config/extjs.rb
+++ config/extjs.rb
@@ -1,4 +1,4 @@
-EXTJS_VERSION = '4.0.2a'
+EXTJS_VERSION = '4.0.7'
 EXTJS_BASEDIR = "ext-#{EXTJS_VERSION}"
 EXTJS_ZIPFILE = "ext-#{EXTJS_VERSION}-gpl.zip"
-EXTJS_PATH='/nfs/team87/services/extjs'
+EXTJS_PATH = '/nfs/team87/services/extjs'
--- public/javascripts/extjs_hacky_monkey_patches.js
+++ public/javascripts/extjs_hacky_monkey_patches.js
@@ -1,98 +1,5 @@
 Ext.onReady(function() {
-    function patches_for_4_0_2() {
-        Ext.override(Ext.menu.Menu, {
-            doConstrain: function() {
-                var me = this,
-                y = me.el.getY(),
-                max, full,
-                vector,
-                returnY = y, normalY, parentEl, scrollTop, viewHeight;
-
-                full = me.getHeight();
-                delete me.height;
-                me.setSize();
-                if (me.floating) {
-                    parentEl = Ext.fly(me.el.dom.parentNode);
-                    scrollTop = parentEl.getScroll().top;
-                    viewHeight = parentEl.getViewSize().height;
-                    //Normalize y by the scroll position for the parent element.  Need to move it into the coordinate space
-                    //of the view.
-                    normalY = y - scrollTop;
-                    max = me.maxHeight ? me.maxHeight : viewHeight - normalY;
-                    if (full > viewHeight) {
-                        max = viewHeight;
-                        //Set returnY equal to (0,0) in view space by reducing y by the value of normalY
-                        returnY = y - normalY;
-                    } else if (max < full) {
-                        returnY = y - (full - max);
-                        max = full;
-                    }
-                }else{
-                    max = me.getHeight();
-                }
-                // Always respect maxHeight
-                if (me.maxHeight){
-                    max = Math.min(me.maxHeight, max);
-                }
-                if (full > max && max > 0){
-                    me.layout.autoSize = false;
-                    me.setHeight(max);
-                    if (me.showSeparator){
-                        me.iconSepEl.setHeight(me.layout.getRenderTarget().dom.scrollHeight);
-                    }
-                }
-                vector = me.getConstrainVector(me.el.dom.parentNode);
-                if (vector) {
-                    me.setPosition(me.getPosition()[0] + vector[0]);
-                }
-                me.el.setY(returnY);
-
-            }
-        });
-
-        Ext.override(Ext.data.Store, {
-            afterEdit : function(record) {
-                var me = this;
-
-                if (me.autoSync) {
-                    me.sync();
-                }
-
-                if (record.dirty) {
-                    record.commit();
-                }
-                me.fireEvent('update', me, record, Ext.data.Model.EDIT);
-            },
-
-            afterReject : function(record) {
-                if (record.dirty) {
-                    record.commit();
-                }
-                this.fireEvent('update', this, record, Ext.data.Model.REJECT);
-            },
-
-            afterCommit : function(record) {
-                if (record.dirty) {
-                    record.commit();
-                }
-                this.fireEvent('update', this, record, Ext.data.Model.COMMIT);
-            }
-
-        });
-    }
-
-    function patches_for_4_0_6() {
-        Ext.override(Ext.EventObjectImpl, {
-            correctWheelDelta: function (delta) {
-                var scale = this.WHEEL_SCALE,
-                ret=Math.round(delta/scale);
-
-                if (!ret && delta) {
-                    ret = (delta < 0) ? -1 : 1;
-                }
-                return ret;
-            }
-        });
+    function patches_for_4_0_7() {
     }
 
     var extvers = [Ext.versions.core.major, Ext.versions.core.minor, Ext.versions.core.patch];
@@ -101,11 +8,7 @@ Ext.onReady(function() {
         return;
     }
 
-    if(extvers[2] <= 6) {
-        patches_for_4_0_6();
-    }
-
-    if(extvers[2] <= 2) {
-        patches_for_4_0_2();
+    if(extvers[2] <= 7) {
+        patches_for_4_0_7();
     }
 });

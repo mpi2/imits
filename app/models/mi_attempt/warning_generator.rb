e8c8765b10997eb6c57c9d80b57f599fb86d6996
Asfand Qazi <aq2@sanger.ac.uk>
2011-11-03 18:03:54 +0000

    MiAttempt errors out if MiPlan is aborted due to ES Cell QC; refactor some validations too

--- app/models/mi_attempt/warning_generator.rb
+++ app/models/mi_attempt/warning_generator.rb
@@ -5,7 +5,7 @@ module MiAttempt::WarningGenerator
   WARNING_MESSAGES = {
     :micro_injecting_unassigned_gene => 'The gene being micro-injected has not been assigned to %{production_centre_name} yet - continuing will assign it to %{production_centre_name}.',
     :gene_already_micro_injected => 'This gene has already been micro-injected by another production centre.',
-    :assigning_production_centre => 'The consortium %{consortium_name} is planning on micro-injecting this gene, but has not yet assigned a production centre.  Continuing will assign %{production_centre_name} as the production centre micro-injecting the gene on behalf of %{consortium_name}.',
+    :setting_production_centre => 'The consortium %{consortium_name} is planning on micro-injecting this gene, but has not yet assigned a production centre.  Continuing will assign %{production_centre_name} as the production centre micro-injecting the gene on behalf of %{consortium_name}.',
     :no_interest_expressed_in_gene => 'There are no expressions of interest for this gene yet.  Continuing will assign %{production_centre_name} to micro-inject it.'
   }
 
@@ -21,15 +21,13 @@ module MiAttempt::WarningGenerator
       return true
     end
 
-    potential_mi_plan = MiPlan.where(mi_plan_lookup_conditions).first
+    potential_mi_plan = find_matching_mi_plan
     if !potential_mi_plan
-      if MiPlan.where(mi_plan_lookup_conditions.merge(:production_centre_id => nil)).first
-        add_warning :assigning_production_centre
-        return true
-      else
-        add_warning :no_interest_expressed_in_gene
-        return true
-      end
+      add_warning :no_interest_expressed_in_gene
+      return true
+    elsif potential_mi_plan.production_centre == nil
+      add_warning :setting_production_centre
+      return true
     end
 
     if ! potential_mi_plan.assigned?

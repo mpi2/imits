4afcc4b544050e440840ce1542b3a86f01ec80dc
Asfand Qazi <aq2@sanger.ac.uk>
2011-11-04 16:28:52 +0000

    Revert "Remove data migration stuff"
    This reverts commit e7d098a580f8e49a005fead2d764d35019bf2e94.

---
+++ test/unit/kermits2/migration/run_from_script_test.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+require 'test_helper'
+
+class Kermits2::Migration::RunFromScriptTest < ExternalScriptTestCase
+  context 'Kermits2::Migration when run from script' do
+
+    setup do
+      Pipeline.destroy_all
+      Centre.destroy_all
+      assert_equal 0, MiAttempt.count
+    end
+
+    should 'works when invoked directly with just one mi attempt' do
+      run_script "./script/runner 'Kermits2::Migration.run(:mi_attempt_ids => [11029])'"
+
+      assert_equal 1, MiAttempt.count
+    end
+
+    should 'work when invoked as ./script/data_migration' do
+      expected_mis = Old::MiAttempt.count - Kermits2::Migration::EXCLUDE_LIST.size
+      run_script './script/data_migration'
+      assert_equal expected_mis, MiAttempt.count
+      cursor = Old::Clone.connection.execute('select count(distinct emi_clone.clone_name) from emi_attempt inner join emi_event on emi_event.id = emi_attempt.event_id  inner join emi_clone on emi_clone.id = emi_event.clone_id')
+      number_of_distinct_clones = cursor.fetch.first.to_i
+      assert_equal number_of_distinct_clones, EsCell.count
+    end
+
+  end
+end

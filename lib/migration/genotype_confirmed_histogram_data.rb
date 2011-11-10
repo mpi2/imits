80b238d75f0a240c8a1cc835b9bdad5ec33fa035
Asfand Qazi <aq2@sanger.ac.uk>
2011-11-10 16:53:24 +0000

    lib/migration/genotype_confirmed_histogram_data.rb added

---
+++ lib/migration/genotype_confirmed_histogram_data.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+
+mi_delta_data = []
+
+MiAttempt.where(:mi_attempt_status_id => MiAttemptStatus.genotype_confirmed.id).order('id asc').each do |mi|
+  ip = mi.status_stamps.detect {|ss| ss.mi_attempt_status == MiAttemptStatus.micro_injection_in_progress}
+
+  if ! ip
+    STDERR.puts "Skipping #{mi.colony_name}(#{mi.id}), no Micro-injection in progress date"
+    next
+  end
+
+  gc = mi.status_stamps.detect {|ss| ss.mi_attempt_status == MiAttemptStatus.genotype_confirmed}
+
+  ip = ip.created_at
+  gc = gc.created_at
+
+  delta = ((gc - ip)/3600/24/30 + 0.5).to_i
+
+  mi_delta_data[delta] ||= []
+  mi_delta_data[delta] << mi
+end
+
+mi_delta_data.map! {|i| i || [] }
+
+CSV.open('histogram_data.csv', 'wb') do |csv|
+  csv << ['months_delta', 'number']
+  mi_delta_data.each_with_index do |mis, months_delta|
+    csv << [months_delta, mis.size]
+  end
+end
+
+CSV.open('mi_delta_data.csv', 'wb') do |csv|
+  csv << ['colony_name', 'mi_date', 'months_delta']
+  mi_delta_data.each_with_index do |mis, months_delta|
+    mis.each do |mi|
+      csv << [mi.colony_name, mi.mi_date, months_delta]
+    end
+  end
+end

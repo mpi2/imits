7c23bbd381405668fd80a0435ca01ca2d89dbcbf
Richard Easty <re4@sanger.ac.uk>
2011-11-10 15:31:38 +0000

    add test for mi_attempts

--- test/unit/reports/mi_plans_test.rb
+++ test/unit/reports/mi_plans_test.rb
@@ -6,7 +6,7 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
   
   context 'Reports::MiPlans' do
 
-    should 'display double-assignments between two consortia and production centres' do
+    should 'display double-assignments between two consortia and production centres (matrix)' do
 
         gene_cbx1 = Factory.create :gene_cbx1
         Factory.create :mi_plan, :gene => gene_cbx1,
@@ -68,8 +68,6 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
         columns = Reports::MiPlans::DoubleAssignment.get_matrix_columns
         assert columns && columns.size > 0, "Could not get columns"
         
-#        puts MiPlan.all.map {|i| i.to_json }
-
         assert report.column('KOMP2/JAX')[0] == 1, "Expected to find value 1"
           
         puts report.to_s
@@ -139,34 +137,12 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
           :mi_plan_status => MiPlanStatus['Assigned']
       
         report = Reports::MiPlans::DoubleAssignment.get_list_raw
-#        report = Reports::MiPlans::DoubleAssignment.get_list
         assert report, "Could not get report"
         columns = Reports::MiPlans::DoubleAssignment.get_list_columns
         assert columns && columns.size > 0, "Could not get columns"
 
         puts report.to_s
         
-        #puts 'Marker Symbol: 0 - ' + report.column('Marker Symbol')[0].to_s
-        #puts 'Marker Symbol: 1 - ' + report.column('Marker Symbol')[1].to_s
-        #puts 'Marker Symbol: 2 - ' + report.column('Marker Symbol')[2].to_s
-        #puts 'Marker Symbol: 3 - ' + report.column('Marker Symbol')[3].to_s
-        #
-        #puts 'Consortium: 0 - ' + report.column('Consortium')[0].to_s
-        #puts 'Consortium: 1 - ' + report.column('Consortium')[1].to_s
-        #puts 'Consortium: 2 - ' + report.column('Consortium')[2].to_s
-        #puts 'Consortium: 3 - ' + report.column('Consortium')[3].to_s
-        #
-        #puts 'Plan Status: 0 - ' + report.column('Plan Status')[0].to_s
-        #puts 'Plan Status: 1 - ' + report.column('Plan Status')[1].to_s
-        #puts 'Plan Status: 2 - ' + report.column('Plan Status')[2].to_s
-        #puts 'Plan Status: 3 - ' + report.column('Plan Status')[3].to_s
-        #
-        #puts 'Centre: 0 - ' + report.column('Centre')[0].to_s
-        #puts 'Centre: 1 - ' + report.column('Centre')[1].to_s
-        #puts 'Centre: 2 - ' + report.column('Centre')[2].to_s
-        #puts 'Centre: 3 - ' + report.column('Centre')[3].to_s
-
-
         assert report.column('Marker Symbol')[0] == 'Cbx1'
         assert report.column('Marker Symbol')[1] == 'Cbx1'
         assert report.column('Marker Symbol')[2] == 'Cbx1'
@@ -186,14 +162,6 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
         assert report.column('Centre')[1] == 'NONE'
         assert report.column('Centre')[2] == 'NONE'
         assert report.column('Centre')[3] == 'NONE'
-
-        #columns.each do |column|
-        #  values = report.column(column)
-        #  values.each do |value|
-        #    puts values.to_s
-        #  end
-        #  
-        #end
         
     end
 
@@ -211,7 +179,6 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
           :production_centre => Centre.find_by_name('WTSI'),
           :mi_plan_status => MiPlanStatus['Assigned']
         
-#        report = Reports::MiPlans::DoubleAssignment.get_list_raw
         report = Reports::MiPlans::DoubleAssignment.get_list
         assert report, "Could not get report"
         columns = Reports::MiPlans::DoubleAssignment.get_list_columns
@@ -226,12 +193,6 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
 
     should 'have consortia on page (list)' do
       
-        #gene_cbx1 = Factory.create :gene_cbx1
-        #Factory.create :mi_plan, :gene => gene_cbx1,
-        #  :consortium => Consortium.find_by_name('BaSH'),
-        #  :production_centre => Centre.find_by_name('WTSI'),
-        #  :mi_plan_status => MiPlanStatus['Assigned']
-   
         gene_trafd1 = Factory.create :gene_trafd1
         Factory.create :mi_plan, :gene => gene_trafd1,
           :consortium => Consortium.find_by_name('BaSH'),
@@ -252,7 +213,6 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
 
         assert report.to_s =~ /DOUBLE-ASSIGNMENTS FOR consortium: BaSH:/, "Cannot find BaSH"
         assert report.to_s =~ /DOUBLE-ASSIGNMENTS FOR consortium: JAX:/, "Cannot find JAX"
-#        assert report.to_s =~ /DOUBLE-ASSIGNMENTS FOR consortium: sid:/, "Cannot find sid"
       
     end
 
@@ -296,6 +256,68 @@ class Reports::MiPlansTest < ActiveSupport::TestCase
         assert report.column('Centre')[1] == 'WTSI'
         assert report.column('Centre')[2] == 'JAX'
         assert report.column('Centre')[3] == 'WTSI'
+
+    end
+
+    should 'display double-assignments between two consortia with production centres and mi_attempts (list)' do
+                 
+        gene_trafd1 = Factory.create :gene_trafd1
+        
+        Factory.create :mi_attempt,
+          :es_cell => Factory.create(:es_cell, :gene => gene_trafd1),
+          'mi_date' => '2011-11-05',
+          :mi_attempt_status => MiAttemptStatus.micro_injection_in_progress,
+          :production_centre_name => 'WTSI',
+          :consortium_name => 'BaSH'
+
+
+#BaSH DTCC
+
+        Factory.create :mi_attempt,
+          :es_cell => Factory.create(:es_cell, :gene => gene_trafd1),
+          'mi_date' => '2011-10-05',
+          :mi_attempt_status => MiAttemptStatus.micro_injection_in_progress,
+          :production_centre_name => 'WTSI',
+          :consortium_name => 'DTCC'
+                  
+#puts "DATE: " + mi_attempt.mi_date.to_s                             
+                  
+        report = Reports::MiPlans::DoubleAssignment.get_list_raw
+        assert report, "Could not get report"
+        columns = Reports::MiPlans::DoubleAssignment.get_list_columns
+        assert columns && columns.size > 0, "Could not get columns"
+
+        puts report.to_s
+        
+        assert report.column('Marker Symbol')[0] == 'Trafd1'
+        assert report.column('Marker Symbol')[1] == 'Trafd1'
+        assert report.column('Marker Symbol')[2] == 'Trafd1'
+        assert report.column('Marker Symbol')[3] == 'Trafd1'
+        
+        assert report.column('Consortium')[0] == 'BaSH'
+        assert report.column('Consortium')[1] == 'DTCC'
+        assert report.column('Consortium')[2] == 'BaSH'
+        assert report.column('Consortium')[3] == 'DTCC'
+        
+        assert report.column('Plan Status')[0] == 'Assigned'
+        assert report.column('Plan Status')[1] == 'Assigned'
+        assert report.column('Plan Status')[2] == 'Assigned'
+        assert report.column('Plan Status')[3] == 'Assigned'
+
+        assert report.column('MI Status')[0] == 'Micro-injection in progress'
+        assert report.column('MI Status')[1] == 'Micro-injection in progress'
+        assert report.column('MI Status')[2] == 'Micro-injection in progress'
+        assert report.column('MI Status')[3] == 'Micro-injection in progress'
+        
+        assert report.column('Centre')[0] == 'WTSI'
+        assert report.column('Centre')[1] == 'WTSI'
+        assert report.column('Centre')[2] == 'WTSI'
+        assert report.column('Centre')[3] == 'WTSI'
+
+        assert report.column('MI Date')[0] == '2011-11-05'
+        assert report.column('MI Date')[1] == '2011-10-05'
+        assert report.column('MI Date')[2] == '2011-11-05'
+        assert report.column('MI Date')[3] == '2011-10-05'
 
     end
     

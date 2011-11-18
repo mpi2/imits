58ee057e94320b867e3987a12e5be177a0ab256b
Asfand Qazi <aq2@sanger.ac.uk>
2011-11-18 11:31:07 +0000

    Whitespace

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,21 +15,21 @@ class ReportsController < ApplicationController
         :filename => filename
       )
   end
-  
+
   def double_assigned_plans_matrix
     @report = Reports::MiPlans::DoubleAssignment.get_matrix
     send_data_csv('double_assigned_matrix.csv', @report) if request.format == :csv
   end
-  
+
   def double_assigned_plans_list
     @report = Reports::MiPlans::DoubleAssignment.get_list
     send_data_csv('double_assigned_list.csv', @report) if request.format == :csv
-  end  
+  end
 
   def double_assigned_plans
     @report1 = Reports::MiPlans::DoubleAssignment.get_matrix
     @report2 = Reports::MiPlans::DoubleAssignment.get_list
-  end  
+  end
 
   def genes_list
     @report = Table(
@@ -287,7 +287,6 @@ class ReportsController < ApplicationController
       end
 
       @summary_by_status << totals = ['TOTAL BY STATUS'] + statuses.map { |status| gene_count_by_status[status] || 0 } + [total_number_of_planned_genes]
-
 
       ##
       ## Counts of mi_plans grouped by priority

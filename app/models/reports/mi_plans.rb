53f44931a141eff0b9962bdaa08e87883cef0935
Richard Easty <re4@sanger.ac.uk>
2011-11-23 14:20:56 +0000

    changes for integration test failing

--- app/models/reports/mi_plans.rb
+++ app/models/reports/mi_plans.rb
@@ -185,8 +185,7 @@ class Reports::MiPlans
 
     def self.get_list
       report = get_list_without_grouping
-      report = Grouping( report, :by => 'Target Consortium',    #:order => lambda { |g| g.size } )
-      :order => ['Marker Symbol', 'Consortium', 'Centre'] )
+      report = Grouping( report, :by => 'Target Consortium', :order => ['Marker Symbol', 'Consortium', 'Centre'] )
       return report
     end
 

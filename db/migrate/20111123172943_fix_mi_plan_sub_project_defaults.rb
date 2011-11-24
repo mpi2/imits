e386e40f2d612d72cf854a25c947967da8912117
Richard Easty <re4@sanger.ac.uk>
2011-11-24 10:17:37 +0000

    comment redundant code

--- db/migrate/20111123172943_fix_mi_plan_sub_project_defaults.rb
+++ db/migrate/20111123172943_fix_mi_plan_sub_project_defaults.rb
@@ -1,23 +1,25 @@
 class FixMiPlanSubProjectDefaults < ActiveRecord::Migration
   def self.up
-    sub_project = MiPlan::SubProject.find_by_name!('')
-
-    MiPlan.all.each do |mi_plan|
-      begin
-        mi_plan.sub_project = sub_project        
-        mi_plan.save!
-      rescue Exception => e
-        e2 = RuntimeError.new("(#{e.class.name}): On\n\n#{mi_plan.to_json}\n\n#{e.message}")
-        e2.set_backtrace(e.backtrace)
-        raise e2
-      end
-    end
+    #sub_project = MiPlan::SubProject.find_by_name!('')
+    #
+    #MiPlan.all.each do |mi_plan|
+    #  begin
+    #    mi_plan.sub_project = sub_project        
+    #    mi_plan.save!
+    #  rescue Exception => e
+    #    e2 = RuntimeError.new("(#{e.class.name}): On\n\n#{mi_plan.to_json}\n\n#{e.message}")
+    #    e2.set_backtrace(e.backtrace)
+    #    raise e2
+    #  end
+    #end
     
+#    execute("UPDATE mi_plans SET sub_project_id = mi_plan_sub_projects.id FROM mi_plan_sub_projects WHERE mi_plan_sub_projects.name = ''")
+    execute("UPDATE mi_plans SET sub_project_id = 1")
     execute('alter table mi_plans alter column sub_project_id set not null')
   end
 
   def self.down
-    execute('update mi_plans set sub_project_id = null')
     execute('alter table mi_plans alter column sub_project_id drop not null')
+#    execute('update mi_plans set sub_project_id = null')
   end
 end

cb78dcdd57956d1f0b6f99a40934cf6f24b3d9f6
Richard Easty <re4@sanger.ac.uk>
2011-12-09 15:17:53 +0000

    initial summary & feed version

---
+++ app/models/reports/consortium_priority_summary.rb
@@ -0,0 +1,173 @@
+# encoding: utf-8
+
+class Reports::ConsortiumPrioritySummary
+
+  extend Reports::Helper
+
+  def self.generate1
+    cached_report = get_cached_report
+
+    report_table = Table(
+      [
+        'Consortium',
+        'Priority',
+        'All',
+        'Activity',
+        'Mice in production',
+        'GLT Mice',
+        'order_by'
+      ]
+    )
+
+    #:breakdown => {
+    #    'All' => {'All' => ['Priority']},
+    #    'Activity' => {
+    #        'Assigned - ES Cell QC In Progress' => ['Priority'],
+    #        'Assigned - ES Cell QC Complete' => ['Priority'],
+    #        'Micro-injection in progress' => ['Priority'],
+    #        'Genotype confirmed' => ['Priority']
+    #    },
+    #    'Mice in production' => {
+    #        'Micro-injection in progress' => ['Priority'],
+    #        'Genotype confirmed' => ['Priority']
+    #    },
+    #    'GLT Mice' => {
+    #        'Genotype confirmed' => ['Priority']
+    #    }
+    #}
+    
+    consortia = [
+      'BaSH',
+      'DTCC',
+      'Helmholtz GMC',
+      'JAX',
+      'MARC',
+      'MGP',
+      'Monterotondo',
+      'NorCOMM2',
+      'Phenomin',
+      'RIKEN BRC'
+    ]
+
+    grouped_report = Grouping( cached_report, :by => [ 'Consortium', 'Priority' ] )
+    
+    activity = ['Assigned - ES Cell QC In Progress', 'Assigned - ES Cell QC Complete', 'Micro-injection in progress', 'Genotype confirmed']
+    
+    production = ['Micro-injection in progress', 'Genotype confirmed']
+    
+    glt = ['Genotype confirmed']
+    
+    order_by_map = { 'Low' => 1, 'Medium' => 2, 'High' => 3}
+
+    grouped_report.each do |consortium|
+      next if ! consortia.include? consortium
+      grouped_report.subgrouping(consortium).summary(
+        'Priority',
+        'All'                => lambda { |group| count_unique_instances_of( group, 'Gene' ) },
+        'Activity'           => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| activity.include? row.data['Status'] } ) },
+        'Mice in production' => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| production.include? row.data['Status'] } ) },
+        'GLT Mice'           => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| glt.include? row.data['Status'] } ) }
+      ).each do |row|
+        report_table << {
+          'Consortium' => consortium,
+          'Priority' => row['Priority'],
+          'All' => row['All'],
+          'Activity' => row['Activity'],
+          'Mice in production' => row['Mice in production'],
+          'GLT Mice' => row['GLT Mice'],
+          'order_by' => order_by_map[row['Priority']]
+        }
+      end
+    end
+   
+    report_table.sort_rows_by!( ['Consortium', 'order_by'] )
+    
+    report_table.remove_column('order_by')
+
+    return report_table
+  end
+
+  def self.generate2
+    cached_report = get_cached_report
+
+    report_table = Table(
+      [
+        'Consortium',
+        'Priority',
+        'All',
+        'ES QC started',
+        'ES QC finished',
+        'MI in progress',
+        'GLT Mice',
+        'order_by'
+      ]
+    )
+ 
+    #:breakdown => {
+    #       'All' => {'All'=>['Priority']},
+    #       'ES QC started' => {'Assigned - ES Cell QC In Progress' => ['Priority']},
+    #       'ES QC finished' => {'Assigned - ES Cell QC Complete' => ['Priority']},
+    #       'MI in progress' => {'Micro-injection in progress' => ['Priority']},
+    #       'GLT Mice' => {'Genotype confirmed' => ['Priority']}
+    #   }
+    
+    #consortia = [
+    #'BaSH',
+    #'DTCC',
+    #'Helmholtz GMC',
+    #'JAX',
+    #'MARC',
+    #'MGP',
+    #'Monterotondo',
+    #'NorCOMM2',
+    #'Phenomin',
+    #'RIKEN BRC'
+    #]
+
+    grouped_report = Grouping( cached_report, :by => [ 'Consortium', 'Priority' ] )
+    
+    es_qc_started = ['Assigned - ES Cell QC In Progress']
+    es_qc_finished = ['Assigned - ES Cell QC Complete']
+    mi_in_progress = ['Micro-injection in progress']    
+    glt = ['Genotype confirmed']
+    
+    order_by_map = { 'Low' => 1, 'Medium' => 2, 'High' => 3}
+
+    grouped_report.each do |consortium|
+      #   next if ! consortia.include? consortium
+      grouped_report.subgrouping(consortium).summary(
+        'Priority',
+        'All'                => lambda { |group| count_unique_instances_of( group, 'Gene' ) },
+        'ES QC started'           => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| es_qc_started.include? row.data['Status'] } ) },
+        'ES QC finished' => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| es_qc_finished.include? row.data['Status'] } ) },
+        'MI in progress' => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| mi_in_progress.include? row.data['Status'] } ) },
+        'GLT Mice'           => lambda { |group| count_unique_instances_of( group, 'Gene',
+            lambda { |row| glt.include? row.data['Status'] } ) }
+      ).each do |row|
+        report_table << {
+          'Consortium' => consortium,
+          'Priority' => row['Priority'],
+          'All' => row['All'],
+          'ES QC started' => row['ES QC started'],
+          'ES QC finished' => row['ES QC finished'],
+          'MI in progress' => row['MI in progress'],
+          'GLT Mice' => row['GLT Mice'],
+          'order_by' => order_by_map[row['Priority']]
+        }
+      end
+    end
+  
+    report_table.sort_rows_by!( ['Consortium', 'order_by'] )
+    
+    report_table.remove_column('order_by')
+
+    return report_table
+  end
+
+end

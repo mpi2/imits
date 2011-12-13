d03211b9b65935bd4097b3c543db2dfe2161841f
Asfand Qazi <aq2@sanger.ac.uk>
2011-12-13 17:13:41 +0000

    Finish status changer to support PhenotypeAttempt Cre Excision Complete

--- app/models/phenotype_attempt/status_changer.rb
+++ app/models/phenotype_attempt/status_changer.rb
@@ -7,7 +7,11 @@ module PhenotypeAttempt::StatusChanger
     if rederivation_started?
       if rederivation_complete?
         if number_of_cre_matings_started > 0
-          status_name = 'Cre Excision Started'
+          if number_of_cre_matings_successful > 0
+            status_name = 'Cre Excision Complete'
+          else
+            status_name = 'Cre Excision Started'
+          end
         else
           status_name = 'Rederivation Complete'
         end

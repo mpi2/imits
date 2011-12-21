df25d902f17de24f0aff30954f9fea50152c3274
Asfand Qazi <aq2@sanger.ac.uk>
2011-12-21 15:21:30 +0000

    Re-add detailed production report download page and link

--- test/integration/reports/mi_production_integration_test.rb
+++ test/integration/reports/mi_production_integration_test.rb
@@ -11,25 +11,11 @@ class Reports::MiProductionIntegrationTest < ActionDispatch::IntegrationTest
 
     context '(detailed version)' do
       should 'have link to cached report' do
-        Factory.create :mi_plan
-
-        assert_equal 0, ReportCache.count
-        Reports::MiProduction::Detail.generate_and_cache
-        cache = ReportCache.first
-        assert cache
-
-        cache.update_attributes!(:updated_at => '2011-12-01 23:59:59 UTC')
         visit '/reports'
         click_link 'Detailed Report'
-        assert page.has_css? "a[href='/report_caches/mi_production_detail.csv?20111201235959']"
+        assert page.has_css? "a[href='/reports/mi_production_detail.csv']"
       end
 
-      should 'not have a link when no report is cached' do
-        assert_equal 0, ReportCache.count
-
-        visit '/reports/mi_production'
-        assert page.has_no_content? '/report_caches/mi_production_detail'
-      end
     end
 
   end

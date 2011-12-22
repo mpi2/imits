dbf1e41ddde05be2648b6d6c2f3d41f1cd1e394c
Asfand Qazi <aq2@sanger.ac.uk>
2011-12-22 10:43:41 +0000

    Rename migration

--- db/migrate/20111220165606_add_ikmc_project_id_and_mutation_subtype_to_es_cells.rb
+++ db/migrate/20111220165606_add_ikmc_project_id_and_mutation_subtype_to_es_cells.rb
@@ -1,4 +1,4 @@
-class AddIkmcProjectIdAndMutationTypeToEsCells < ActiveRecord::Migration
+class AddIkmcProjectIdAndMutationSubtypeToEsCells < ActiveRecord::Migration
   def self.up
     add_column :es_cells, :ikmc_project_id, :string, :limit => 100
     add_column :es_cells, :mutation_subtype, :string, :limit => 100

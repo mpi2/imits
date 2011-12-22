326f99b2db5aed3dbc21da295729ff02b7023517
Asfand Qazi <aq2@sanger.ac.uk>
2011-12-22 11:36:19 +0000

    Move links to all mi production summaries to separate page

--- test/integration/reports/mi_production_integration_test.rb
+++ test/integration/reports/mi_production_integration_test.rb
@@ -3,19 +3,27 @@
 require 'test_helper'
 
 class Reports::MiProductionIntegrationTest < ActionDispatch::IntegrationTest
-  context 'MI production report' do
+  context 'MI production reports:' do
 
     setup do
       login
     end
 
-    context '(detailed version)' do
+    context 'detailed MI production report' do
       should 'have link to cached report' do
         visit '/reports'
         click_link 'Detailed Report'
         assert page.has_css? "a[href='/reports/mi_production/detail.csv']"
       end
 
+    end
+
+    context 'summaries page' do
+      should 'work' do
+        visit '/reports/mi_production/summaries'
+        assert page.has_css? 'h2', :text => 'MI Production Summaries'
+        assert page.has_css? 'a'
+      end
     end
 
   end

8ef057ce049b9dadfffe50480270fc966cf45a82
Richard Easty <reasty451-github@yahoo.co.uk>
2012-01-21 16:24:38 +0000

    temp changes

--- app/models/reports/mi_production/summary_komp22.rb
+++ app/models/reports/mi_production/summary_komp22.rb
@@ -124,41 +124,11 @@ class Reports::MiProduction::SummaryKomp22
     return report_title, table
   end
 
-  #MAPPING_SUMMARIES = {
-  #  'All' => [],
-  #  'ES QC started' => ['Assigned - ES Cell QC In Progress'],
-  #  'MI in progress' => ['Micro-injection in progress'],
-  #  'Genotype Confirmed Mice' => ['Genotype confirmed'],
-  #  'MI Aborted' => ['Micro-injection aborted'],
-  #  'ES QC confirmed' => ['Assigned - ES Cell QC Complete'],
-  #  'ES QC failed' => ['Aborted - ES Cell QC Failed'],
-  #  'Registered for Phenotyping' => ['Phenotype Attempt Registered']
-  #}
-
   def self.all(params, row)
-    #overall status + Date
-    # lies within last month
-
     months = params['months'] ? params['months'].to_i : 1
-    
-    status_date = row.data['Overall Status'] + ' Date'   
-    return false if !status_date || status_date.to_s.length < 1
-
-    status_date = row.data[status_date]
-    return false if !status_date || status_date.to_s.length < 1
-
-    #    day = row.data[status_date]
-
-    splits = status_date.to_s.split(/\-/)
-    return false if ! splits
-    #raise splits.inspect
-    day = Time.new(splits[0],splits[1],splits[2]).to_date
-      
-    first_day = Date.today << months
-    first_day = Time.new(first_day.year,first_day.month,1).to_date
-    last_day = (Date.today << 1).end_of_month
-
-    return day && day >= first_day && day <= last_day
+    day = to_date(row.data['Overall Status'] + ' Date')
+    first_day, last_day = get_first_and_last_days_of_month(months)
+    return date_between(day, first_day, last_day)
   end
 
   def self.generic(params, row, key)
@@ -166,82 +136,44 @@ class Reports::MiProduction::SummaryKomp22
     return check_date(params, row, key)
   end
   
-  #MONTHS_BACK = 1
-  
   def self.check_date(params, row, key)
     months = params['months'] ? params['months'].to_i : 1
-    #    first_day = Date.today << MONTHS_BACK
-    first_day = Date.today << months
-    first_day = Time.new(first_day.year,first_day.month,1).to_date
-    last_day = (Date.today << 1).end_of_month
-    #raise last_day.inspect
-    #return false if !MAPPING_SUMMARIES[key].include? row.data['Overall Status']
-    #Assigned - ES Cell QC In Progress Date
+    first_day, last_day = get_first_and_last_days_of_month(months)
+
     array = MAPPING_SUMMARIES[key]
     array.each do |item|
-      item += ' Date'
-      #day = row[item] ?
-      next if ! row[item]
-      splits = row[item].to_s.split(/\-/)
-      next if ! splits || splits.size < 3
-      #raise splits.inspect
-      #raise "SPLITS: " + splits.inspect + " ROW: " + row[item].inspect if splits.size < 3
-      #begin
-      day = Time.new(splits[0],splits[1],splits[2])
-      day = day ? day.to_date : nil
-      next if ! day
-      #rescue
-      #  raise row[item]
-      #end
-      #raise "DAY: " + day.inspect + " ORIGINAL: " + row[item].to_s + " CONVERTED: " + day.inspect.to_s
-      #day << 1
-      return day && day >= first_day && day <= last_day
+      day = to_date(row[item + ' Date'])
+      return true if date_between(day, first_day, last_day)
     end
+
     return false
   end
-  
-  #def self.es_qc_started(row)
-  #  return false if !MAPPING_SUMMARIES['ES QC started'].include? row.data['Overall Status']
-  #  #Assigned - ES Cell QC In Progress Date
-  #  #array = MAPPING_SUMMARIES['ES QC started']
-  #  return check_date(row, 'ES QC started')
-  #end
-  #
-  #def self.es_qc_confirmed(row)
-  #  return false if ! MAPPING_SUMMARIES['ES QC confirmed'].include? row.data['Overall Status']
-  #  return true
-  #end
-  #def self.es_qc_failed(row)
-  #  return false if ! MAPPING_SUMMARIES['ES QC failed'].include? row.data['Overall Status']
-  #  return true
-  #end
-  #def self.mi_in_progress(row)
-  #  return false if ! MAPPING_SUMMARIES['MI in progress'].include? row.data['Overall Status']
-  #  return true
-  #end
-  #def self.genotype_confirmed_mice(row)
-  #  return false if ! MAPPING_SUMMARIES['Genotype Confirmed Mice'].include? row.data['Overall Status']
-  #  return true
-  #end
-  #def self.mi_aborted(row)
-  #  return false if ! MAPPING_SUMMARIES['MI Aborted'].include? row.data['Overall Status']
-  #  return true
-  #end
-  #def self.registered_for_phenotyping(row)
-  #  row && row['PhenotypeAttempt Status'] && row['PhenotypeAttempt Status'].to_s.length > 1
-  #end
-  
-  #def self.get_month(month)
-  #  month = month.to_i
-  #  return "Unknown" if ! month || month < 1 || month > 12
-  #  month = month == 1 ? 12 : month - 1
-  #  return Date::MONTHNAMES[month]
-  #end
 
+  def self.get_first_and_last_days_of_month(month)
+    first_day = Date.today << month
+    first_day = Time.new(first_day.year,first_day.month,1).to_date
+    last_day = (Date.today << month).end_of_month
+    return first_day, last_day
+  end
+  
   def self.get_month(month)
     return "Unknown" if ! month || month < 0
     day = Date.today << month
     return Date::MONTHNAMES[day.month] 
   end
 
+  def self.to_date(string)
+    return false if ! string || string.to_s.length < 1 || ! /-/.match(string)
+    splits = string.to_s.split(/\-/)
+    next if ! splits || splits.size < 3
+    day = Time.new(splits[0],splits[1],splits[2])
+    day = day ? day.to_date : nil
+    return day
+  end
+  
+  def self.date_between(target_date, start_date, end_date)
+    return false if !target_date || !start_date || !end_date
+    return target_date >= start_date && target_date <= end_date
+  end
+  
 end

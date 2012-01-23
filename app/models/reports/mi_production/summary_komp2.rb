62a099de475c6be5a31dd02e52f557d24a29e9d9
Richard Easty <re4@sanger.ac.uk>
2012-01-23 11:50:21 +0000

    tidy; re-order columns

--- app/models/reports/mi_production/summary_komp2.rb
+++ app/models/reports/mi_production/summary_komp2.rb
@@ -19,7 +19,47 @@ class Reports::MiProduction::SummaryKomp2
     end
 
     report = generate_common(request, params)
-    
+
+    #headings = Reports::MiProduction::SummaryKomp2Common::HEADINGS
+
+    #['Consortium', 'Production Centre',
+    #    'All',
+    #    'ES QC started',
+    #    'ES QC confirmed',
+    #    'MI in progress',
+    #    #              'Chimaeras',
+    #    'Genotype Confirmed Mice',
+    #    'Registered for Phenotyping',
+    #    'Phenotyping Started',
+    #    'Rederivation Started',
+    #    'Rederivation Complete',
+    #    'Cre Excision Started',
+    #    'Cre Excision Complete',
+    #    'Phenotyping Complete',
+    #    'ES QC failed',
+    #    'MI Aborted',
+    #    'Phenotype Attempt Aborted'
+    #    #              'Pipeline efficiency (%)',
+    #    #              'Pipeline efficiency (by clone)'
+    #  ]
+
+    reordered = ['Consortium',
+      'All',
+      'ES QC started',
+      'ES QC confirmed',
+      'ES QC failed',
+      'Production Centre',
+      'MI in progress',
+      'MI Aborted',
+      'Genotype Confirmed Mice',
+      'Pipeline efficiency (%)',
+      'Pipeline efficiency (by clone)',
+      'Registered for Phenotyping'
+    ]
+
+    report.remove_columns("Phenotyping Started", "Rederivation Started", "Rederivation Complete", "Cre Excision Started", "Cre Excision Complete", "Phenotyping Complete")
+    report.reorder(reordered)
+  
     return REPORT_TITLE, request && request.format == :csv ? report.to_csv : report.to_html
   end
   

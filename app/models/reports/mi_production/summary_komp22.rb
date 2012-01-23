61ca02a7bb4b25b3df45821766ab2de2d5e26ca7
Richard Easty <re4@sanger.ac.uk>
2012-01-23 14:23:52 +0000

    fix komp22

--- app/models/reports/mi_production/summary_komp22.rb
+++ app/models/reports/mi_production/summary_komp22.rb
@@ -48,16 +48,18 @@ class Reports::MiProduction::SummaryKomp22
 #
 #    report.rename_columns(HEADINGS, headings)
 
+    report.remove_columns('All', 'Pipeline efficiency (%)', 'Pipeline efficiency (by clone)')
+
     return report_title, report.to_html
   end
 
-  def self.all(row)
-#    raise "correct all"
-    months = 1
-    day = to_date(row.data['Overall Status'] + ' Date')
-    first_day, last_day = get_first_and_last_days_of_month(months)
-    return date_between(day, first_day, last_day)
-  end
+#  def self.all(row)
+##    raise "correct all"
+#    months = 1
+#    day = to_date(row.data['Overall Status'] + ' Date')
+#    first_day, last_day = get_first_and_last_days_of_month(months)
+#    return date_between(day, first_day, last_day)
+#  end
 
   def self.generic(row, key)
     return false if !MAPPING_SUMMARIES[key].include? row.data['Overall Status']
@@ -104,76 +106,113 @@ class Reports::MiProduction::SummaryKomp22
     return target_date >= start_date && target_date <= end_date
   end
 
-  def self.es_qc_started(row)
-    return generic(row, 'ES QC started')
-    #    return MAPPING_SUMMARIES['ES QC started'].include? row.data['Overall Status']
-  end
-  
-  def self.es_qc_confirmed(row)
-    return generic(row, 'ES QC confirmed')
-    #    return MAPPING_SUMMARIES['ES QC confirmed'].include? row.data['Overall Status']
-  end
-  
-  def self.es_qc_failed(row)
-    return generic(row, 'ES QC failed')
-    #    return MAPPING_SUMMARIES['ES QC failed'].include? row.data['Overall Status']
-  end
+  #def self.es_qc_started(row)
+  #  return generic(row, 'ES QC started')
+  #  #    return MAPPING_SUMMARIES['ES QC started'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.es_qc_confirmed(row)
+  #  return generic(row, 'ES QC confirmed')
+  #  #    return MAPPING_SUMMARIES['ES QC confirmed'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.es_qc_failed(row)
+  #  return generic(row, 'ES QC failed')
+  #  #    return MAPPING_SUMMARIES['ES QC failed'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.mi_in_progress(row)
+  #  return generic(row, 'MI in progress')
+  #  #    return MAPPING_SUMMARIES['MI in progress'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.genotype_confirmed_mice(row)
+  #  return generic(row, 'Genotype Confirmed Mice')
+  #  #(MAPPING_SUMMARIES['Genotype Confirmed Mice'].include?(row.data['Overall Status'])) ||
+  #  #  ((MAPPING_SUMMARIES['Registered for Phenotyping'].include? row.data['Overall Status']) &&
+  #  #  (row.data['Genotype confirmed Date'] && row.data['Genotype confirmed Date'].to_s.length > 0))
+  #end
+  #
+  #def self.mi_aborted(row)
+  #  return generic(row, 'MI Aborted')
+  #  #    return MAPPING_SUMMARIES['MI Aborted'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.registered_for_phenotyping(row)
+  #  return generic(row, 'Registered for Phenotyping')
+  #  #    row && row['PhenotypeAttempt Status'] && row['PhenotypeAttempt Status'].to_s.length > 1 || MAPPING_SUMMARIES['Registered for Phenotyping'].include?(row.data['Overall Status'])
+  #end
+  #
+  #def self.phenotyping_started(row)
+  #  return generic(row, 'Phenotyping Started')
+  #  #    return MAPPING_SUMMARIES['Phenotyping Started'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.rederivation_started(row)
+  #  return generic(row, 'Rederivation Started')
+  #  #    return MAPPING_SUMMARIES['Rederivation Started'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.rederivation_complete(row)
+  #  return generic(row, 'Rederivation Complete')
+  #  #    return MAPPING_SUMMARIES['Rederivation Complete'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.cre_excision_started(row)
+  #  return generic(row, 'Cre Excision Started')
+  #  #    return MAPPING_SUMMARIES['Cre Excision Started'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.cre_excision_complete(row)
+  #  return generic(row, 'Cre Excision Complete')
+  #  #    return MAPPING_SUMMARIES['Cre Excision Complete'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.phenotyping_complete(row)
+  #  return generic(row, 'Phenotyping Complete')
+  #  #    return MAPPING_SUMMARIES['Phenotyping Complete'].include? row.data['Overall Status']
+  #end
+  #
+  #def self.phenotype_attempt_aborted(row)
+  #  return generic(row, 'Phenotype Attempt Aborted')
+  #  #    return MAPPING_SUMMARIES['Phenotype Attempt Aborted'].include? row.data['Overall Status']
+  #end
 
-  def self.mi_in_progress(row)
-    return generic(row, 'MI in progress')
-    #    return MAPPING_SUMMARIES['MI in progress'].include? row.data['Overall Status']
-  end
+  def self.process_row(row, key)
+    keys2 = [
+      'Phenotype Attempt Aborted',
+      'ES QC started',
+      'ES QC confirmed',
+      'ES QC failed',
+      'MI in progress',
+      'MI Aborted',
+      'Phenotyping Started',
+      'Rederivation Started',
+      'Rederivation Complete',
+      'Cre Excision Started',
+      'Cre Excision Complete',
+      'Phenotyping Complete',
+      'Phenotype Attempt Aborted',
+      'Genotype Confirmed Mice',
+      'Registered for Phenotyping'
+    ]
 
-  def self.genotype_confirmed_mice(row)
-    return generic(row, 'Genotype Confirmed Mice')
-    #(MAPPING_SUMMARIES['Genotype Confirmed Mice'].include?(row.data['Overall Status'])) ||
-    #  ((MAPPING_SUMMARIES['Registered for Phenotyping'].include? row.data['Overall Status']) &&
-    #  (row.data['Genotype confirmed Date'] && row.data['Genotype confirmed Date'].to_s.length > 0))
-  end
+    return generic(row, key) if keys2.include? key
+    
+    if key == 'All'
+      months = 1
+      day = to_date(row.data['Overall Status'] + ' Date')
+      first_day, last_day = get_first_and_last_days_of_month(months)
+      return date_between(day, first_day, last_day)
+    end
+    
+    #return (row && row['PhenotypeAttempt Status'] && row['PhenotypeAttempt Status'].to_s.length > 1 ||
+    #  MAPPING_SUMMARIES['Registered for Phenotyping'].include?(row.data['Overall Status'])) if key == 'Registered for Phenotyping'
+    
+    #if key == 'Registered for Phenotyping'
+    #end
   
-  def self.mi_aborted(row)
-    return generic(row, 'MI Aborted')
-    #    return MAPPING_SUMMARIES['MI Aborted'].include? row.data['Overall Status']
-  end
-
-  def self.registered_for_phenotyping(row)
-    return generic(row, 'Registered for Phenotyping')
-    #    row && row['PhenotypeAttempt Status'] && row['PhenotypeAttempt Status'].to_s.length > 1 || MAPPING_SUMMARIES['Registered for Phenotyping'].include?(row.data['Overall Status'])
-  end
-  
-  def self.phenotyping_started(row)
-    return generic(row, 'Phenotyping Started')
-    #    return MAPPING_SUMMARIES['Phenotyping Started'].include? row.data['Overall Status']
-  end
-  
-  def self.rederivation_started(row)
-    return generic(row, 'Rederivation Started')
-    #    return MAPPING_SUMMARIES['Rederivation Started'].include? row.data['Overall Status']
-  end
-  
-  def self.rederivation_complete(row)
-    return generic(row, 'Rederivation Complete')
-    #    return MAPPING_SUMMARIES['Rederivation Complete'].include? row.data['Overall Status']
-  end
-  
-  def self.cre_excision_started(row)
-    return generic(row, 'Cre Excision Started')
-    #    return MAPPING_SUMMARIES['Cre Excision Started'].include? row.data['Overall Status']
-  end
-  
-  def self.cre_excision_complete(row)
-    return generic(row, 'Cre Excision Complete')
-    #    return MAPPING_SUMMARIES['Cre Excision Complete'].include? row.data['Overall Status']
-  end
-
-  def self.phenotyping_complete(row)
-    return generic(row, 'Phenotyping Complete')
-    #    return MAPPING_SUMMARIES['Phenotyping Complete'].include? row.data['Overall Status']
-  end
-  
-  def self.phenotype_attempt_aborted(row)
-    return generic(row, 'Phenotype Attempt Aborted')
-    #    return MAPPING_SUMMARIES['Phenotype Attempt Aborted'].include? row.data['Overall Status']
+    raise "process_row: invalid key detected '#{key}'"
   end
   
 end

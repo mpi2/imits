c017dea15119417c0ee3629e4786f1ddf13d1091
Richard Easty <re4@sanger.ac.uk>
2012-01-24 10:09:56 +0000

    tidy; fix intermediate test

--- app/models/reports/mi_production/summary_komp2.rb
+++ app/models/reports/mi_production/summary_komp2.rb
@@ -39,8 +39,8 @@ class Reports::MiProduction::SummaryKomp2
 #    report.reorder(reordered)
     report.rename_column('All', 'All Genes')
   
-#    return REPORT_TITLE, request && request.format == :csv ? report.to_csv : report.to_html
-    return REPORT_TITLE, request && request.format == :csv ? report.to_csv : prettify_table(report)
+    return REPORT_TITLE, request && request.format == :csv ? report.to_csv : report.to_html
+#    return REPORT_TITLE, request && request.format == :csv ? report.to_csv : prettify_table(report)
   
   end
   

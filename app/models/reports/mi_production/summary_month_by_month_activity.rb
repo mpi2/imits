112570abb4ea61da9294860a0928dcc5424e553c
Richard Easty <reasty451-github@yahoo.co.uk>
2012-01-29 11:02:23 +0000

    add production centre to table

--- app/models/reports/mi_production/summary_month_by_month_activity.rb
+++ app/models/reports/mi_production/summary_month_by_month_activity.rb
@@ -6,19 +6,19 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
   VERBOSE = false
 
   def self.generate(request = nil, params={})
-    table = params['table'].blank? ? 2 : params['table'].to_i
+    table = params['table'].blank? ? 5 : params['table'].to_i
     tables = generate_original
-#raise table.inspect
+    #raise table.inspect
     return table > -1 && table < tables.size ? tables[table] : nil
   end
 
- #def self.generate(request = nil, params={})
- #   table = params['table'].blank? ? nil : params['table'].to_i
- #   #return generate_plans(request, params)
- #   #return generate_attempts(request, params)
- #   return generate_original
- #   # return nil
- #end
+  #def self.generate(request = nil, params={})
+  #   table = params['table'].blank? ? nil : params['table'].to_i
+  #   #return generate_plans(request, params)
+  #   #return generate_attempts(request, params)
+  #   return generate_original
+  #   # return nil
+  #end
  
   def self.generate_attempts(request = nil, params={})
     
@@ -74,32 +74,33 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
 
   def self.generate_original
     summary = Hash.new{|h,k| h[k]=Hash.new(&h.default_proc) }
-    table = Table(['Year', 'Month', 'Consortium', 'All', 'es_qcs', 'es_confirms', 'es_fails'])
-    table2 = Table(['Year', 'Month', 'Consortium', 'All', 'mis', 'gc', 'abort'])
-    table3 = Table(['Year', 'Month', 'Consortium', 'es_qcs', 'es_confirms', 'es_fails', 'mis', 'gc', 'abort'])
+    table = Table(['Year', 'Month', 'Consortium', 'Production Centre', 'All', 'es_qcs', 'es_confirms', 'es_fails'])
+    table2 = Table(['Year', 'Month', 'Consortium', 'Production Centre', 'All', 'mis', 'gc', 'abort'])
+    table3 = Table(['Year', 'Month', 'Consortium', 'Production Centre', 'es_qcs', 'es_confirms', 'es_fails', 'mis', 'gc', 'abort'])
 
     MiPlan::StatusStamp.all.each do |stamp|
       year = stamp.created_at.year
       month = stamp.created_at.month
       consortium = stamp.mi_plan.consortium.name
+      pcentre = stamp.mi_plan.centre.name
       next unless (consortium == 'BaSH' || consortium == 'DTCC' || consortium == 'JAX')
       gene_id = stamp.mi_plan.gene_id
       status = stamp.status.name
       #puts "#{stamp.id} #{year} #{month} #{status}"
-      summary[year][month][consortium][:all][gene_id] = 1
+      summary[year][month][consortium][pcentre][:all][gene_id] = 1
     
       if(status == 'Assigned - ES Cell QC In Progress')
-        summary[year][month][consortium][:es_qcs][gene_id] = 1
+        summary[year][month][consortium][pcentre][:es_qcs][gene_id] = 1
       end
     
       if(status == 'Assigned - ES Cell QC Complete')
-        summary[year][month][consortium][:es_qcs][gene_id] = 1
-        summary[year][month][consortium][:es_confirms][gene_id] = 1
+        summary[year][month][consortium][pcentre][:es_qcs][gene_id] = 1
+        summary[year][month][consortium][pcentre][:es_confirms][gene_id] = 1
       end
     
       if(status == 'Aborted - ES Cell QC Failed')
-        summary[year][month][consortium][:es_qcs][gene_id] = 1
-        summary[year][month][consortium][:es_fails][gene_id] = 1
+        summary[year][month][consortium][pcentre][:es_qcs][gene_id] = 1
+        summary[year][month][consortium][pcentre][:es_fails][gene_id] = 1
       end
     
       #puts "#{stamp.mi_plan.consortium.name}, #{stamp.status.name}, #{stamp.created_at.month}"
@@ -112,23 +113,24 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
       month = stamp.created_at.month
       plan = stamp.mi_attempt.mi_plan
       consortium = stamp.mi_attempt.mi_plan.consortium.name
+      pcentre = stamp.mi_plan.centre.name
       next unless (consortium == 'BaSH' || consortium == 'DTCC' || consortium == 'JAX')
       gene_id = plan.gene_id
       status = stamp.mi_attempt_status.description
       #puts "#{stamp.id} #{year} #{month} #{status}"
     
       if(status == 'Micro-injection in progress')
-        summary[year][month][consortium][:mi][gene_id] = 1
+        summary[year][month][consortium][pcentre][:mi][gene_id] = 1
       end
     
       if(status == 'Genotype confirmed')
-        summary[year][month][consortium][:mi][gene_id] = 1
-        summary[year][month][consortium][:gc][gene_id] = 1
+        summary[year][month][consortium][pcentre][:mi][gene_id] = 1
+        summary[year][month][consortium][pcentre][:gc][gene_id] = 1
       end
     
       if(status == 'Micro-injection aborted')
-        summary[year][month][consortium][:mi][gene_id] = 1
-        summary[year][month][consortium][:abort][gene_id] = 1
+        summary[year][month][consortium][pcentre][:mi][gene_id] = 1
+        summary[year][month][consortium][pcentre][:abort][gene_id] = 1
       end
     end
     
@@ -142,17 +144,22 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
         puts month if VERBOSE
         cons_hash = month_hash[month]
         cons_hash.keys.sort.each do |cons|
-          status_hash = cons_hash[cons]
-          all = status_hash[:all].keys.size
-          es_qcs = status_hash[:es_qcs].keys.size
-          es_confirms = status_hash[:es_confirms].keys.size
-          es_fails = status_hash[:es_fails].keys.size
-          puts "#{cons},#{all},#{es_qcs},#{es_confirms},#{es_fails}" if VERBOSE
-          table << {
-            'Year' => year,
-            'Month' => Date::MONTHNAMES[month],
-            'Consortium' => cons, 'All' => all, 'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails
-          }
+          centre_hash = month_hash[cons]
+          centre_hash.keys.sort.each do |centre|
+            status_hash = cons_hash[centre]
+            all = status_hash[:all].keys.size
+            es_qcs = status_hash[:es_qcs].keys.size
+            es_confirms = status_hash[:es_confirms].keys.size
+            es_fails = status_hash[:es_fails].keys.size
+            puts "#{cons},#{all},#{es_qcs},#{es_confirms},#{es_fails}" if VERBOSE
+            table << {
+              'Year' => year,
+              'Month' => Date::MONTHNAMES[month],
+              'Consortium' => cons,
+              'Production Centre' => centre,
+              'All' => all, 'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails
+            }
+          end
         end
       end
     end
@@ -167,36 +174,41 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
         puts month if VERBOSE
         cons_hash = month_hash[month]
         cons_hash.keys.sort.each do |cons|
-          status_hash = cons_hash[cons]
-          all = status_hash[:all].keys.size
-          mis = status_hash[:mi].keys.size
-          gc = status_hash[:gc].keys.size
-          abort = status_hash[:abort].keys.size
+          centre_hash = cons_hash[cons]
+          centre_hash.keys.sort.each do |centre|
+            status_hash = cons_hash[centre]
+            all = status_hash[:all].keys.size
+            mis = status_hash[:mi].keys.size
+            gc = status_hash[:gc].keys.size
+            abort = status_hash[:abort].keys.size
 
-          es_qcs = status_hash[:es_qcs].keys.size
-          es_confirms = status_hash[:es_confirms].keys.size
-          es_fails = status_hash[:es_fails].keys.size
+            es_qcs = status_hash[:es_qcs].keys.size
+            es_confirms = status_hash[:es_confirms].keys.size
+            es_fails = status_hash[:es_fails].keys.size
 
-          puts "#{cons},#{all},#{mis},#{gc},#{abort}" if VERBOSE
-          table2 << {
-            'Year' => year,
-            'Month' => Date::MONTHNAMES[month],
-            'Consortium' => cons,
-            'All' => all,
-            'mis' => mis,
-            'gc' => gc,
-            'abort' => abort
-          }
-          table3 << {
-            'Year' => year,
-            'Month' => Date::MONTHNAMES[month],
-            'Consortium' => cons,
-            #'All' => all,
-            'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails,
-            'mis' => mis,
-            'gc' => gc,
-            'abort' => abort
-          }
+            puts "#{cons},#{all},#{mis},#{gc},#{abort}" if VERBOSE
+            table2 << {
+              'Year' => year,
+              'Month' => Date::MONTHNAMES[month],
+              'Consortium' => cons,
+              'Production Centre' => centre,
+              'All' => all,
+              'mis' => mis,
+              'gc' => gc,
+              'abort' => abort
+            }
+            table3 << {
+              'Year' => year,
+              'Month' => Date::MONTHNAMES[month],
+              'Consortium' => cons,
+              'Production Centre' => centre,
+              #'All' => all,
+              'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails,
+              'mis' => mis,
+              'gc' => gc,
+              'abort' => abort
+            }
+          end
         end
       end
     end
@@ -213,21 +225,67 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     
     wrapper = Class.new do
       @table = nil
+      @string = nil
       def to_csv
         raise "Not Yet Implemented!"
       end
       def to_html
         @table.to_html
+        #@string
       end
-      def set_table(new_table)
-        @table = new_table
+      #def set_table(new_table)
+      #  @table = new_table
+      #end
+      def set_html(string)
+        @string = string
       end
     end
     
     proxy = wrapper.new
-    proxy.set_table(table4)
+    proxy.set_html(prettify(table3))
     
     return [grouped_report, grouped_report2, grouped_report3, table4, table3, proxy]
+  end
+  
+  def self.prettify(table)
+    html_array = []
+    grouped_report = Grouping( table, :by => [ 'Year', 'Month', 'Consortium', 'Production Centre' ], :order => :name )
+    
+    html_array.push '<table>'
+    html_array.push '<tr>'
+    table.column_names.each { |name| html_array.push "<th>#{name}</th>" }
+    html_array.push '</tr>'
+    
+    grouped_report.each do |year|
+
+      html_array.push '<tr>'
+      
+      month_group = grouped_report.subgrouping(year)
+      
+      month_group.subgrouping(year).each do |month|
+        
+        consortium_group = month_group.subgrouping(month)
+        
+        consortium_group.subgrouping(month).each do |consortium|
+
+          production_centre_group = month_group.subgrouping(consortium)
+
+          production_centre_group.subgrouping(consortium).each do |production_centre|
+          
+            html_array.push "<td>#{production_centre}</td>"
+          
+            html_array.push '</tr>'
+                    
+          end
+
+        end
+        
+      end
+      
+    end
+    
+    html_array.push '</table>'
+    return table
   end
   
 end

620297cedc2149aa679e9eafdc24e326779462f7
Richard Easty <reasty451-github@yahoo.co.uk>
2012-01-30 13:50:03 +0000

    tidy

--- app/models/reports/mi_production/summary_month_by_month_activity.rb
+++ app/models/reports/mi_production/summary_month_by_month_activity.rb
@@ -140,88 +140,88 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
       end
     end
     
-    summary.keys.each do |year|
-      puts "" if VERBOSE
-      puts "" if VERBOSE
-      puts year
-      month_hash = summary[year]
-      month_hash.keys.each do |month|
-        puts "" if VERBOSE
-        puts month if VERBOSE
-        cons_hash = month_hash[month]
-        cons_hash.keys.each do |cons|
-          centre_hash = cons_hash[cons]
-          centre_hash.keys.each do |centre|
-            next if centre.blank?
-            status_hash = centre_hash[centre]
-            all = status_hash[:all].keys.size
-            es_qcs = status_hash[:es_qcs].keys.size
-            es_confirms = status_hash[:es_confirms].keys.size
-            es_fails = status_hash[:es_fails].keys.size
-            puts "#{cons},#{all},#{es_qcs},#{es_confirms},#{es_fails}" if VERBOSE
-            table << {
-              'Year' => year,
-              #              'Month' => Date::MONTHNAMES[month],
-              'Month' => month,
-              'Consortium' => cons,
-              'Production Centre' => centre,
-              'All' => all, 'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails
-            }
-          end
-        end
-      end
-    end
+    #summary.keys.each do |year|
+    #  puts "" if VERBOSE
+    #  puts "" if VERBOSE
+    #  puts year
+    #  month_hash = summary[year]
+    #  month_hash.keys.each do |month|
+    #    puts "" if VERBOSE
+    #    puts month if VERBOSE
+    #    cons_hash = month_hash[month]
+    #    cons_hash.keys.each do |cons|
+    #      centre_hash = cons_hash[cons]
+    #      centre_hash.keys.each do |centre|
+    #        next if centre.blank?
+    #        status_hash = centre_hash[centre]
+    #        all = status_hash[:all].keys.size
+    #        es_qcs = status_hash[:es_qcs].keys.size
+    #        es_confirms = status_hash[:es_confirms].keys.size
+    #        es_fails = status_hash[:es_fails].keys.size
+    #        puts "#{cons},#{all},#{es_qcs},#{es_confirms},#{es_fails}" if VERBOSE
+    #        table << {
+    #          'Year' => year,
+    #          #              'Month' => Date::MONTHNAMES[month],
+    #          'Month' => month,
+    #          'Consortium' => cons,
+    #          'Production Centre' => centre,
+    #          'All' => all, 'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails
+    #        }
+    #      end
+    #    end
+    #  end
+    #end
     
-    summary.keys.each do |year|
-      puts "" if VERBOSE
-      puts year if VERBOSE
-      month_hash = summary[year]
-      #month_hash.keys.each do |month|
-      month_hash.keys.each do |month|
-        puts "" if VERBOSE
-        puts "" if VERBOSE
-        puts month if VERBOSE
-        cons_hash = month_hash[month]
-        cons_hash.keys.each do |cons|
-          centre_hash = cons_hash[cons]
-          centre_hash.keys.each do |centre|
-            next if centre.blank?
-            status_hash = centre_hash[centre]
-            all = status_hash[:all].keys.size
-            mis = status_hash[:mi].keys.size
-            gc = status_hash[:gc].keys.size
-            abort = status_hash[:abort].keys.size
-
-            es_qcs = status_hash[:es_qcs].keys.size
-            es_confirms = status_hash[:es_confirms].keys.size
-            es_fails = status_hash[:es_fails].keys.size
-
-            puts "#{cons},#{all},#{mis},#{gc},#{abort}" if VERBOSE
-            table2 << {
-              'Year' => year,
-              'Month' => month,
-              'Consortium' => cons,
-              'Production Centre' => centre,
-              'All' => all,
-              'mis' => mis,
-              'gc' => gc,
-              'abort' => abort
-            }
-            table3 << {
-              'Year' => year,
-              'Month' => month,
-              'Consortium' => cons,
-              'Production Centre' => centre,
-              #'All' => all,
-              'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails,
-              'mis' => mis,
-              'gc' => gc,
-              'abort' => abort
-            }
-          end
-        end
-      end
-    end
+    #summary.keys.each do |year|
+    #  puts "" if VERBOSE
+    #  puts year if VERBOSE
+    #  month_hash = summary[year]
+    #  #month_hash.keys.each do |month|
+    #  month_hash.keys.each do |month|
+    #    puts "" if VERBOSE
+    #    puts "" if VERBOSE
+    #    puts month if VERBOSE
+    #    cons_hash = month_hash[month]
+    #    cons_hash.keys.each do |cons|
+    #      centre_hash = cons_hash[cons]
+    #      centre_hash.keys.each do |centre|
+    #        next if centre.blank?
+    #        status_hash = centre_hash[centre]
+    #        all = status_hash[:all].keys.size
+    #        mis = status_hash[:mi].keys.size
+    #        gc = status_hash[:gc].keys.size
+    #        abort = status_hash[:abort].keys.size
+    #
+    #        es_qcs = status_hash[:es_qcs].keys.size
+    #        es_confirms = status_hash[:es_confirms].keys.size
+    #        es_fails = status_hash[:es_fails].keys.size
+    #
+    #        puts "#{cons},#{all},#{mis},#{gc},#{abort}" if VERBOSE
+    #        table2 << {
+    #          'Year' => year,
+    #          'Month' => month,
+    #          'Consortium' => cons,
+    #          'Production Centre' => centre,
+    #          'All' => all,
+    #          'mis' => mis,
+    #          'gc' => gc,
+    #          'abort' => abort
+    #        }
+    #        table3 << {
+    #          'Year' => year,
+    #          'Month' => month,
+    #          'Consortium' => cons,
+    #          'Production Centre' => centre,
+    #          #'All' => all,
+    #          'es_qcs' => es_qcs, 'es_confirms' => es_confirms, 'es_fails' => es_fails,
+    #          'mis' => mis,
+    #          'gc' => gc,
+    #          'abort' => abort
+    #        }
+    #      end
+    #    end
+    #  end
+    #end
 
     grouped_report = Grouping( table, :by => [ 'Year' ], :order => :name )
     grouped_report2 = Grouping( table2, :by => [ 'Year' ], :order => :name )
@@ -263,7 +263,11 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     proxy3.set_table(table)
     proxy3.set_html(prettify2(summary, table))
     
-    return [grouped_report, grouped_report2, grouped_report3, table4, table3, proxy, proxy2, proxy3]
+    proxy4 = wrapper.new
+    proxy4.set_table(table)
+    proxy4.set_html(prettify3(summary))
+    
+    return [grouped_report, grouped_report2, grouped_report3, table4, table3, proxy, proxy2, proxy3, proxy4]
   end
 
   def self.prettify(table)
@@ -465,5 +469,56 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     string += '</table>'
     return string
   end
-  
+
+  def self.prettify3(summary)
+    string = ''
+    string += '<table>'
+    string += '<tr>'
+
+    column_names = ['Year', 'Month', 'Consortium', 'Production Centre', 'es_qcs', 'es_confirms', 'es_fails']
+
+    column_names.each do |name|
+      string += "<th>#{name}</th>"
+    end
+
+    summary.keys.sort.each do |year|
+      string += '</tr>'
+      year_count = 0
+      string += '<tr>'
+      string += "<td rowspan='YEAR_ROWSPAN'>#{year}</td>"
+      month_hash = summary[year]
+      month_hash.keys.sort.each do |month|
+        string += "<td rowspan='MONTH_ROWSPAN'>#{month}</td>"
+        cons_hash = month_hash[month]
+        month_count = 0
+        cons_hash.keys.each do |cons|
+          centre_hash = cons_hash[cons]
+          string += "<td rowspan='CONS_ROWSPAN'>#{cons}</td>"
+          centre_hash.keys.each do |centre|
+            next if centre.blank?
+            status_hash = centre_hash[centre]
+            #all = status_hash[:all].keys.size
+            es_qcs = status_hash[:es_qcs].keys.size
+            es_confirms = status_hash[:es_confirms].keys.size
+            es_fails = status_hash[:es_fails].keys.size
+
+            string += "<td>#{centre}</td>"
+#            string += "<td>#{all}</td>"
+            string += "<td>#{es_qcs}</td>"
+            string += "<td>#{es_confirms}</td>"
+            string += "<td>#{es_fails}</td>"
+            string += "</tr>\n"
+            year_count += 1
+            month_count += 1
+          end
+          string = string.gsub(/CONS_ROWSPAN/, centre_hash.keys.size.to_s)
+        end
+        string = string.gsub(/MONTH_ROWSPAN/, month_count.to_s)
+      end
+      string = string.gsub(/YEAR_ROWSPAN/, year_count.to_s)
+    end
+    string += '</table>'
+    return string
+  end
+    
 end

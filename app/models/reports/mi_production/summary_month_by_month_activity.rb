6e111b5427bbbd73d6dc29b374292463faa58b0d
Richard Easty <reasty451-github@yahoo.co.uk>
2012-01-30 10:36:03 +0000

    tidy

--- app/models/reports/mi_production/summary_month_by_month_activity.rb
+++ app/models/reports/mi_production/summary_month_by_month_activity.rb
@@ -259,7 +259,11 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     proxy2.set_table(table)
     proxy2.set_html(prettify_new(table))
     
-    return [grouped_report, grouped_report2, grouped_report3, table4, table3, proxy, proxy2]
+    proxy3 = wrapper.new
+    proxy3.set_table(table)
+    proxy3.set_html(prettify2(table))    
+    
+    return [grouped_report, grouped_report2, grouped_report3, table4, table3, proxy, proxy2, proxy3]
   end
 
   def self.prettify(table)
@@ -410,6 +414,50 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     	item.each_pair { |k, v| string = string.gsub(k, v.to_s) }
     end
     
+    return string
+  end
+  
+  def self.prettify2(summary)
+    string = ''
+    string += '<table>'
+    string += '<tr>'
+    table.column_names.each do |name|
+      string += "<th>#{name}</th>"
+    end
+    string += '</tr>'
+
+    summary.keys.each do |year|
+      row_count = 0
+      string += '<tr>'
+      string += "<td rowspan='YEAR_ROWSPAN'>#{year}</td>"
+      month_hash = summary[year]
+      month_hash.keys.each do |month|
+        string += "<td rowspan='MONTH_ROWSPAN'>#{month}</td>"
+        cons_hash = month_hash[month]
+        cons_hash.keys.each do |cons|
+          centre_hash = cons_hash[cons]
+          string += "<td rowspan='CONS_ROWSPAN'>#{cons}</td>"
+          centre_hash.keys.each do |centre|
+            next if centre.blank?
+            string += "<td>#{centre}</td>"
+            status_hash = centre_hash[centre]
+            all = status_hash[:all].keys.size
+            es_qcs = status_hash[:es_qcs].keys.size
+            es_confirms = status_hash[:es_confirms].keys.size
+            es_fails = status_hash[:es_fails].keys.size
+            puts "#{cons},#{all},#{es_qcs},#{es_confirms},#{es_fails}" if VERBOSE
+            string += "<td>#{es_confirms}</td>"
+            string += "<td>#{es_fails}</td>"
+            string += '</tr>'
+            row_count += 1
+          end
+          string = string.gsub(/CONS_ROWSPAN/, row_count.to_s)
+        end
+        string = string.gsub(/MONTH_ROWSPAN/, row_count.to_s)
+      end
+      string = string.gsub(/YEAR_ROWSPAN/, row_count.to_s)
+    end
+    string += '</table>'
     return string
   end
   

a7072ad570a54e81c10e7bca66cb796ee09ee0ef
Richard Easty <re4@sanger.ac.uk>
2012-01-30 12:44:25 +0000

    tidy

--- app/models/reports/mi_production/summary_month_by_month_activity.rb
+++ app/models/reports/mi_production/summary_month_by_month_activity.rb
@@ -261,7 +261,7 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     
     proxy3 = wrapper.new
     proxy3.set_table(table)
-    proxy3.set_html(prettify2(table))    
+    proxy3.set_html(prettify2(summary, table))
     
     return [grouped_report, grouped_report2, grouped_report3, table4, table3, proxy, proxy2, proxy3]
   end
@@ -417,43 +417,48 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     return string
   end
   
-  def self.prettify2(summary)
+  def self.prettify2(summary, table)
     string = ''
     string += '<table>'
     string += '<tr>'
     table.column_names.each do |name|
       string += "<th>#{name}</th>"
     end
-    string += '</tr>'
 
-    summary.keys.each do |year|
+    summary.keys.sort.each do |year|
+      string += '</tr>'
       row_count = 0
       string += '<tr>'
       string += "<td rowspan='YEAR_ROWSPAN'>#{year}</td>"
       month_hash = summary[year]
-      month_hash.keys.each do |month|
+      month_hash.keys.sort.each do |month|
         string += "<td rowspan='MONTH_ROWSPAN'>#{month}</td>"
         cons_hash = month_hash[month]
+        month_count = 0
         cons_hash.keys.each do |cons|
           centre_hash = cons_hash[cons]
           string += "<td rowspan='CONS_ROWSPAN'>#{cons}</td>"
           centre_hash.keys.each do |centre|
             next if centre.blank?
-            string += "<td>#{centre}</td>"
             status_hash = centre_hash[centre]
             all = status_hash[:all].keys.size
             es_qcs = status_hash[:es_qcs].keys.size
             es_confirms = status_hash[:es_confirms].keys.size
             es_fails = status_hash[:es_fails].keys.size
             puts "#{cons},#{all},#{es_qcs},#{es_confirms},#{es_fails}" if VERBOSE
+            string += "<td>#{centre}</td>"
+            string += "<td>#{all}</td>"
+            string += "<td>#{es_qcs}</td>"
             string += "<td>#{es_confirms}</td>"
             string += "<td>#{es_fails}</td>"
-            string += '</tr>'
+            string += "</tr>\n"
             row_count += 1
+            month_count += 1
           end
-          string = string.gsub(/CONS_ROWSPAN/, row_count.to_s)
+          string = string.gsub(/CONS_ROWSPAN/, centre_hash.keys.size.to_s)
         end
-        string = string.gsub(/MONTH_ROWSPAN/, row_count.to_s)
+        #string = string.gsub(/MONTH_ROWSPAN/, row_count.to_s)
+        string = string.gsub(/MONTH_ROWSPAN/, month_count.to_s)
       end
       string = string.gsub(/YEAR_ROWSPAN/, row_count.to_s)
     end

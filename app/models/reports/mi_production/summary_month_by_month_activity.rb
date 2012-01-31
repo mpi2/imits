4821dc285593cd57084df8d9fa7585c700c3576f
Richard Easty <re4@sanger.ac.uk>
2012-01-31 18:12:19 +0000

    tidy

--- app/models/reports/mi_production/summary_month_by_month_activity.rb
+++ app/models/reports/mi_production/summary_month_by_month_activity.rb
@@ -30,6 +30,8 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
   HEADINGS_MAP = {}
   HEADINGS.each { |name| HEADINGS_MAP[name] = name }
   HEADINGS_MAP_INVERTED = HEADINGS_MAP.invert
+  HEADINGS_CLEAN = []
+  HEADINGS.each { |name| HEADINGS_CLEAN.push(HEADINGS_MAP[name]) }
 
   def self.generate(request = nil, params={}, consortia = ['BaSH', 'DTCC', 'JAX'])
     summary = Hash.new{|h,k| h[k]=Hash.new(&h.default_proc) }
@@ -92,19 +94,6 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
       end
     end
     
-    #TODO: this need fixing
-    
-    #statuses = [
-    #  'Phenotype Attempt Aborted',
-    #  'Phenotype Attempt Registered',
-    #  'Rederivation Started',
-    #  'Rederivation Complete',
-    #  'Cre Excision Started',
-    #  'Cre Excision Complete',
-    #  'Phenotyping Started',
-    #  'Phenotyping Complete'
-    #]
-
     PhenotypeAttempt::StatusStamp.all.each do |stamp|
 
       next if stamp.created_at < 6.months.ago.to_date
@@ -121,29 +110,6 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
       next if consortia && ! consortia.include?(consortium)
       gene_id = stamp.phenotype_attempt.mi_plan.gene_id
       status = stamp.phenotype_attempt.status.name
-
-      #statuses.each do |name|
-      #  summary[year][month][consortium][pcentre][name][gene_id] = 1 if name == status
-      #end
-
-
-      #
-      #10 Phenotype Aborted Count genes with PhenotypeAttempt Status Phenotype Aborted
-      #11 Phenotype data completes Count genes with PhenotypeAttempt Status Phenotype Complete      
-      #12 Phenotype data starts Count genes with PhenotypeAttempt Status Phenotype Data Started + 11
-      #13 Cre Excision Complete Count genes with Status = Cre Excision Complete + 11 + 12
-      #14 Cre Excision Starts Count status = Cre Excision Started + 13 + 12 + 11
-#      #15 Rederivation Starts Phenotype Attempt Status >= Rederivation Starts AND an actual Rederivation Start date
-      #16 Rederivation Cpmpletes Phenotype Attempt Status >= Rederivation Complete AND an actual Rederivation Complete date
-      #17 Phenotype Registrations Phenotype Attempt Status = Registered + 10 + 11 + 12 + 13 + 14 + 15 + 16
-      #
-
-      #statuses = [
-      #  'Phenotype Attempt Registered',
-      #  '',
-      #  '',
-      #  '',
-      #]
 
     if status == 'Phenotype Attempt Aborted'
       summary[year][month][consortium][pcentre]['Phenotype Attempt Aborted'][gene_id] = 1
@@ -177,9 +143,6 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
       status == 'Rederivation Started' || status == 'Rederivation Complete'
       summary[year][month][consortium][pcentre]['Phenotype Attempt Registered'][gene_id] = 1
     end
-    
-
-
 
     end
 
@@ -188,155 +151,12 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
     return { :csv => table.to_csv, :html => html_string}
   end
 
-  #def self.prettify_old(request, params, summary)
-  #  string = ''
-  #  string += '<table>'
-  #  string += '<tr>'
-  #
-  #  report_table = Table([
-  #      'Year',
-  #      'Month',
-  #      'Consortium',
-  #      'Production Centre', 
-  #      'ES Cell QC In Progress',
-  #      'ES Cell QC Complete',
-  #      'ES Cell QC Failed',
-  #      'Micro-injection in progress',
-  #      'Genotype confirmed',
-  #      'Micro-injection aborted',
-  #      'Phenotype Attempt Registered',
-  #      'Rederivation Started',
-  #      'Rederivation Complete',
-  #      'Cre Excision Started',
-  #      'Cre Excision Complete',
-  #      'Phenotyping Started',
-  #      'Phenotyping Complete',
-  #      'Phenotype Attempt Aborted'
-  #    ])
-  #
-  #  make_clean = lambda do |value|
-  #    return value if request && request.format == :csv
-  #    return '' if value.to_s.length < 1
-  #    return '' if value.to_i == 0
-  #    return value
-  #  end
-  #  
-  #  report_table.column_names.each { |name| string += "<th>#{name}</th>" }
-  #
-  #  summary.keys.sort.reverse!.each do |year|      
-  #    string += '</tr>'
-  #    year_count = 0
-  #    string += '<tr>'
-  #    string += "<td rowspan='YEAR_ROWSPAN'>#{year}</td>"
-  #    month_hash = summary[year]
-  #    month_hash.keys.sort.reverse!.each do |month|
-  #      string += "<td rowspan='MONTH_ROWSPAN'>#{Date::MONTHNAMES[month]}</td>"
-  #      cons_hash = month_hash[month]
-  #      month_count = 0
-  #      cons_hash.keys.each do |cons|
-  #        centre_hash = cons_hash[cons]
-  #        string += "<td rowspan='CONS_ROWSPAN'>#{cons}</td>"
-  #        centre_hash.keys.each do |centre|
-  #          next if centre.blank?
-  #          status_hash = centre_hash[centre]
-  #
-  #          es_qcs = make_clean.call status_hash[:es_qcs].keys.size
-  #          es_confirms = make_clean.call status_hash[:es_confirms].keys.size
-  #          es_fails = make_clean.call status_hash[:es_fails].keys.size
-  #
-  #          mis = make_clean.call status_hash[:mi].keys.size
-  #          gc = make_clean.call status_hash[:gc].keys.size
-  #          abort = make_clean.call status_hash[:abort].keys.size
-  #
-  #          string += "<td>#{centre}</td>"
-  #
-  #          string += "<td>#{es_qcs}</td>"
-  #          string += "<td>#{es_confirms}</td>"
-  #          string += "<td>#{es_fails}</td>"
-  #          
-  #          string += "<td>#{mis}</td>"
-  #          string += "<td>#{gc}</td>"
-  #          string += "<td>#{abort}</td>"
-  #          
-  #          paa = make_clean.call status_hash['Phenotype Attempt Aborted'].keys.size
-  #          par = make_clean.call status_hash['Phenotype Attempt Registered'].keys.size
-  #          rs = make_clean.call status_hash['Rederivation Started'].keys.size
-  #          rc = make_clean.call status_hash['Rederivation Complete'].keys.size
-  #          ces = make_clean.call status_hash['Cre Excision Started'].keys.size
-  #          cec = make_clean.call status_hash['Cre Excision Complete'].keys.size
-  #          ps = make_clean.call status_hash['Phenotyping Started'].keys.size
-  #          pc = make_clean.call status_hash['Phenotyping Complete'].keys.size
-  #
-  #          string += "<td>#{par}</td>"
-  #          string += "<td>#{rs}</td>"
-  #          string += "<td>#{rc}</td>"
-  #          string += "<td>#{ces}</td>"
-  #          string += "<td>#{cec}</td>"
-  #          string += "<td>#{ps}</td>"
-  #          string += "<td>#{pc}</td>"
-  #          string += "<td>#{paa}</td>"
-  #
-  #          string += "</tr>\n"
-  #          year_count += 1
-  #          month_count += 1
-  #
-  #          report_table << {
-  #            'Year' => year,
-  #            'Month' => month,
-  #            'Consortium' => cons,
-  #            'Production Centre' => centre,
-  #            'ES Cell QC In Progress' => es_qcs,
-  #            'ES Cell QC Complete' => es_confirms,
-  #            'ES Cell QC Failed' => es_fails,
-  #            'Micro-injection in progress' => mis,
-  #            'Genotype confirmed' => gc,
-  #            'Micro-injection aborted' => abort,
-  #            'Phenotype Attempt Aborted' => paa,
-  #            'Phenotype Attempt Registered' => par,
-  #            'Rederivation Started' => rs,
-  #            'Rederivation Complete' => rc,
-  #            'Cre Excision Started' => ces,
-  #            'Cre Excision Complete' => cec,
-  #            'Phenotyping Started' => ps,
-  #            'Phenotyping Complete' => pc
-  #          }
-  #
-  #        end
-  #        string = string.gsub(/CONS_ROWSPAN/, centre_hash.keys.size.to_s)
-  #      end
-  #      string = string.gsub(/MONTH_ROWSPAN/, month_count.to_s)
-  #    end
-  #    string = string.gsub(/YEAR_ROWSPAN/, year_count.to_s)
-  #  end
-  #  string += '</table>'
-  #  return report_table, string
-  #end
-
   def self.prettify(request, params, summary)
     string = ''
     string += '<table>'
     string += '<tr>'
 
-    report_table = Table([
-        'Year',
-        'Month',
-        'Consortium',
-        'Production Centre', 
-        'ES Cell QC In Progress',
-        'ES Cell QC Complete',
-        'ES Cell QC Failed',
-        'Micro-injection in progress',
-        'Genotype confirmed',
-        'Micro-injection aborted',
-        'Phenotype Attempt Registered',
-        'Rederivation Started',
-        'Rederivation Complete',
-        'Cre Excision Started',
-        'Cre Excision Complete',
-        'Phenotyping Started',
-        'Phenotyping Complete',
-        'Phenotype Attempt Aborted'
-      ])
+    report_table = Table(HEADINGS_CLEAN)
 
     make_clean = lambda do |value|
       return value if request && request.format == :csv && ! CSV_BLANKS
@@ -402,27 +222,6 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
             year_count += 1
             month_count += 1
 
-            #report_table << {
-            #  'Year' => year,
-            #  'Month' => month,
-            #  'Consortium' => cons,
-            #  'Production Centre' => centre,
-            #  'ES Cell QC In Progress' => status_hash[:es_qcs].keys.size,
-            #  'ES Cell QC Complete' => status_hash[:es_confirms].keys.size,
-            #  'ES Cell QC Failed' => status_hash[:es_fails].keys.size,
-            #  'Micro-injection in progress' => status_hash[:mis].keys.size,
-            #  'Genotype confirmed' => status_hash[:gc].keys.size,
-            #  'Micro-injection aborted' => status_hash[:abort].keys.size,
-            #  'Phenotype Attempt Aborted' => status_hash['Phenotype Attempt Registered'].keys.size,
-            #  'Phenotype Attempt Registered' => status_hash['Phenotype Attempt Registered'].keys.size,
-            #  'Rederivation Started' => status_hash['Rederivation Started'].keys.size,
-            #  'Rederivation Complete' => status_hash['Rederivation Complete'].keys.size,
-            #  'Cre Excision Started' => status_hash['Cre Excision Started'].keys.size,
-            #  'Cre Excision Complete' => status_hash['Cre Excision Complete'].keys.size,
-            #  'Phenotyping Started' => status_hash['Phenotyping Started'].keys.size,
-            #  'Phenotyping Complete' => status_hash['Phenotyping Complete'].keys.size
-            #}
-
             hash = {
               'Year' => year,
               'Month' => month,
@@ -450,15 +249,6 @@ class Reports::MiProduction::SummaryMonthByMonthActivity
             array.each { |name| hash[name] = make_clean.call(status_hash[name].keys.size) }
 
             report_table << hash
-            
-            #'Phenotype Attempt Aborted' => status_hash['Phenotype Attempt Registered'].keys.size,
-            #'Phenotype Attempt Registered' => status_hash['Phenotype Attempt Registered'].keys.size,
-            #'Rederivation Started' => status_hash['Rederivation Started'].keys.size,
-            #'Rederivation Complete' => status_hash['Rederivation Complete'].keys.size,
-            #'Cre Excision Started' => status_hash['Cre Excision Started'].keys.size,
-            #'Cre Excision Complete' => status_hash['Cre Excision Complete'].keys.size,
-            #'Phenotyping Started' => status_hash['Phenotyping Started'].keys.size,
-            #'Phenotyping Complete' => status_hash['Phenotyping Complete'].keys.size
 
           end
           string = string.gsub(/CONS_ROWSPAN/, centre_hash.keys.size.to_s)

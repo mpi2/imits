c7b13fdcf6a05d7a14c8e4573265c9273cdeddd9
Asfand Qazi <aq2@sanger.ac.uk>
2012-02-01 13:56:18 +0000

    Put seeds into fixtures and disable seed loading at beginning of each test

---
+++ script/fill_fixtures_from_db.rb
@@ -0,0 +1,31 @@
+MODELS = [
+  MiAttemptStatus,
+  QcResult,
+  DepositedMaterial,
+  Consortium,
+  Centre,
+  MiPlan::Status,
+  MiPlan::Priority,
+  MiPlan::SubProject,
+  PhenotypeAttempt::Status,
+  Strain,
+  Strain::BlastStrain,
+  Strain::ColonyBackgroundStrain,
+  Strain::TestCrossStrain
+]
+
+MODELS.each do |model|
+  table_name = model.table_name
+
+  data = {}
+  model.all.sort_by(&:id).each {|i| data[i.id] = i.attributes}
+
+  data.each do |fixturename, record|
+    record.delete('created_at')
+    record.delete('updated_at')
+  end
+
+  File.open(Rails.root + "test/fixtures/#{table_name}.yml", 'wb') do |file|
+    file.puts data.to_yaml
+  end
+end

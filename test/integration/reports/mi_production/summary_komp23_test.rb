d40ce9d060e0cec1aaccc5e2553f3862f1559a2d
Richard Easty <re4@sanger.ac.uk>
2012-02-13 14:03:51 +0000

    fix title check

--- test/integration/reports/mi_production/summary_komp23_test.rb
+++ test/integration/reports/mi_production/summary_komp23_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class Reports::MiProduction::SummaryKomp23Test < ActionDispatch::IntegrationTest
-  
+
   DEBUG = false
 
   context 'Reports::MiProduction::SummaryKomp23' do
@@ -11,7 +11,7 @@ class Reports::MiProduction::SummaryKomp23Test < ActionDispatch::IntegrationTest
       visit '/reports/mi_production'
       assert_login_page
     end
-  
+
     context 'once logged in' do
 
     setup do
@@ -20,9 +20,9 @@ class Reports::MiProduction::SummaryKomp23Test < ActionDispatch::IntegrationTest
         :name => 'mi_production_intermediate',
         :csv_data => ProductionSummaryHelper::get_csv('komp2')
       )
-      assert ReportCache.find_by_name('mi_production_intermediate')      
+      assert ReportCache.find_by_name('mi_production_intermediate')
       report = ReportCache.find_by_name!('mi_production_intermediate').to_table
-      
+
       puts 'SETUP:' if DEBUG
       puts report.to_s if DEBUG
       assert report
@@ -35,15 +35,15 @@ class Reports::MiProduction::SummaryKomp23Test < ActionDispatch::IntegrationTest
     should 'allow users to visit the page & see entries' do
       visit '/reports/mi_production/summary_komp23?live=true'
       assert_match '/reports/mi_production/summary_komp23?live=true', current_url
-      
-      assert_match 'KOMP2 Report 3', page.body
+
+      assert_match 'KOMP2 Production Summary', page.body
       assert_match 'Download as CSV', page.body
-      
+
       # save_and_open_page if DEBUG
-      
+
       sleep(10.seconds) if DEBUG
     end
-    
+
     should 'allow users to visit the full page & see entries' do
       #visit '/reports/mi_production/summary_impc23?live=true'
       #assert_match '/reports/mi_production/summary_impc23?live=true', current_url
@@ -57,22 +57,22 @@ class Reports::MiProduction::SummaryKomp23Test < ActionDispatch::IntegrationTest
       #
       #sleep(10.seconds) if DEBUG
     end
-    
+
     should 'allow users to visit the detail page & see entries' do
       visit '/reports/mi_production/summary_komp23?live=true&consortium=BaSH&type=Genotype+Confirmed+Mice'
-      
+
       one = "/reports/mi_production/summary_komp23?live=true&consortium=BaSH&type=Genotype%20Confirmed%20Mice"
       other = "/reports/mi_production/summary_komp23?live=true&consortium=BaSH&type=Genotype+Confirmed+Mice"
       target = /\%20/.match(current_url) ? one : other
       assert_match target, current_url
-      
+
       puts current_url if DEBUG
-      
+
       assert_match 'Production Summary Detail', page.body
       assert_match 'Download as CSV', page.body
-      
+
       # save_and_open_page if DEBUG
-      
+
       sleep(10.seconds) if DEBUG
     end
 

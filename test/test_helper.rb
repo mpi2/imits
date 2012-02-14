236e1e9c27e5d5c4e713c4d133b79d94913c04f3
Asfand Qazi <aq2@sanger.ac.uk>
2012-02-14 12:25:23 +0000

    Remove pointless sleep from run_script in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -237,13 +237,12 @@ class ExternalScriptTestCase < ActiveSupport::TestCase
     error_output = nil
     exit_status = nil
     output = nil
+
     Open3.popen3("cd #{Rails.root}; #{commands}") do |scriptin, scriptout, scripterr, wait_thr|
       error_output = scripterr.read
       exit_status = wait_thr.value.exitstatus
       output = scriptout.read
     end
-
-    sleep 3
 
     assert_blank error_output, "Script has output to STDERR:\n#{error_output}"
     assert_equal 0, exit_status, "Script exited with error code #{exit_status}"

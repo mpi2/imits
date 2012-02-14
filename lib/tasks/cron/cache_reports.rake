7bce30be318cb280f0c8e90237a8b202119de7dc
Asfand Qazi <aq2@sanger.ac.uk>
2012-02-14 13:50:45 +0000

    rake cron:cache_reports caches summary 3 reports

--- lib/tasks/cron/cache_reports.rake
+++ lib/tasks/cron/cache_reports.rake
@@ -2,7 +2,8 @@ desc 'Generate cached reports'
 task 'cron:cache_reports' => [:environment] do
   ApplicationModel.audited_transaction do
     Reports::MiProduction::Intermediate.new.cache
-    #Reports::MiProduction::SummaryKomp23.generate_and_cache
+    Reports::MiProduction::SummaryKomp23.new.cache
+    Reports::MiProduction::SummaryImpc3.new.cache
   end
 end
 
--- test/integration/rake/cron/cache_reports_test.rb
+++ test/integration/rake/cron/cache_reports_test.rb
@@ -15,6 +15,18 @@ module Rake
           assert_equal 2, ReportCache.where(:name => Reports::MiProduction::Intermediate.report_name).count
         end
 
+        should 'cache summary komp23 report' do
+          Factory.create :phenotype_attempt
+          run_script 'rake cron:cache_reports'
+          assert_equal 2, ReportCache.where(:name => Reports::MiProduction::SummaryKomp23.report_name).count
+        end
+
+        should 'cache summary impc3 report' do
+          Factory.create :phenotype_attempt
+          run_script 'rake cron:cache_reports'
+          assert_equal 2, ReportCache.where(:name => Reports::MiProduction::SummaryImpc3.report_name).count
+        end
+
       end
     end
   end

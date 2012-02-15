2f789471a392254aa6d5141de11fd2e71d5af5b3
Asfand Qazi <aq2@sanger.ac.uk>
2012-02-15 17:42:42 +0000

    Fix test

--- test/unit/mi_plan/status_changer_test.rb
+++ test/unit/mi_plan/status_changer_test.rb
@@ -39,7 +39,7 @@ class MiPlan::StatusChangerTest < ActiveSupport::TestCase
     end
 
     should 'not do any status changes if it is currently Inactive' do
-      mi_plan = Factory.create :mi_plan, :status => MiPlan::Status[:Inactive]
+      mi_plan = Factory.create :mi_plan, :is_active => false
       assert_equal 'Inactive', mi_plan.status.name
 
       mi_plan.number_of_es_cells_passing_qc = 6

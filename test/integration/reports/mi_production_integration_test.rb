2a84e7daf84bac134eddbb8cd38ddd9c23e99529
Vivek Iyer <vvi@sanger.ac.uk>
2012-02-22 12:45:46 +0000

    extra tests for remaining mgp reports

--- test/integration/reports/mi_production_integration_test.rb
+++ test/integration/reports/mi_production_integration_test.rb
@@ -28,5 +28,28 @@ class Reports::MiProductionIntegrationTest < ActionDispatch::IntegrationTest
       end
     end
 
+    context '/reports/mi_production/mgp_summary_priority' do
+      should 'have link to cached report' do
+        visit '/reports/mi_production/mgp_summary_priority'
+	assert page.has_css? "#content"
+        assert page.has_css? "a[href='/reports/mi_production/mgp_summary_priority.csv']"
+      end
+    end
+
+    context '/reports/mi_production/languishing_mgp_sub_project' do
+      should 'have link to cached report' do
+        visit '/reports/mi_production/languishing_mgp_sub_project'
+	assert page.has_css? "#content"
+        assert page.has_css? "a[href='/reports/mi_production/languishing_mgp_sub_project.csv']"
+      end
+    end
+
+    context '/reports/mi_production/languishing_mgp_priority' do
+      should 'have link to cached report' do
+        visit '/reports/mi_production/languishing_mgp_priority'
+	assert page.has_css? "#content"
+        assert page.has_css? "a[href='/reports/mi_production/languishing_mgp_priority.csv']"
+      end
+    end
   end
 end

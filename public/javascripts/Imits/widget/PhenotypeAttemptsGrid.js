edaeb59349416b3f88900dcd1d4ac6c7e348dac5
Asfand Qazi <aq2@sanger.ac.uk>
2012-03-01 13:25:21 +0000

    Add separate edit in form link on PA grid

--- public/javascripts/Imits/widget/PhenotypeAttemptsGrid.js
+++ public/javascripts/Imits/widget/PhenotypeAttemptsGrid.js
@@ -48,15 +48,18 @@ Ext.define('Imits.widget.PhenotypeAttemptsGrid', {
         hidden: true
     },
     {
+        header: 'Edit In Form',
+        dataIndex: 'edit_link',
+        renderer: function(value, metaData, record) {
+            var miId = record.getId();
+            return Ext.String.format('<a href="{0}/phenotype_attempts/{1}">Edit in Form</a>', window.basePath, miId);
+        },
+        sortable: false
+    },
+    {
         dataIndex: 'colony_name',
         header: 'Colony Name',
-        readOnly: true,
-        width: 115,
-        renderer: function(value, metaData, record) {
-          var miId = record.getId();
-          var colonyName = record.get('colony_name');
-          return Ext.String.format('<a href="{0}/phenotype_attempts/{1}">{2}</a>', window.basePath, miId, colonyName);
-        },
+        editor: 'textfield',
         filter: {
             type: 'string'
         }

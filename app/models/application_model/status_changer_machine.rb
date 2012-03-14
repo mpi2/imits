40d7a1cf68714f9c0772569a0745221822e5b70a
Asfand Qazi <aq2@sanger.ac.uk>
2012-03-14 16:33:06 +0000

    Factor out status changer into reusable component

---
+++ app/models/application_model/status_changer_machine.rb
@@ -0,0 +1,42 @@
+class ApplicationModel::StatusChangerMachine
+
+  class Item
+    def initialize(required, &conditions)
+      @conditions = conditions
+      @required = required
+    end
+
+    def conditions_met_for?(object)
+      conditions_met = @conditions.call(object)
+      if @required
+        required_conditions_met = @required.conditions_met_for?(object)
+      else
+        required_conditions_met = true
+      end
+      return (conditions_met and required_conditions_met)
+    end
+  end
+
+  def initialize
+    @items = {}
+    yield self
+  end
+
+  def add(status, required = nil, &conditions)
+    raise "Already have #{status}" if @items.has_key?(status)
+    required = @items[required]
+    object = Item.new(required, &conditions)
+    @items[status] = object
+  end
+
+  def get_status_for(object)
+    new_status = nil
+    @items.each do |status, item|
+      if item.conditions_met_for?(object)
+        new_status = status
+      end
+    end
+    return new_status
+  end
+
+end
--- app/models/phenotype_attempt/status_changer.rb
+++ app/models/phenotype_attempt/status_changer.rb
@@ -1,76 +1,41 @@
 # encoding: utf-8
 
 module PhenotypeAttempt::StatusChanger
-  class ConfigItem
-    def initialize(required, &conditions)
-      @conditions = conditions
-      @required = required
+
+  @@status_changer_machine = ApplicationModel::StatusChangerMachine.new do |sc|
+    sc.add('Phenotype Attempt Registered') { |pt| true }
+
+    sc.add('Rederivation Started') do |pt|
+      pt.rederivation_started?
     end
 
-    def conditions_met_for?(object)
-      conditions_met = @conditions.call(object)
-      if @required
-        required_conditions_met = @required.conditions_met_for?(object)
-      else
-        required_conditions_met = true
-      end
-      return (conditions_met and required_conditions_met)
-    end
-  end
-
-  class Config
-    @@items = {}
-
-    def self.add(status, required = nil, &conditions)
-      raise "Already have #{status}" if @@items.has_key?(status)
-      required = @@items[required]
-      object = ConfigItem.new(required, &conditions)
-      @@items[status] = object
+    sc.add('Rederivation Complete', 'Rederivation Started') do |pt|
+      pt.rederivation_complete?
     end
 
-    def self.get_status_for(phenotype_attempt)
-      new_status = nil
-      @@items.each do |status, item|
-        if item.conditions_met_for?(phenotype_attempt)
-          new_status = status
-        end
-      end
-      return new_status
+    sc.add('Cre Excision Started') do |pt|
+      pt.number_of_cre_matings_started > 0
     end
-  end
 
-  Config.add('Phenotype Attempt Registered') { |pt| true }
+    sc.add('Cre Excision Complete', 'Cre Excision Started') do |pt|
+      pt.number_of_cre_matings_successful > 0 and pt.mouse_allele_type == 'b'
+    end
 
-  Config.add('Rederivation Started') do |pt|
-    pt.rederivation_started?
-  end
+    sc.add('Phenotyping Started', 'Cre Excision Complete') do |pt|
+      pt.phenotyping_started?
+    end
 
-  Config.add('Rederivation Complete', 'Rederivation Started') do |pt|
-    pt.rederivation_complete?
-  end
+    sc.add('Phenotyping Complete', 'Phenotyping Started') do |pt|
+      pt.phenotyping_complete?
+    end
 
-  Config.add('Cre Excision Started') do |pt|
-    pt.number_of_cre_matings_started > 0
-  end
-
-  Config.add('Cre Excision Complete', 'Cre Excision Started') do |pt|
-    pt.number_of_cre_matings_successful > 0 and pt.mouse_allele_type == 'b'
-  end
-
-  Config.add('Phenotyping Started', 'Cre Excision Complete') do |pt|
-    pt.phenotyping_started?
-  end
-
-  Config.add('Phenotyping Complete', 'Phenotyping Started') do |pt|
-    pt.phenotyping_complete?
-  end
-
-  Config.add('Phenotype Attempt Aborted') do |pt|
-    ! pt.is_active?
+    sc.add('Phenotype Attempt Aborted') do |pt|
+      ! pt.is_active?
+    end
   end
 
   def change_status
-    self.status = PhenotypeAttempt::Status[Config.get_status_for(self)]
+    self.status = PhenotypeAttempt::Status[@@status_changer_machine.get_status_for(self)]
   end
 
 end

3b16a6f8f4c15da68ca6dea3964bdd63e5d17eb2
Richard Easty <re4@sanger.ac.uk>
2012-03-28 14:14:59 +0100

    initial changes for new model

---
+++ app/models/intermediate_report.rb
@@ -0,0 +1,71 @@
+class IntermediateReport < ActiveRecord::Base
+
+  def self.generate
+    # in txn
+    # delete table
+    # get the report table
+    # iterate over it to save
+
+    IntermediateReport.transaction do
+      IntermediateReport.delete_all
+
+      cached_report = ReportCache.find_by_name_and_format!(Reports::MiProduction::Intermediate.report_name, 'csv').to_table
+
+      #puts "columns: " + cached_report.column_names.inspect
+      #cached_report.each do |row|
+      #  puts row.inspect
+      #  cached_report.column_names.each do |column_name|
+      #    puts "#{column_name}: #{row[column_name]}"
+      #  end
+      #  break
+      #end
+
+      cached_report.each do |row|
+        hash = {}
+        cached_report.column_names.each { |column_name| hash[column_name.parameterize.underscore.to_sym] = row[column_name] }
+        IntermediateReport.create hash
+      end
+    end
+  end
+end
+
+# == Schema Information
+#
+# Table name: intermediate_reports
+#
+#  id                                           :integer         not null, primary key
+#  consortium                                   :string(255)     not null
+#  sub_project                                  :string(255)     not null
+#  priority                                     :string(255)
+#  production_centre                            :string(100)     not null
+#  gene                                         :string(75)      not null
+#  mgi_accession_id                             :string(40)
+#  overall_status                               :string(50)
+#  miplan_status                                :string(50)
+#  miattempt_status                             :string(50)
+#  phenotypeattempt_status                      :string(50)
+#  ikmc_project_id                              :integer
+#  mutation_sub_type                            :string(100)
+#  allele_symbol                                :string(75)      not null
+#  genetic_background                           :string(50)      not null
+#  assigned_date                                :date
+#  assigned_es_cell_qc_in_progress_date         :date
+#  assigned_es_cell_qc_complete_date            :date
+#  micro_injection_in_progress_date             :date
+#  chimeras_obtained_date                       :date
+#  genotype_confirmed_date                      :date
+#  micro_injection_aborted_date                 :date
+#  phenotype_attempt_registered_date            :date
+#  rederivation_started_date                    :date
+#  rederivation_complete_date                   :date
+#  cre_excision_started_date                    :date
+#  cre_excision_complete_date                   :date
+#  phenotyping_started_date                     :date
+#  phenotyping_complete_date                    :date
+#  phenotype_attempt_aborted_date               :date
+#  distinct_genotype_confirmed_es_cells         :integer
+#  distinct_old_non_genotype_confirmed_es_cells :integer
+#  miplan_id                                    :integer
+#  created_at                                   :datetime
+#  updated_at                                   :datetime
+#
---
+++ db/migrate/20120328110402_create_intermediate_reports.rb
@@ -0,0 +1,44 @@
+class CreateIntermediateReports < ActiveRecord::Migration
+  def self.up
+    create_table :intermediate_reports do |t|
+      t.string :consortium, :null => false, :size => 15
+      t.string :sub_project, :null => false
+      t.string :priority, :size => 10
+      t.string :production_centre, :null => false, :limit => 100
+      t.string :gene, :null => false, :limit => 75
+      t.string :mgi_accession_id, :limit => 40
+      t.string :overall_status, :limit => 50
+      t.string :miplan_status, :limit => 50
+      t.string :miattempt_status, :limit => 50
+      t.string :phenotypeattempt_status, :limit => 50
+      t.integer :ikmc_project_id
+      t.string :mutation_sub_type, :limit => 100
+      t.string :allele_symbol, :null => false, :limit => 75
+      t.string :genetic_background, :null => false, :limit => 50
+      t.date :assigned_date
+      t.date :assigned_es_cell_qc_in_progress_date
+      t.date :assigned_es_cell_qc_complete_date
+      t.date :micro_injection_in_progress_date
+      t.date :chimeras_obtained_date
+      t.date :genotype_confirmed_date
+      t.date :micro_injection_aborted_date
+      t.date :phenotype_attempt_registered_date
+      t.date :rederivation_started_date
+      t.date :rederivation_complete_date
+      t.date :cre_excision_started_date
+      t.date :cre_excision_complete_date
+      t.date :phenotyping_started_date
+      t.date :phenotyping_complete_date
+      t.date :phenotype_attempt_aborted_date
+      t.integer :distinct_genotype_confirmed_es_cells
+      t.integer :distinct_old_non_genotype_confirmed_es_cells
+      t.integer :miplan_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :intermediate_reports
+  end
+end

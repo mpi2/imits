00950c08f6f2b0b0a91ff46b70d0ff1b3966d123
Asfand Qazi <aq2@sanger.ac.uk>
2012-04-02 12:26:21 +0100

    Fiddle with contacts and notifications controllers

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.create(params[:contact])
+    @contact = Contact.new(params[:contact])
     @gene = Gene.find(params[:contact][:notification][:gene_id])
 
     if @contact.save
@@ -35,8 +35,8 @@ class ContactsController < ApplicationController
   end
 
   def show
-    @contact = Contact.find_by_id(params[:id])
-    @gene = Gene.find_by_mgi_accession_id(params[:mgi_accession_id])
+    @contact = Contact.find_by_id!(params[:id])
+    @gene = Gene.find_by_mgi_accession_id!(params[:mgi_accession_id])
     respond_with(@contact)
   end
 
@@ -59,21 +59,19 @@ class ContactsController < ApplicationController
         flash[:notice] = "Successfully created contact and registered for email notification."
         redirect_to notification_path(@notification, {:mgi_accession_id => @gene.mgi_accession_id, :email => @contact.email})
       end
+
     end
   end
 
   def search_email
-    @gene = Gene.find_by_mgi_accession_id(params[:mgi_accession_id])
+    @gene = Gene.find_by_mgi_accession_id!(params[:mgi_accession_id])
     @contact = Contact.find_by_email(params[:email]) || Contact.new
     @contact.email = params[:email]
 
     if @contact.new_record?
       render :action => "new"
     else
-      @notification = Notification.new
-      @notification.gene = @gene
-      @notification.contact = @contact
-      redirect_to new_notification_path(@notification, {:mgi_accession_id => @gene.mgi_accession_id, :email => @contact.email})
+      redirect_to new_notification_path(:mgi_accession_id => @gene.mgi_accession_id, :email => @contact.email)
     end
   end
 
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,8 +3,8 @@
 class NotificationsController < ApplicationController
   respond_to :html, :only => [:new, :create, :show, :edit]
   def new
-    @contact = Contact.find_by_email(params[:email])
-    @gene = Gene.find_by_mgi_accession_id(params[:mgi_accession_id])
+    @contact = Contact.find_by_email!(params[:email])
+    @gene = Gene.find_by_mgi_accession_id!(params[:mgi_accession_id])
     notifications = Notification.where(:gene_id => @gene.id, :contact_id => @contact.id)
     if notifications.length > 0
       flash[:notice] = "You have previously registered interest in this gene. Resending notification email."

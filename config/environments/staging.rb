223ffeb3cb2726bf97b117cc4929e9739126d4ad
Garan Jones <gj2@sanger.ac.uk>
2012-04-24 11:31:24 +0100

    changed raise_delivery_errors to true

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -35,7 +35,7 @@ Kermits2::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.delivery_method = :sendmail
 

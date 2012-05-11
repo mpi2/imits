56ebb58cca3dec80d58378038eb4b5f9970f9704
Garan Jones <gj2@sanger.ac.uk>
2012-05-11 16:31:38 +0100

    unit tests for phenotype attempts

--- test/unit/gene_test.rb
+++ test/unit/gene_test.rb
@@ -469,10 +469,48 @@ class GeneTest < ActiveSupport::TestCase
                 :marker_symbol => 'Moo1',
                 :mgi_accession_id => 'MGI:12345'
 
-        2.times do
-          pa = Factory.create :populated_phenotype_attempt
-        end
+        mi = Factory.create :wtsi_mi_attempt_genotype_confirmed,
+                :es_cell => Factory.create(:es_cell, :gene => gene),
+                :consortium_name => 'MGP',
+                :is_active => true
+        pa = Factory.create :populated_phenotype_attempt, :mi_attempt => mi
+        assert_equal MiAttemptStatus.genotype_confirmed.description, mi.status
 
+        mi = Factory.create :wtsi_mi_attempt_genotype_confirmed,
+                :es_cell => Factory.create(:es_cell, :gene => gene),
+                :consortium_name => 'MGP',
+                :is_active => true
+        assert_equal MiAttemptStatus.genotype_confirmed.description, mi.status
+
+        mi = Factory.create :mi_attempt_genotype_confirmed,
+                :es_cell => Factory.create(:es_cell, :gene => gene),
+                :consortium_name => 'DTCC',
+                :production_centre_name => 'UCD',
+                :is_active => true
+        pa = Factory.create :populated_phenotype_attempt, :mi_attempt => mi
+        assert_equal MiAttemptStatus.genotype_confirmed.description, mi.status
+
+        Factory.create :mi_attempt,
+                :es_cell => Factory.create(:es_cell, :gene => gene),
+                :consortium_name => 'MARC',
+                :production_centre_name => 'MARC',
+                :is_active => false
+
+        in_progress_mi = Factory.create :mi_attempt_genotype_confirmed,
+                :es_cell => Factory.create(:es_cell, :gene => gene),
+                :consortium_name => 'EUCOMM-EUMODIC',
+                :production_centre_name => 'WTSI'
+        in_progress_mi.number_of_het_offspring = 0
+        in_progress_mi.total_male_chimeras = 0
+        in_progress_mi.save!
+        assert_equal MiAttemptStatus.micro_injection_in_progress.description, in_progress_mi.status
+
+        assert gene
+        assert_equal 4, gene.mi_plans.count
+        assert_equal 2, gene.phenotype_attempts.count
+        result = gene.pretty_print_phenotype_attempts
+        assert_match '[DTCC:UCD:1]', result
+        assert_match '[MGP:WTSI:1]', result
       end
     end
 

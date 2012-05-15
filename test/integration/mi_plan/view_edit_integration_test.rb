c8f575f318bfed019fffa5bd66387e39be76b95d
Garan Jones <gj2@sanger.ac.uk>
2012-05-15 13:23:05 +0100

    removed assert

--- test/integration/mi_plan/view_edit_integration_test.rb
+++ test/integration/mi_plan/view_edit_integration_test.rb
@@ -81,7 +81,6 @@ class ViewEditIntegrationTest < Kermits2::JsIntegrationTest
         sleep 1
         page.find('div.x-grid-cell-inner').click
         assert page.has_css?('.plan.editor')
-        assert page.has_css?('x-grid-row')
         assert page.find('.plan.editor div#is_bespoke_allele').visible?
       end
 
@@ -101,7 +100,6 @@ class ViewEditIntegrationTest < Kermits2::JsIntegrationTest
         sleep 1
         page.find('div.x-grid-cell-inner').click
         assert page.has_css?('.plan.editor')
-        assert page.has_css?('x-grid-row')
         assert ! page.find('.plan.editor div#is_bespoke_allele').visible?
       end
     end

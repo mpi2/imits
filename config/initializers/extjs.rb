27b9b3af49cbff770131de80bac8f1c01f728f45
Asfand Qazi <aq2@sanger.ac.uk>
2012-05-23 10:56:52 +0100

    Remove complication from extjs.rb

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,6 +1,4 @@
-lambda {
-  processed = ERB.new(File.read(Rails.root + 'config/extjs.yml')).result(binding)
-  EXTJS_CONFIG = YAML.load(processed)
-  EXTJS_CONFIG['css'].map! {|i| EXTJS_CONFIG['base_path'] + '/' + i }
-  EXTJS_CONFIG['javascript'].map! {|i| EXTJS_CONFIG['base_path'] + '/' + i }
-}.call
+processed = ERB.new(File.read(Rails.root + 'config/extjs.yml')).result(binding)
+EXTJS_CONFIG = YAML.load(processed)
+EXTJS_CONFIG['css'].map! {|i| EXTJS_CONFIG['base_path'] + '/' + i }
+EXTJS_CONFIG['javascript'].map! {|i| EXTJS_CONFIG['base_path'] + '/' + i }

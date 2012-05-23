a6335968e0110c5b248914f3fa879bdc0386a4fe
Asfand Qazi <aq2@sanger.ac.uk>
2012-05-23 10:52:47 +0100

    Rename config file to extjs.yml; fix Extjs.ux paths

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,5 +1,5 @@
 lambda {
-  processed = ERB.new(File.read(Rails.root + 'config/extjs.yaml')).result(binding)
+  processed = ERB.new(File.read(Rails.root + 'config/extjs.yml')).result(binding)
   EXTJS_CONFIG = YAML.load(processed)
   EXTJS_CONFIG['css'].map! {|i| EXTJS_CONFIG['base_path'] + '/' + i }
   EXTJS_CONFIG['javascript'].map! {|i| EXTJS_CONFIG['base_path'] + '/' + i }

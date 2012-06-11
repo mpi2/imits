82ff2cd44ef3bc720055265720316bef923da74d
Garan Jones <gj2@sanger.ac.uk>
2012-06-11 15:49:58 +0100

    added a reload cmd to mi_attempt after save

--- app/controllers/mi_attempts_controller.rb
+++ app/controllers/mi_attempts_controller.rb
@@ -79,6 +79,7 @@ class MiAttemptsController < ApplicationController
     return unless authorize_user_production_centre
 
     if @mi_attempt.save
+      @mi_attempt.reload
       flash.now[:notice] = 'MI attempt updated successfully'
     end
 

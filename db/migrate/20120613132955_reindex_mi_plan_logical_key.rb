d27781aeb0a7e7c91856ac8747ddfa9ac14bd92a
Garan Jones <gj2@sanger.ac.uk>
2012-06-18 16:49:27 +0100

    edits

--- db/migrate/20120613132955_reindex_mi_plan_logical_key.rb
+++ db/migrate/20120613132955_reindex_mi_plan_logical_key.rb
@@ -6,5 +6,6 @@ class ReindexMiPlanLogicalKey < ActiveRecord::Migration
 
   def self.down
     remove_index :mi_plans, :name => :mi_plan_logical_key
+    add_index :mi_plans, [:gene_id, :consortium_id, :production_centre_id], :unique => true, :name => :mi_plan_logical_key
   end
 end

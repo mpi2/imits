64fbdec03560e663cbac2ba0abeac378555d3a32
Garan Jones <gj2@sanger.ac.uk>
2012-06-21 16:57:33 +0100

    stash

--- app/controllers/quality_overviews_controller.rb
+++ app/controllers/quality_overviews_controller.rb
@@ -18,9 +18,21 @@ class QualityOverviewsController < ApplicationController
 
   end
 
+  def group_by_consortium_and_centre(quality_overviews)
+    @grouping_consortium_store = Hash.new
+    quality_overviews.each do |quality_overview|
+      if @grouping_consortium_store.include?(quality_overview.mi_plan_consortium)
+
+      else
+        grouping_centre_store = Hash.new
+        grouping_centre_store
+      end
+    end
+  end
+
   def index
     import
-
+    group_by_consortium_and_centre(@quality_overviews)
   end
 
   def show

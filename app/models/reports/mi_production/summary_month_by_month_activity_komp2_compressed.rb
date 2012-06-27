ea1639f73b9abd5eda788e2393f830e741e5129f
Peter Matthews <pm9@sanger.ac.uk>
2012-06-27 13:25:49 +0100

    Data Generated for report

--- app/models/reports/mi_production/summary_month_by_month_activity_komp2_compressed.rb
+++ app/models/reports/mi_production/summary_month_by_month_activity_komp2_compressed.rb
@@ -1,63 +1,70 @@
 # encoding: utf-8
 
-class Reports::MiProduction::SummaryMonthByMonthActivityKomp2Compressed < Reports::MiProduction::SummaryMonthByMonthActivityImpc
+class Reports::MiProduction::SummaryMonthByMonthActivityKomp2Compressed < Reports::Base
 
   CUT_OFF_DATE = Date.parse('2011-06-01')
 
-  def self.report_name; 'summary_month_by_month_activity_komp2'; end
-  def self.report_title; 'KOMP2 Summary Month by Month'; end
+  def self.report_name; 'summary_month_by_month_activity_komp2_compressed'; end
+  def self.report_title; 'KOMP2 Summary Month by Month Compressed'; end
   def self.consortia; ['BaSH', 'DTCC', 'JAX']; end
-  def self.states; [['ES Cell QC In Progress','assigned_es_cell_qc_in_progress_date'],['ES Cell QC Complete', 'assigned_es_cell_qc_complete_date'], ['ES Cell QC Failed'],['Micro-injection in progress'],['Chimeras obtained'],['Genotype confirmed'],['Micro-injection aborted'],['Phenotype Attempt Registered'],['Rederivation Started'],['Rederivation Complete'],['Cre Excision Started'],['Cre Excision Complete'],['Phenotyping Started'],['Phenotyping Complete'],['Phenotype Attempt Aborted']]; end
+  def self.states; [['ES Cell QC In Progress','assigned_es_cell_qc_in_progress_date'],['ES Cell QC Complete', 'assigned_es_cell_qc_complete_date'], ['ES Cell QC Failed', 'updated_at'],['Micro-injection in progress', 'micro_injection_in_progress_date'],['Chimeras obtained', 'chimeras_obtained_date'],['Genotype confirmed', 'genotype_confirmed_date'],['Micro-injection aborted','micro_injection_aborted_date'],['Phenotype Attempt Registered','phenotype_attempt_registered_date'],['Rederivation Started','rederivation_started_date'],['Rederivation Complete', 'rederivation_complete_date'],['Cre Excision Started','cre_excision_started_date'],['Cre Excision Complete','cre_excision_complete_date'],['Phenotyping Started','phenotyping_started_date'],['Phenotyping Complete','phenotyping_complete_date'],['Phenotype Attempt Aborted','phenotype_attempt_aborted_date']]; end
 
   def initialize
-    generated = self.class.generate :komp2 => true
-    @csv = generated[:csv]
-    @html = generated[:html]
+    puts 'hello'
+    generated = self.class.generate #:komp2 => true
+#    @csv = generated[:csv]
+#    @html = generated[:html]
   end
 
-  def generate
+  def self.generate
     summary = Hash.new{|h,k| h[k]=Hash.new(&h.default_proc) }
     time = Time.now
     while time >= CUT_OFF_DATE
       year, month = convert_date(time)
-      self.consortium.each do |consortium|
+      self.consortia.each do |consortium|
         self.states.each do |state|
-          summary [year][month][consortium][state[0]]
+          summary [consortium][year][month][state[0]] = 0
         end
       end
-      time = time.new(year,month -1, 1)
+      time = time.prev_month
     end
 
-    summary = IntermediateReport.all
-    summary.each do |miplanrec|
+    data = IntermediateReport.all
+    data.each do |miplanrec|
+      next if !self.consortia.include?(miplanrec.consortium)
       self.states.each do |state|
-        if miplanrec in self.consortia
-          if
-          miplanrec.mi_plan_status == 'Aborted' and ()
-          miplanrec.mi_attempt_status == 'Aborted' and ()
-          miplanrec.phenotype_attempt_status == 'Aborted' and ()
-
-
-            date = miplanrec[state[1]]
-            if date != nil
-              year, month = convert_date(date)
-              consortium = miplanrec.consortium
-              if summary [year][month][consortium].haskey?(state[0])
-                summary [year][month][consortium][state[0]] += 1
-              else
-              summary [year][month][consortium][state[0]] = 0
-              end
-            end
-          end
-        end
+        date = miplanrec[state[1]]
+        next if date == nil
+        year, month = convert_date(date)
+        consortium = miplanrec.consortium
+        summary [consortium][year][month][state[0]] += 1
       end
+    end
+    return summary
+    #html = self.convert_to_html(summary)
   end
 
-  def convert_date (stamp)
-    year = stamp.created_at.year
-    month = stamp.created_at.month
-    day = stamp.created_at.day
+  def self.convert_date (stamp)
+    year = stamp.year
+    month = stamp.month
+    day = stamp.day
     [year,month,day]
   end
+
+#  def self.convert_to_html(summary)
+#    states = self.states
+#    string = '<table>'
+#    summary.keys & states
+#    summary.each do |key, value|
+
+
+#      if state.include?(key)
+
+#      string += '<tr>'
+
+#      string += '</tr>'
+#    end
+#    string += '</table>'
+#  end
 
 end

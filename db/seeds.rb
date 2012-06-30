6b230fd506c01ea99e13a40cbe164d7eea87e047
Vivek Iyer <vvi@sanger.ac.uk>
2012-06-30 02:52:23 +0100

    Another new strain name

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,7 +76,8 @@ Seeds.load Strain, [
   {:id => 43, :name => 'ICR/Jcl'},
   {:id => 44, :name => 'C57BL/6Dnk'},
   {:id => 45, :name => 'C57BL/6Brd-Tyr<c-Brd>;Stock'},
-  {:id => 46, :name => 'C57BL/6Brd-Tyr<c-Brd>;C57BL/6JIco'}
+  {:id => 46, :name => 'C57BL/6Brd-Tyr<c-Brd>;C57BL/6JIco'},
+  {:id => 47, :name => 'C57BL/6Dnk or C57BL/6NTac'}
 ]
 
 Seeds.load MiAttemptStatus, [

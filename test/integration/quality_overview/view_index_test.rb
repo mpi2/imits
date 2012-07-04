5d9192c20c7d63650108172d3964387ae4006e15
Garan Jones <gj2@sanger.ac.uk>
2012-07-04 12:11:58 +0100

    updated test to reflect ordering

--- test/integration/quality_overview/view_index_test.rb
+++ test/integration/quality_overview/view_index_test.rb
@@ -8,12 +8,7 @@ class QualityOverviewTest < Kermits2::IntegrationTest
 
     context 'once logged in' do
       setup do
-        visit '/users/logout'
-        login
-      end
-
-      should 'allow users to visit the correct page & see entries' do
-
+        #row 1
         gene_tpi1 = Factory.create :gene, :marker_symbol => 'Tpi1', :mgi_accession_id => 'MGI:98797', :ikmc_projects_count => 3, :conditional_es_cells_count => 11,
         :non_conditional_es_cells_count => 8, :deletion_es_cells_count => 6, :other_targeted_mice_count => nil, :other_condtional_mice_count => nil, :mutation_published_as_lethal_count => nil,
         :publications_for_gene_count => nil, :go_annotations_for_gene_count => nil
@@ -30,7 +25,47 @@ class QualityOverviewTest < Kermits2::IntegrationTest
         mi_attempt_tpi1.distribution_centres.push(mi_attempt_distribution_centre)
         mi_attempt_tpi1.save!
 
-        puts mi_attempt_tpi1.pretty_print_distribution_centres == "[EMMA::CNB]"
+        #row 2
+        gene_celsr3 = Factory.create :gene, :marker_symbol => 'Celsr3', :mgi_accession_id => 'MGI:1858236', :ikmc_projects_count => 2, :conditional_es_cells_count => nil,
+        :non_conditional_es_cells_count => nil, :deletion_es_cells_count => 2, :other_targeted_mice_count => nil, :other_condtional_mice_count => nil, :mutation_published_as_lethal_count => nil,
+        :publications_for_gene_count => nil, :go_annotations_for_gene_count => nil
+
+        Factory.create :pipeline, :name => 'KOMP-Regeneron'
+        pipeline = Pipeline.find_by_name! 'KOMP-Regeneron'
+        es_cell_celsr3 = Factory.create :es_cell, :name => '10009A-F9', :allele_symbol_superscript_template => 'tm1(KOMP)Vlcg', :allele_type => nil, :pipeline => pipeline,
+        :gene => gene_celsr3, :parental_cell_line => 'VGB6', :mutation_subtype => 'deletion'
+
+        centre = Centre.find_by_name! 'UCD'
+        mi_attempt_distribution_centre = Factory.create :mi_attempt_distribution_centre, :centre => centre, :is_distributed_by_emma => true
+        mi_attempt_celsr3 = Factory.create :mi_attempt_genotype_confirmed, :es_cell => es_cell_celsr3, :colony_name => 'UCD-10009A-F9-1', :production_centre_name => 'UCD',
+        :number_of_het_offspring => 2
+
+        mi_attempt_celsr3.distribution_centres.push(mi_attempt_distribution_centre)
+        mi_attempt_celsr3.save!
+
+        #row 3
+        gene_lgi2 = Factory.create :gene, :marker_symbol => 'Lgi2', :mgi_accession_id => 'MGI:2180196', :ikmc_projects_count => 2, :conditional_es_cells_count => 12,
+        :non_conditional_es_cells_count => 4, :deletion_es_cells_count => 5, :other_targeted_mice_count => nil, :other_condtional_mice_count => nil, :mutation_published_as_lethal_count => nil,
+        :publications_for_gene_count => nil, :go_annotations_for_gene_count => nil
+
+        pipeline = Pipeline.find_by_name! 'KOMP-Regeneron'
+        es_cell_lgi2 = Factory.create :es_cell, :name => '10011B-G3', :allele_symbol_superscript_template => 'tm1(KOMP)Vlcg', :allele_type => nil, :pipeline => pipeline,
+        :gene => gene_lgi2, :parental_cell_line => 'VGB6', :mutation_subtype => 'deletion'
+
+        centre = Centre.find_by_name! 'UCD'
+        mi_attempt_distribution_centre = Factory.create :mi_attempt_distribution_centre, :centre => centre, :is_distributed_by_emma => true
+        mi_attempt_lgi2 = Factory.create :mi_attempt_genotype_confirmed, :es_cell => es_cell_lgi2, :colony_name => 'BL1071', :production_centre_name => 'UCD',
+        :number_of_het_offspring => 2
+
+        mi_attempt_lgi2.distribution_centres.push(mi_attempt_distribution_centre)
+        mi_attempt_lgi2.save!
+
+
+        visit '/users/logout'
+        login
+      end
+
+      should 'allow users to visit the correct page & see entries' do
 
         visit '/quality_overviews'
         assert_match '/quality_overviews', current_url
@@ -54,20 +89,20 @@ class QualityOverviewTest < Kermits2::IntegrationTest
         assert page.has_css?('div.quality-overviews tr:nth-child(1) th:nth-child(15)', :text => 'Mouse-qc')
 
         assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(1)', :text => 'EUCOMM-EUMODIC')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(2)', :text => 'WTSI')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(2)', :text => 'UCD')
         assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(3)')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(4)', :text => 'Tpi1')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(5)', :text => 'EPD0183_4_A09')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(6)', :text => 'MCFC')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(4)', :text => 'Celsr3')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(5)', :text => '10009A-F9')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(6)', :text => 'UCD-10009A-F9-1')
         assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(7)', :text => 'Genotype confirmed')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(8)', :text => 'true')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(9)', :text => '1')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(10)', :text => '1')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(11)', :text => '1')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(12)', :text => '1')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(8)', :text => 'false')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(9)', :text => '')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(10)', :text => '')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(11)', :text => '')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(12)', :text => '')
         assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(13)', :text => '')
         assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(14)', :text => '')
-        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(15)', :text => 'qc_lacz_sr_pcr qc_loa_qpcr qc_loxp_confirmation qc_mutant_specific_sr_pcr qc_neo_count_qpcr qc_tv_backbone_assay')
+        assert page.has_css?('div.quality-overviews tr:nth-child(2) td:nth-child(15)', :text => '')
 
       end
 

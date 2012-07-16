d206ac9305f10c32f7e6a9018075d51b1b92fd9e
Peter Matthews <pm9@sanger.ac.uk>
2012-07-16 14:19:31 +0100

    corrected spacing to look nice.

--- script/scratch/jax_make_public.rb
+++ script/scratch/jax_make_public.rb
@@ -2,23 +2,24 @@
 
 ApplicationModel.audited_transaction do
 
-puts 'Non public Jax records'
-a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 'f'})
-puts a.count
-puts 'Public Jax records'
-a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 't'})
-puts a.count
-puts 'Total Jax records'
-a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'})
-puts a.count
-a.each {|rec| MiAttempt.find(rec.id).update_attributes!(:report_to_public => 't', :audit_comment => "jax_make_public.rb")}
+  puts 'Non public Jax records'
+  a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 'f'})
+  puts a.count
+  puts 'Public Jax records'
+  a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 't'})
+  puts a.count
+  puts 'Total Jax records'
 
-puts 'No. non public Jax records after update'
-a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 'f'})
-puts a.count
-puts 'No. public Jax records after update'
-a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 't'})
-puts a.count
+  a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'})
+  puts a.count
+  a.each {|rec| MiAttempt.find(rec.id).update_attributes!(:report_to_public => 't', :audit_comment => "jax_make_public.rb")}
 
-#raise ActiveRecord::Rollback
+  puts 'No. non public Jax records after update'
+  a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 'f'})
+  puts a.count
+  puts 'No. public Jax records after update'
+  a = MiAttempt.joins(:mi_plan => :consortium).where(:consortia => {:name => 'JAX'}, :mi_attempts => {:report_to_public => 't'})
+  puts a.count
+
+  #raise ActiveRecord::Rollback
 end

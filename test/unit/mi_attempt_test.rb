c19f888d853853310bca261a5b9295da3815fc14
Peter Matthews <pm9@sanger.ac.uk>
2012-07-19 13:08:09 +0100

    written test

--- test/unit/mi_attempt_test.rb
+++ test/unit/mi_attempt_test.rb
@@ -70,6 +70,15 @@ class MiAttemptTest < ActiveSupport::TestCase
           default_mi_attempt.valid?
           assert_match(/cannot be changed/i, default_mi_attempt.errors[:mi_attempt_status].first)
         end
+
+        should 'count the number of phenotype_attempts' do
+          set_mi_attempt_genotype_confirmed(default_mi_attempt)
+          Factory.create :phenotype_attempt, :mi_attempt => default_mi_attempt
+          default_mi_attempt.reload
+          default_mi_attempt.is_active = false
+          count = default_mi_attempt.phenotype_count
+          assert_match(count, 1)
+        end
       end
 
       context '#status_stamps' do

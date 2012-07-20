76e197210139b127ef626d05cf746dd1ad60bb9f
Garan Jones <gj2@sanger.ac.uk>
2012-07-20 16:09:04 +0100

    draft version

--- app/models/phenotype_attempt/distribution_centre.rb
+++ app/models/phenotype_attempt/distribution_centre.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
-class MiAttempt::DistributionCentre < ActiveRecord::Base
+class PhenotypeAttempt::DistributionCentre < ActiveRecord::Base
   extend AccessAssociationByAttribute
 
-  belongs_to :mi_attempt
+  belongs_to :phenotype_attempt
   belongs_to :centre
   belongs_to :deposited_material
 
-  validates :mi_attempt_id, :presence => true
+  validates :phenotype_attempt_id, :presence => true
   validates :centre_id, :presence => true
   validates :deposited_material_id, :presence => true
 
@@ -34,16 +34,15 @@ end
 
 # == Schema Information
 #
-# Table name: mi_attempt_distribution_centres
+# Table name: phenotype_attempt_distribution_centres
 #
 #  id                     :integer         not null, primary key
 #  start_date             :date
 #  end_date               :date
-#  mi_attempt_id          :integer         not null
+#  phenotype_attempt_id   :integer         not null
 #  deposited_material_id  :integer         not null
 #  centre_id              :integer         not null
 #  is_distributed_by_emma :boolean         default(FALSE), not null
-#  boolean                :boolean         default(FALSE), not null
 #  created_at             :datetime
 #  updated_at             :datetime
 #

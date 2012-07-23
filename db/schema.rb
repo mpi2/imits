04579d2933f50c7eeedb85e81c46004a83ff30e0
Garan Jones <gj2@sanger.ac.uk>
2012-07-23 13:19:03 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716095723) do
+ActiveRecord::Schema.define(:version => 20120721093257) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
@@ -279,6 +279,11 @@ ActiveRecord::Schema.define(:version => 20120716095723) do
     t.integer  "sub_project_id",                                    :null => false
     t.boolean  "is_active",                      :default => true,  :null => false
     t.boolean  "is_bespoke_allele",              :default => false, :null => false
+    t.boolean  "is_conditional_allele",          :default => false, :null => false
+    t.boolean  "is_deletion_allele",             :default => false, :null => false
+    t.boolean  "is_cre_knock_in_allele",         :default => false, :null => false
+    t.boolean  "is_cre_bac_allele",              :default => false, :null => false
+    t.text     "comment"
   end
 
   add_index "mi_plans", ["gene_id", "consortium_id", "production_centre_id", "sub_project_id"], :name => "mi_plan_logical_key", :unique => true

c0d3520b19ffcfe881a9a99311c43875401bb555
Asfand Qazi <aq2@sanger.ac.uk>
2012-08-08 10:16:40 +0100

    Make script/scratch/further_tidy_status_stamps.rb deal with MiPlan "save others" nastiness

--- script/scratch/further_tidy_status_stamps.rb
+++ script/scratch/further_tidy_status_stamps.rb
@@ -1,16 +1,24 @@
 #!/usr/bin/env ruby
 
 ApplicationModel.audited_transaction do
-  [MiPlan, MiAttempt, PhenotypeAttempt].each do |model_class|
-    model_class.all.each do |obj|
-      ss_before = obj.status_stamps.map {|ss| ss.code + '[' + ss.created_at.strftime('%F') + ']'}.join(', ')
-      obj.save!
-      ss_after = obj.status_stamps.map {|ss| ss.code + '[' + ss.created_at.strftime('%F') + ']'}.join(', ')
+  all_before = {}
 
-      if ss_before != ss_after
+  [MiPlan, MiAttempt, PhenotypeAttempt].each do |model_class|
+    before = all_before[model_class] = {}
+    model_class.all.each do |obj|
+      before[obj.id] = obj.status_stamps.map {|ss| ss.code + '[' + ss.created_at.strftime('%F') + ']'}.join(', ')
+    end
+  end
+
+  [MiPlan, MiAttempt, PhenotypeAttempt].each do |model_class|
+    before = all_before[model_class]
+    model_class.all.each do |obj|
+      obj.save!
+      after = obj.status_stamps.map {|ss| ss.code + '[' + ss.created_at.strftime('%F') + ']'}.join(', ')
+      if before[obj.id] != after
         puts "#{obj.class.name}(#{obj.id}):"
         puts "  Status: #{obj.status.code}"
-        puts "  New stamps: #{ss_after}"
+        puts "  New stamps: #{after}"
       end
     end
     puts

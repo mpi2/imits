18cd62a47f8b6a908c87d3ee35403a89a03e1064
Asfand Qazi <aq2@sanger.ac.uk>
2012-08-28 11:23:56 +0100

    Fix some more tests - create missing fixture files for some tables

--- test/unit/centre_test.rb
+++ test/unit/centre_test.rb
@@ -4,16 +4,15 @@ require 'test_helper'
 
 class CentreTest < ActiveSupport::TestCase
   context 'Centre' do
-    context '(misc. tests)' do
-      setup do
-        Factory.create :centre
-      end
 
-      should have_db_column(:name).of_type(:string).with_options(:null => false, :limit => 100)
-      should have_db_index(:name).unique(true)
-      should validate_presence_of :name
-      should validate_uniqueness_of :name
+    setup do
+      Factory.create :centre
     end
+
+    should have_db_column(:name).of_type(:string).with_options(:null => false, :limit => 100)
+    should have_db_index(:name).unique(true)
+    should validate_presence_of :name
+    should validate_uniqueness_of :name
 
     should 'order by name by default' do
       Centre.destroy_all
@@ -24,5 +23,6 @@ class CentreTest < ActiveSupport::TestCase
 
       assert_equal ['AA1', 'AA9', 'ZZ1', 'ZZ2'], Centre.all.map(&:name)
     end
+
   end
 end
--- test/unit/test_helper_test.rb
+++ test/unit/test_helper_test.rb
@@ -26,6 +26,7 @@ class TestHelperTest < ActiveSupport::TestCase
         mi = Factory.create :mi_attempt_genotype_confirmed
         expected = [
           ['Micro-injection in progress', '2010-03-11 07:12:03 UTC'],
+          ['Chimeras obtained', '2010-03-12 07:43:03 UTC'],
           ['Genotype confirmed', '2012-05-13 05:04:01 UTC']
         ]
         replace_status_stamps(mi, expected)
@@ -35,10 +36,8 @@ class TestHelperTest < ActiveSupport::TestCase
       end
 
       should 'work for other' do
-        plan = Factory.create :mi_plan, :status => MiPlan::Status['Assigned']
+        plan = Factory.create :mi_plan
         expected = [
-          ['Interest', '2011-03-11 01:01:01 UTC'],
-          ['Conflict', '2011-04-12 02:02:02 UTC'],
           ['Assigned', '2011-05-13 03:03:03 UTC']
         ]
         replace_status_stamps(plan, expected)
@@ -48,15 +47,13 @@ class TestHelperTest < ActiveSupport::TestCase
       end
 
       should 'take only dates for status stamps' do
-        plan = Factory.create :mi_plan, :status => MiPlan::Status['Withdrawn']
+        plan = Factory.create :mi_plan, :number_of_es_cells_starting_qc => 1
         replace_status_stamps(plan,
-          'Interest' => '2011-01-13',
-          'Assigned' => '2011-05-13',
-          'Withdrawn' => '2011-08-13')
+           'Assigned' => '2011-05-13',
+           'Assigned - ES Cell QC In Progress' => '2011-08-13')
         expected = [
-          ['Interest', '2011-01-13 00:00:00 UTC'],
           ['Assigned', '2011-05-13 00:00:00 UTC'],
-          ['Withdrawn', '2011-08-13 00:00:00 UTC']
+          ['Assigned - ES Cell QC In Progress', '2011-08-13 00:00:00 UTC']
         ]
         got = plan.status_stamps.map {|i| [i.name, i.created_at.to_s]}
         assert_equal expected, got

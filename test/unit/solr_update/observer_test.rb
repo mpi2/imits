6f0452345d4b2105d758022a1e3c9f693d92f9c0
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-26 09:46:57 +0100

    Whitespace

--- test/unit/solr_update/observer_test.rb
+++ test/unit/solr_update/observer_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class SolrUpdate::ObserverTest < ActiveSupport::TestCase
-  context 'SolrUpdate::Observer::MiAttempt' do
 
+  context 'SolrUpdate::Observer::MiAttempt' do
     should 'enqueue update when an MiAttempt is changed' do
       mi = stub('mi_attempt', :id => 55)
       SolrUpdate::Queue.expects(:enqueue_for_update).with(mi)

34bcad8d7663232ef00825e6ad6423c4bdb41e22
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-26 13:38:49 +0100

    Test a few more attributes of SolrUpdate::DocFactory

--- app/models/solr_update/doc_factory.rb
+++ app/models/solr_update/doc_factory.rb
@@ -2,9 +2,22 @@ class SolrUpdate::DocFactory
   def self.create_for_mi_attempt(mi_attempt)
     solr_doc = {
       'id' => mi_attempt.id,
-      'type' => 'mi_attempt',
-      'mgi_accession_id' => mi_attempt.gene.mgi_accession_id
+      'type' => 'mi_attempt'
     }
+
+    if mi_attempt.gene.mgi_accession_id
+      solr_doc['mgi_accession_id'] = mi_attempt.gene.mgi_accession_id
+    end
+
+    if mi_attempt.es_cell.mutation_subtype
+      solr_doc['allele_type'] = mi_attempt.es_cell.mutation_subtype.titleize
+    end
+
+    if mi_attempt.colony_background_strain
+      solr_doc['strain'] = mi_attempt.colony_background_strain.name
+    end
+
+    solr_doc['allele_name'] = mi_attempt.allele_symbol
 
     return solr_doc
   end
--- test/unit/solr_update/doc_factory_test.rb
+++ test/unit/solr_update/doc_factory_test.rb
@@ -4,26 +4,50 @@ class SolrUpdate::DocFactoryTest < ActiveSupport::TestCase
   context 'SolrUpdate::DocFactory' do
 
     context 'when creating solr docs for mi_attempt' do
-      def setup_mi_attempt_and_doc
+
+      setup do
+        @es_cell = Factory.create :es_cell,
+                :gene => cbx1,
+                :mutation_subtype => 'conditional_ready'
         @mi_attempt = Factory.create :mi_attempt, :id => 43,
-                :es_cell => Factory.create(:es_cell, :gene => cbx1)
+                :colony_background_strain => Strain.first,
+                :es_cell => @es_cell
 
         @doc = SolrUpdate::DocFactory.create_for_mi_attempt(@mi_attempt)
       end
 
       should 'set id and type' do
-        setup_mi_attempt_and_doc
         assert_equal ['mi_attempt', 43], @doc.values_at('type', 'id')
       end
 
       should 'set mgi_accession_id' do
-        setup_mi_attempt_and_doc
         assert_equal cbx1.mgi_accession_id, @doc['mgi_accession_id']
       end
 
-      should 'set allele_type based' do
-        flunk
+      should 'set allele_type' do
+        @es_cell.mutation_subtype = 'conditional_ready'
+        doc = SolrUpdate::DocFactory.create_for_mi_attempt(@mi_attempt)
+        assert_equal 'Conditional Ready', doc['allele_type']
+
+        @es_cell.mutation_subtype = 'deletion'
+        doc = SolrUpdate::DocFactory.create_for_mi_attempt(@mi_attempt)
+        assert_equal 'Deletion', doc['allele_type']
+
+        flunk "IT'S MORE COMPLICATED THAN THAT!"
       end
+
+      should 'set strain of origin' do
+        @mi_attempt.colony_background_strain = Strain.create!(:name => 'TEST STRAIN')
+        doc = SolrUpdate::DocFactory.create_for_mi_attempt @mi_attempt
+        assert_equal 'TEST STRAIN', doc['strain']
+      end
+
+      should 'set mgi_allele_name' do
+        @mi_attempt.expects(:allele_symbol).returns('TEST ALLELE SYMBOL')
+        doc = SolrUpdate::DocFactory.create_for_mi_attempt @mi_attempt
+        assert_equal 'TEST ALLELE SYMBOL', doc['allele_name']
+      end
+
     end
 
   end

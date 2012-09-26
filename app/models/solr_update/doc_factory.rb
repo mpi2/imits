f08b7759785e9073fb57d6e4d906f5ad083c70bf
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-26 11:25:34 +0100

    Beginnings of a solr doc factory

--- app/models/solr_update/doc_factory.rb
+++ app/models/solr_update/doc_factory.rb
@@ -1,5 +1,11 @@
 class SolrUpdate::DocFactory
   def self.create_for_mi_attempt(mi_attempt)
-    raise 'TODO'
+    solr_doc = {
+      'id' => mi_attempt.id,
+      'type' => 'mi_attempt',
+      'mgi_accession_id' => mi_attempt.gene.mgi_accession_id
+    }
+
+    return solr_doc
   end
 end
--- test/unit/solr_update/doc_factory_test.rb
+++ test/unit/solr_update/doc_factory_test.rb
@@ -4,12 +4,25 @@ class SolrUpdate::DocFactoryTest < ActiveSupport::TestCase
   context 'SolrUpdate::DocFactory' do
 
     context 'when creating solr docs for mi_attempt' do
-      should 'set id and type' do
-        @mi_attempt = Factory.build :mi_attempt
-        @mi_attempt.stubs(:id => 43)
+      def setup_mi_attempt_and_doc
+        @mi_attempt = Factory.create :mi_attempt, :id => 43,
+                :es_cell => Factory.create(:es_cell, :gene => cbx1)
 
         @doc = SolrUpdate::DocFactory.create_for_mi_attempt(@mi_attempt)
+      end
+
+      should 'set id and type' do
+        setup_mi_attempt_and_doc
         assert_equal ['mi_attempt', 43], @doc.values_at('type', 'id')
+      end
+
+      should 'set mgi_accession_id' do
+        setup_mi_attempt_and_doc
+        assert_equal cbx1.mgi_accession_id, @doc['mgi_accession_id']
+      end
+
+      should 'set allele_type based' do
+        flunk
       end
     end
 

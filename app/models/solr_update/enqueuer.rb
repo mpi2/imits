c76fd998bbaffd335c4c8c6976a55685ba55c89a
Asfand Qazi <aq2@sanger.ac.uk>
2012-10-04 18:02:01 +0100

    Make SolrUpdate::Enqueuer implemented for PAs and MIs

--- app/models/solr_update/enqueuer.rb
+++ app/models/solr_update/enqueuer.rb
@@ -1,2 +1,36 @@
 class SolrUpdate::Enqueuer
+  def mi_attempt_updated(mi)
+    return if mi.gene.mgi_accession_id.nil?
+
+    reference = {'type' => 'mi_attempt', 'id' => mi.id}
+
+    if mi.has_status? :gtc.status
+      SolrUpdate::Queue.enqueue_for_update(reference)
+    else
+      SolrUpdate::Queue.enqueue_for_delete(reference)
+    end
+
+    mi.phenotype_attempts.each do |pa|
+      self.phenotype_attempt_updated(pa)
+    end
+
+  end
+
+  def mi_attempt_destroyed(mi)
+    SolrUpdate::Queue.enqueue_for_delete({'type' => 'mi_attempt', 'id' => mi.id})
+  end
+
+  def phenotype_attempt_updated(pa)
+    reference = {'type' => 'phenotype_attempt', 'id' => pa.id}
+
+    if pa.has_status? :cec.status
+      SolrUpdate::Queue.enqueue_for_update(reference)
+    else
+      SolrUpdate::Queue.enqueue_for_delete(reference)
+    end
+  end
+
+  def phenotype_attempt_destroyed(pa)
+    SolrUpdate::Queue.enqueue_for_delete({'type' => 'phenotype_attempt', 'id' => pa.id})
+  end
 end
--- test/unit/solr_update/enqueuer_test.rb
+++ test/unit/solr_update/enqueuer_test.rb
@@ -3,49 +3,75 @@ require 'test_helper'
 class SolrUpdate::EnqueuerTest < ActiveSupport::TestCase
   context 'SolrUpdate::Enqueuer' do
 
+    setup do
+      @enqueuer = SolrUpdate::Enqueuer.new
+    end
+
     teardown do
       SolrUpdate::Queue.unstub(:enqueue_for_update)
       SolrUpdate::Queue.unstub(:enqueue_for_delete)
     end
 
-    should 'update an MI attempt\'s phenotype attempts on update' do
-      mi_attempt = Factory.create(:mi_attempt_genotype_confirmed, :id => 44)
-      Factory.create(:phenotype_attempt_status_cec, :mi_attempt => mi_attempt, :id => 55)
-      Factory.create(:phenotype_attempt_status_cec, :mi_attempt => mi_attempt, :id => 66)
-
-      phenotype_attempt_refs = [
-        {'id' => 55, 'type' => 'phenotype_attempt'},
-        {'id' => 66, 'type' => 'phenotype_attempt'}
-      ]
-
-      @testobj.expects(:do).with(phenotype_attempt_refs[0], 'update')
-      @testobj.expects(:do).with(phenotype_attempt_refs[1], 'update')
-
-      @testobj.after_update({'id' => 44, 'type' => 'mi_attempt'})
-    end
-
-    should 'do nothing if phenotype attempt is passed in' do
-      @testobj.expects(:do).never
-
-      @testobj.after_update({'id' => 44, 'type' => 'phenotype_attempt'})
-    end
-
     context 'when a mi_attempt changes' do
       should 'enqueue an update to it if it has status "gtc"' do
-        flunk
+        mi = Factory.create :mi_attempt_genotype_confirmed, :id => 55
+        SolrUpdate::Queue.expects(:enqueue_for_update).with({'type' => 'mi_attempt', 'id' => 55})
+        @enqueuer.mi_attempt_updated(mi)
       end
 
-      should 'enqueue a deletion if it has been deleted'
-      should 'not enqueue an update if mi_attempt gene does not have mgi_accession_id'
-      should 'not enqueue an update when a non-"gtc" mouse is changed'
-      should 'enqueue a deletion for it if it has left the "gtc" status'
-      should 'enqueue updates to it\'s "cec" phenotype attempts'
+      should 'enqueue a deletion if it has been deleted' do
+        mi = Factory.create :mi_attempt_genotype_confirmed, :id => 22
+        SolrUpdate::Queue.expects(:enqueue_for_delete).with({'type' => 'mi_attempt', 'id' => 22})
+        @enqueuer.mi_attempt_destroyed(mi)
+      end
+
+      should 'not enqueue an update if mi_attempt gene does not have mgi_accession_id' do
+        mi = Factory.create :mi_attempt_genotype_confirmed, :id => 55
+        mi.gene.update_attributes!(:mgi_accession_id => nil)
+        mi.reload
+        SolrUpdate::Queue.expects(:enqueue_for_update).never
+        @enqueuer.mi_attempt_updated(mi)
+      end
+
+      should 'enqueue a deletion for it if it does not have status "gtc"' do
+        mi = Factory.create :mi_attempt_chimeras_obtained, :id => 55
+        SolrUpdate::Queue.expects(:enqueue_for_update).never
+        SolrUpdate::Queue.expects(:enqueue_for_delete).with({'type' => 'mi_attempt', 'id' => mi.id})
+        @enqueuer.mi_attempt_updated(mi)
+      end
+
+      should 'tell itself the mi_attempt\'s phenotype attempts have changed too' do
+        mi = Factory.create :mi_attempt_genotype_confirmed, :id => 67
+        pa1 = Factory.create :phenotype_attempt, :mi_attempt => mi, :id => 675
+        pa2 = Factory.create :phenotype_attempt, :mi_attempt => mi, :id => 1475
+        mi.reload
+
+        SolrUpdate::Queue.expects(:enqueue_for_update).with({'type' => 'mi_attempt', 'id' => mi.id})
+        SolrUpdate::Enqueuer.any_instance.expects(:phenotype_attempt_updated).with(pa1)
+        SolrUpdate::Enqueuer.any_instance.expects(:phenotype_attempt_updated).with(pa2)
+        @enqueuer.mi_attempt_updated(mi)
+      end
     end
 
     context 'when a phenotype_attempt changes' do
-      should 'enqueue an update to it if it has status "cec"'
-      should 'enqueue a deletion for it if it has left the "cec" state'
-      should 'enqueue a deletion for it if it has been deleted'
+      should 'enqueue an update for it if it has status "cec"' do
+        pa = Factory.create :phenotype_attempt_status_cec, :id => 67545
+        SolrUpdate::Queue.expects(:enqueue_for_update).with({'type' => 'phenotype_attempt', 'id' => pa.id})
+        @enqueuer.phenotype_attempt_updated(pa)
+      end
+
+      should 'enqueue a deletion for it if it does not have status "cec"' do
+        pa = Factory.create :phenotype_attempt, :id => 345
+        SolrUpdate::Queue.expects(:enqueue_for_delete).with({'type' => 'phenotype_attempt', 'id' => pa.id})
+        @enqueuer.phenotype_attempt_updated(pa)
+      end
+
+      should 'enqueue a deletion for it if it has been deleted' do
+        pa = Factory.create :phenotype_attempt, :id => 568
+        SolrUpdate::Queue.expects(:enqueue_for_delete).with({'type' => 'phenotype_attempt', 'id' => pa.id})
+        @enqueuer.phenotype_attempt_destroyed(pa)
+      end
+
     end
 
   end

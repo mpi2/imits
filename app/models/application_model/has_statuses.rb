4226ebd15b277ca447e778f24c8c436dba6a67b9
Asfand Qazi <aq2@sanger.ac.uk>
2012-10-11 13:12:17 +0100

    HasStatuses#has_status? can now take symbol or string for status code or name

--- app/models/application_model/has_statuses.rb
+++ app/models/application_model/has_statuses.rb
@@ -1,5 +1,20 @@
 module ApplicationModel::HasStatuses
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def status_class; return self.const_get('Status'); end
+  end
+
+  def status_class; self.class.status_class; end
+
   def has_status?(status)
+    if status.kind_of?(String) or status.kind_of?(Symbol)
+      status_name = status
+      status = status_class.find_by_code(status_name)
+      if ! status
+        status = status_class.find_by_name(status_name)
+      end
+    end
     return status_stamps.where(:status_id => status.id).size != 0
   end
 end
--- test/unit/application_model/has_statuses_test.rb
+++ test/unit/application_model/has_statuses_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class ApplicationModel::HasStatusesTest < ActiveSupport::TestCase
   context 'ApplicationModel::HasStatuses' do
 
+    context '#status_class' do
+      should 'work' do
+        assert_equal Test::Person::Status, Test::Person.status_class
+        assert_equal Test::Person::Status, Test::Person.new.status_class
+      end
+    end
+
     context '#has_status?' do
       setup do
         @person = Test::Person.create!(:name => 'Bob')
@@ -16,6 +23,16 @@ class ApplicationModel::HasStatusesTest < ActiveSupport::TestCase
 
       should 'be false if status stamps do not include given status' do
         assert_equal false, @person.has_status?(Test::Person::Status::BURIED)
+      end
+
+      should 'work with status codes' do
+        assert_equal true, @person.has_status?(:alive)
+        assert_equal false, @person.has_status?(:buried)
+      end
+
+      should 'work with status names' do
+        assert_equal true, @person.has_status?('Alive')
+        assert_equal false, @person.has_status?('Buried')
       end
     end
 

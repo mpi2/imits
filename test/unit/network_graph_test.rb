7187a07f87e9f089c2149626b0fdb5ebd0de970d
Peter Matthews <pm9@sanger.ac.uk>
2012-11-01 15:43:43 +0000

    network tests fixed

--- test/unit/network_graph_test.rb
+++ test/unit/network_graph_test.rb
@@ -8,10 +8,11 @@ class NetworkGraphTest < ActiveSupport::TestCase
     context 'has nodes for' do
       context 'genes which' do
         should 'have a html label' do
-          marker_symbol = "cpk1"
-          id = "1"
+          gene = Factory.create :gene
+          marker_symbol = gene.marker_symbol
+          id = gene.id
           symbol = "G1"
-          node = NetworkGraph::GeneNode.new(:symbol => symbol, :id => id, :marker_symbol => marker_symbol)
+          node = NetworkGraph::GeneNode.new(gene ,params= {:symbol => symbol, :url => ""})
           expected = "<<table>" +
                    "<tr><td colspan=\"2\">Gene</td></tr>" +
                    "<tr><td>Marker Symbol:</td><td>#{marker_symbol}</td></tr>" +
@@ -23,17 +24,19 @@ class NetworkGraphTest < ActiveSupport::TestCase
 
       context 'mi_plans and' do
         should 'have a html label' do
-          mi_plan = Factory.create :mi_plan
+          mi_plan = Factory.create :mi_plan_with_production_centre
           status = mi_plan.status_stamps.first
+          current_status = mi_plan.status.name
           id = mi_plan.id
           symbol = "PA1"
-          consortium = "BaSH"
-          centre = "WTSI"
-          node = NetworkGraph::MiPlanNode.new(:symbol => symbol, :id => id, :consortium => consortium, :centre => centre, :url => "")
+          consortium = mi_plan.consortium.name
+          centre = mi_plan.production_centre.name
+          node = NetworkGraph::MiPlanNode.new(mi_plan, params= {:symbol => symbol, :url => ""})
           expected = "<<table>" +
                      "<tr><td colspan=\"2\">Mi Plan</td></tr>" +
                      "<tr><td>Consortium:</td><td>#{consortium}</td></tr>" +
                      "<tr><td>Centre:</td><td>#{centre}</td></tr>" +
+                     "<tr><td>Current Status:</td><td>#{current_status}</td></tr>" +
                      "<tr><td>#{status.status.name}:</td><td>#{status.created_at.strftime "%d/%m/%Y"}</td></tr>" +
                      "</table>>"
           got = node.label_html
@@ -43,21 +46,29 @@ class NetworkGraphTest < ActiveSupport::TestCase
 
       context 'mi_attempts and' do
         should 'have a html label' do
-          mi_attempt = Factory.create :mi_attempt
-          status = mi_attempt.status_stamps.first
+          mi_attempt = Factory.create :randomly_populated_mi_attempt
           id = mi_attempt.id
+          status_stamps = mi_attempt.status_stamps.order("created_at DESC")
           symbol = "PA1"
-          consortium = "BaSH"
-          centre = "WTSI"
-          colony_background_strain = "blogs"
-          test_cross_strain = "blogs"
-          node = NetworkGraph::MiAttemptNode.new(:symbol => symbol, :id => id, :consortium => consortium, :centre => centre, :test_cross_strain => test_cross_strain, :colony_background_strain => colony_background_strain , :url => "")
+          consortium = mi_attempt.consortium.name
+          centre = mi_attempt.production_centre.name
+          colony_background_strain = CGI.escapeHTML(mi_attempt.colony_background_strain.name)
+          current_status = mi_attempt.status.name
+          colony_name = mi_attempt.colony_name.to_s
+          test_cross_strain = CGI.escapeHTML(mi_attempt.test_cross_strain.name)
+          status_string = ''
+          status_stamps.each do |status|
+            status_string << "<tr><td>#{status.status.name}:</td><td>#{status.created_at.strftime "%d/%m/%Y"}</td></tr>"
+          end
+          node = NetworkGraph::MiAttemptNode.new(mi_attempt, params= {:symbol => symbol, :url => "" })
           expected = "<<table>" +
                      "<tr><td colspan=\"2\">Mouse Production</td></tr>" +
                      "<tr><td>Consortium:</td><td>#{consortium}</td></tr>" +
+                     "<tr><td>Current Status:</td><td>#{current_status}</td></tr>" +
                      "<tr><td>Centre:</td><td>#{centre}</td></tr>" +
-                     "<tr><td>#{status.status.name}:</td><td>#{status.created_at.strftime "%d/%m/%Y"}</td></tr>" +
+                     "#{status_string}" +
                      "<tr><td>Colony background strain:</td><td>#{colony_background_strain}</td></tr>" +
+                     "<tr><td>Colony name:</td><td>#{colony_name}</td></tr>" +
                      "<tr><td>Test cross strain:</td><td>#{test_cross_strain}</td></tr>" +
                      "</table>>"
           got = node.label_html
@@ -67,20 +78,28 @@ class NetworkGraphTest < ActiveSupport::TestCase
 
       context 'phenotypes and' do
         should 'have a html label' do
-          phenotype_attempt = Factory.create :phenotype_attempt
-          status = phenotype_attempt.status_stamps.first
+          phenotype_attempt = Factory.create :phenotype_attempt_status_cec
+          statuses = phenotype_attempt.status_stamps.all
           id = phenotype_attempt.id
           symbol = "PA1"
-          consortium = "BaSH"
-          centre = "WTSI"
-          cre_deleter_strain = "blogs"
-          node = NetworkGraph::PhenotypeAttemptNode.new(:symbol => symbol, :id => id, :consortium => consortium, :centre => centre, :cre_deleter_strain => cre_deleter_strain, :url => "")
+          consortium = phenotype_attempt.consortium.name
+          centre = phenotype_attempt.production_centre.name
+          cre_deleter_strain = phenotype_attempt.deleter_strain.name
+          current_status = phenotype_attempt.status.name
+          colony_name = phenotype_attempt.colony_name.to_s
+          all_statuses = ''
+          statuses.each do |status|
+            all_statuses << "<tr><td>#{status.status.name}:</td><td>#{status.created_at.strftime "%d/%m/%Y"}</td></tr>" 
+          end
+          node = NetworkGraph::PhenotypeAttemptNode.new(phenotype_attempt, params={ :symbol => symbol, :url => "" })
           expected = "<<table>" +
                    "<tr><td colspan=\"2\">Phenotype Attempt</td></tr>" +
                    "<tr><td>Consortium:</td><td>#{consortium}</td></tr>" +
                    "<tr><td>Centre:</td><td>#{centre}</td></tr>" +
+                   "<tr><td>Current Status:</td><td>#{current_status}</td></tr>" +
                    "<tr><td>Cre Deleter Strain:</td><td>#{cre_deleter_strain}</td></tr>" +
-                   "<tr><td>#{status.status.name}:</td><td>#{status.created_at.strftime "%d/%m/%Y"}</td></tr>" +
+                   "#{all_statuses}" +
+                   "<tr><td>Colony name:</td><td>#{colony_name}</td></tr>" +
                    "</table>>"
             got = node.label_html
             assert_equal expected, got
@@ -112,7 +131,7 @@ class NetworkGraphTest < ActiveSupport::TestCase
 
       expected = <<-EOL
 {rank=same;"Gene";"G1"}
-{rank=same;"Mi Plans";"P1";"P2"}
+{rank=same;"Plan";"P1";"P2"}
 {rank=same;"Mouse Production";"MA1"}
 {rank=same;"Phenotype Attempts";"PA1"}
       EOL

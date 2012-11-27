b65422225e2a251cbde6a4f20fcede579a17ff7c
Jack Regnart <jr18@sanger.ac.uk>
2012-11-27 15:24:35 +0000

    not sure how this got commited

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Kermits2::Application.config.session_store :cookie_store, :key => "_kermits2_#{Rails.env}_session", :domain => :all
+Kermits2::Application.config.session_store :cookie_store, :key => "_kermits2_#{Rails.env}_session"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

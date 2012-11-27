3c0dd7c3858b9c72db500acd7abac776cf84f07d
Richard Easty <re4@sanger.ac.uk>
2012-11-27 14:10:29 +0000

    tidy

--- script/scratch/add_robot_users.rb
+++ script/scratch/add_robot_users.rb
@@ -23,21 +23,18 @@ DEBUG = false
 ApplicationModel.audited_transaction do
 
 Centre.all.each do |centre|
-	base_name = centre.name.gsub(/\s+/, '_').downcase
-	name = base_name + '_robot'
-	next if User.find_by_name name
-	# see http://stackoverflow.com/questions/88311/how-best-to-generate-a-random-string-in-ruby
-      	password = ('a'..'z').to_a.shuffle[0,8].join
-	email = "#{name}@#{base_name}.ac.uk"
-	puts "#{name} - '#{email}' - '#{password}'" if DEBUG
-	puts "#{name} - '#{email}'" if ! DEBUG
-      	User.create!( { :name => name, :email => email, :password => password, :production_centre_id => centre.id } )
+  base_name = centre.name.gsub(/\s+/, '_').downcase
+  name = base_name + '_robot'
+  next if User.find_by_name name
+  # see http://stackoverflow.com/questions/88311/how-best-to-generate-a-random-string-in-ruby
+  password = ('a'..'z').to_a.shuffle[0,8].join
+  email = "#{name}@#{base_name}.ac.uk"
+  puts "#{name} - '#{email}' - '#{password}'" if DEBUG
+  puts "#{name} - '#{email}'" if ! DEBUG
+  User.create!( { :name => name, :email => email, :password => password, :production_centre_id => centre.id } )
 end
 
 raise 'rollback!' if DEBUG
 
 end
 
-#User.all.each do |user|
-	#puts "NAME: #{user.name}"
-#end

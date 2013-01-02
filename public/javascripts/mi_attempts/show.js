f5b1ce80c606d8f68eed12045fba17d2fc19ba21
Jack Regnart <jr18@sanger.ac.uk>
2013-01-02 14:17:28 +0000

    remove text from emails && update mi_attempt distribution_centres js & css

--- public/javascripts/mi_attempts/show.js
+++ public/javascripts/mi_attempts/show.js
@@ -22,7 +22,7 @@ Ext.select('form .add-row').on("click", function(event){
 
   data = data.replace(regexp, time);
 
-  Ext.select("#distribution_centres_table tr:last").insertSibling(data, 'after');
+  Ext.select("#distribution_centres_table:not(td > table) > tbody > tr:last").insertSibling(data, 'after');
 
   Ext.select("#distribution_centres_table tr:last a.remove-row").on("click", function(e){
         e.preventDefault();

170a7e0adbf7cf1880af7496b3ddcd8668c7af71
Jack Regnart <jr18@sanger.ac.uk>
2013-02-27 15:00:01 +0000

    javascripts controller update

--- app/controllers/targ_rep/javascripts_controller.rb
+++ app/controllers/targ_rep/javascripts_controller.rb
@@ -1,2 +1,7 @@
 class TargRep::JavascriptsController < TargRep::BaseController
+  def dynamic_esc_qc_conflict_selects
+    respond_to do |format|
+      format.js {render :content_type => 'text/javascript'}
+    end
+  end
 end

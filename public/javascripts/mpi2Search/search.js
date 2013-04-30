67fc72c294c84ce2367349747a88f51bad85cd2f
Peter Matthews <pm9@sanger.ac.uk>
2013-04-30 10:56:28 +0100

    add gene search text box

--- public/javascripts/mpi2Search/search.js
+++ public/javascripts/mpi2Search/search.js
@@ -1,19 +1,23 @@
 function populate_allele_grid(){
-  document.getElementById("mpi2-allele-grid").style.visibility="hidden";
-  var mgiAccession = '';
-  var patt = new RegExp("MGI:");
-  if (patt.test(document.getElementById("marker_symbol").value)){
-    mgiAccession = document.getElementById("marker_symbol").value;
-    create_grid(mgiAccession);
-  }
-  else if (document.getElementById("marker_symbol").value) {
-    var request = $.ajax({url: window.basePath +"genes.json?marker_symbol_eq=" + document.getElementById("marker_symbol").value, dataType: 'json'});
-    request.done(function (response, textStatus, jqZHR){
-      if (response.length != 0){
-        mgiAccession = response[0]['mgi_accession_id'];
-        create_grid(mgiAccession);
-      };
-    });
+  var search_text = document.getElementById("search_for_gene").value;
+  go_to_home_page();
+  if (document.readyState === "complete"){
+    document.getElementById("mouse-alleles").style.visibility="hidden";
+    var mgiAccession = '';
+    var patt = new RegExp("MGI:");
+    if (patt.test(search_text)){
+      mgiAccession = search_text;
+      create_grid(mgiAccession);
+    }
+    else if (search_text) {
+      var request = $.ajax({url: window.basePath +"genes.json?marker_symbol_eq=" + search_text, dataType: 'json'});
+      request.done(function (response){
+        if (response.length != 0){
+          mgiAccession = response[0]['mgi_accession_id'];
+          create_grid(mgiAccession);
+        };
+      });
+    }
   }
 }
 
@@ -21,6 +25,13 @@ function create_grid(mgiAccession){
   if (mgiAccession){
     mgiAccession = mgiAccession.replace('MGI:', '');
     $("#mpi2-allele-grid").mpi2GenePageAlleleGrid().trigger('search', {solrParams: {q: 'mgi_accession_id:'+mgiAccession}});
-    document.getElementById("mpi2-allele-grid").style.visibility="visible";
+    document.getElementById("mouse-alleles").style.visibility="visible";
+  }
+}
+
+function go_to_home_page(){
+  if (document.getElementsByTagName("title")[0].text != "iMits - Home"){
+    window.location.replace(window.basePath + "/");
+
   }
 }

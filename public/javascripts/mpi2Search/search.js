a72559b8c20907db053fd63383604d3265334ae1
Peter Matthews <pm9@sanger.ac.uk>
2013-04-30 14:23:09 +0100

    reposition search_box

--- public/javascripts/mpi2Search/search.js
+++ public/javascripts/mpi2Search/search.js
@@ -1,23 +1,20 @@
 function populate_allele_grid(){
   var search_text = document.getElementById("search_for_gene").value;
-  go_to_home_page();
-  if (document.readyState === "complete"){
-    document.getElementById("mouse-alleles").style.visibility="hidden";
-    var mgiAccession = '';
-    var patt = new RegExp("MGI:");
-    if (patt.test(search_text)){
-      mgiAccession = search_text;
-      create_grid(mgiAccession);
-    }
-    else if (search_text) {
-      var request = $.ajax({url: window.basePath +"genes.json?marker_symbol_eq=" + search_text, dataType: 'json'});
-      request.done(function (response){
-        if (response.length != 0){
-          mgiAccession = response[0]['mgi_accession_id'];
-          create_grid(mgiAccession);
-        };
-      });
-    }
+  document.getElementById("mouse-alleles").style.visibility="hidden";
+  var mgiAccession = '';
+  var patt = new RegExp("MGI:");
+  if (patt.test(search_text)){
+    mgiAccession = search_text;
+    create_grid(mgiAccession);
+  }
+  else if (search_text) {
+    var request = $.ajax({url: window.basePath +"genes.json?marker_symbol_eq=" + search_text, dataType: 'json'});
+    request.done(function (response){
+      if (response.length != 0){
+        mgiAccession = response[0]['mgi_accession_id'];
+        create_grid(mgiAccession);
+      };
+    });
   }
 }
 
@@ -26,12 +23,5 @@ function create_grid(mgiAccession){
     mgiAccession = mgiAccession.replace('MGI:', '');
     $("#mpi2-allele-grid").mpi2GenePageAlleleGrid().trigger('search', {solrParams: {q: 'mgi_accession_id:'+mgiAccession}});
     document.getElementById("mouse-alleles").style.visibility="visible";
-  }
-}
-
-function go_to_home_page(){
-  if (document.getElementsByTagName("title")[0].text != "iMits - Home"){
-    window.location.replace(window.basePath + "/");
-
   }
 }

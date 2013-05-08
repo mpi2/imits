f9706d7c7a748abe0e64825f8a000ecec0d21e14
Jack Regnart <jr18@sanger.ac.uk>
2013-05-08 10:37:49 +0100

    looks like this has been removed manually on the database and not by a migration

--- db/migrate/20130430091822_add_mi_attempt_distribution_centre_mi_attempt_fk.rb
+++
@@ -1,9 +0,0 @@
-class AddMiAttemptDistributionCentreMiAttemptFk < ActiveRecord::Migration
-  def up
-    add_foreign_key :mi_attempt_distribution_centres, :mi_attempts
-  end
-
-  def down
-    remove_foreign_key :mi_attempt_distribution_centres, :mi_attempts
-  end
-end

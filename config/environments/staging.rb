86b5920eb0857c8b6845b43c29a2a98a8399002f
Jack Regnart <jr18@sanger.ac.uk>
2013-05-23 10:45:55 +0100

    don't use sendfile on staging or labs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ TarMits::Application.configure do
   config.action_mailer.default_url_options = { :host => "i-dcc.org" }
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'

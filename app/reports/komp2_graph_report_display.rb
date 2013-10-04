d05a9395ab3b9ed4106651719254e720a9c52657
Peter Matthews <pm9@sanger.ac.uk>
2013-10-04 15:00:26 +0100

    add graph with 2016 goals

--- app/reports/komp2_graph_report_display.rb
+++ app/reports/komp2_graph_report_display.rb
@@ -19,12 +19,24 @@ class Komp2GraphReportDisplay < BaseSummaryByMonthReport
     return @size
   end
 
-  def generate_graphs
+  def generate_graphs #_with_2016_goals
 
     dataset = {}
     dates = []
     cut_off_date = '2011-06-01'.to_date
     report_date = self.class.date_previous_month.to_date.at_beginning_of_month
+
+    sql = <<-EOF
+      SELECT consortia.name AS consortium, production_goals.gc_goal AS gc_goal
+      FROM production_goals
+        JOIN consortia ON consortia.id = production_goals.consortium_id
+      WHERE production_goals.year = 2016 AND production_goals.month = 1
+    EOF
+    report_data = ActiveRecord::Base.connection.execute(sql)
+    goals_for_2016 = {}
+    report_data.each do |report_row|
+      goals_for_2016[report_row['consortium']] = report_row['gc_goal']
+    end
 
     while report_date >= cut_off_date do
       dates.insert(0, report_date)
@@ -59,9 +71,36 @@ class Komp2GraphReportDisplay < BaseSummaryByMonthReport
 
         dataset[consortium]['cre_excised_data'].append(@report_hash["#{consortium}-#{date}-Cumulative Cre Excision Complete"].to_i)
         dataset[consortium]['x_data'].append([rowno,"#{Date::ABBR_MONTHNAMES[date.month]}-#{date.year.to_s[2..3]}"])
+
         rowno +=1
       end
+      size = dataset[consortium]['x_data'].count
+      dataset[consortium]['x_data_extended'] = Array.new(size){ |index| (index+1)%2 == 0 ? nil : dataset[consortium]['x_data'][index] }.compact
+      dataset[consortium]['pos_gc_diff_data'].append(0)
+      dataset[consortium]['neg_gc_diff_data'].append(0)
+      dataset[consortium]['extended_gc_goals'] = dataset[consortium]['gc_goal_data'].dup
+      last_date_so_far = dates.last
+      last_date = "2016-07-01".to_date
+      number_of_remaining_months = ((last_date.year - last_date_so_far.year) * 12) + (last_date.month - last_date_so_far.month)
+      remaining_goal_difference = (goals_for_2016["#{consortium}"].to_f - dataset[consortium]['gc_goal_data'].last.to_f)/number_of_remaining_months
+      new_goal = dataset[consortium]['gc_goal_data'].last.to_f
+      for i in 1..(number_of_remaining_months-1)
+        last_date_so_far = last_date_so_far.next_month
+        if rowno%2 == 0
+          dataset[consortium]['x_data_extended'].append([rowno, "#{Date::ABBR_MONTHNAMES[last_date_so_far.month]}-#{last_date_so_far.year.to_s[2..3]}"])
+        else
+          dataset[consortium]['x_data_extended'].append([rowno,''])
+        end
+        rowno +=1
+        new_goal += remaining_goal_difference
+        dataset[consortium]['extended_gc_goals'].append(new_goal.to_i)
+      end
+      dataset[consortium]['extended_gc_goals'].append(goals_for_2016["#{consortium}"].to_i)
+      dataset[consortium]['x_data_extended'].append([rowno,""])
+      rowno+=1
+      dataset[consortium]['x_data_extended'].append([rowno,"Aug-16"])
     end
+
     return dataset
   end
 
@@ -70,8 +109,10 @@ class Komp2GraphReportDisplay < BaseSummaryByMonthReport
     index = 0
     graph.each do |consortium, graph_data|
       x_data_lables = graph_data['x_data']
+      x_data_lables_to_2016 = graph_data['x_data_extended']
       mi_max = (([graph_data['mi_data'].max, graph_data['mi_goal_data'].max].max / 40) + 1) * 40
       gc_max = (([graph_data['gc_data'].max, graph_data['gc_goal_data'].max].max / 40) + 1) * 40
+      extended_gc_max = (([graph_data['gc_data'].max, graph_data['gc_goal_data'].max, graph_data['extended_gc_goals'].max].max / 40) + 1) * 40
       one_width << [(graph_data['mi_data'].count + 2) * 40 , 600].max
       draw_graph({:title => "#{consortium} Total Microinjections",
                   :legend => ['Total Mouse Production (Genes)','Mouse Production Goals (Genes)', 'Below', 'Above'],
@@ -93,7 +134,7 @@ class Komp2GraphReportDisplay < BaseSummaryByMonthReport
                   :pointer_marker => x_data_lables,
                   :data => graph_data,
                   :line_data_plots => ['gc_data', 'gc_goal_data', 'cre_excised_data'],
-                  :area_data_plots => ['pos_gc_diff_data', 'neg_gc_diff_data']
+                  :area_data_plots => ['pos_gc_diff_data', 'neg_gc_diff_data'],
                  },
                  {:name => 'gc',
                   :colour => ['#009966', '#6886B4','#662266', '#FFCC00', '#000DCC'],
@@ -102,25 +143,44 @@ class Komp2GraphReportDisplay < BaseSummaryByMonthReport
                   :max_value => gc_max,
                   :consortium => consortium}
                 )
+
+      draw_graph({:title => "#{consortium} Genotype Confirmed Mouse Production (inc 2016 goals)",
+                  :legend => ['Total Genotype Confirmed Mice (Genes)','Genotype Confirmed Goals (Genes)', 'Cre Excision Complete Mice (Genes)', 'Below', 'Above'],
+                  :pointer_marker => x_data_lables_to_2016,
+                  :data => graph_data,
+                  :line_data_plots => ['gc_data', 'extended_gc_goals', 'cre_excised_data'],
+                  :area_data_plots => ['pos_gc_diff_data', 'neg_gc_diff_data'],
+                 },
+                 {:name => 'all_inc_gc',
+                  :colour => ['#009966', '#6886B4','#662266', '#FFCC00', '#000DCC'],
+                  :width => one_width[index],
+                  :height =>380, :min_value => 0,
+                  :max_value => extended_gc_max,
+                  :consortium => consortium}
+                )
       index += 1
     end
     return one_width
   end
 
-  def draw_graph(graph = {:title => '', :legend => ['',''], :pointer_marker => [], :data => {}, :line_data_plots => [], :area_data_plots => []}, render = {:name => '', :colour => ['#009966', '#6886B4','#662266', '#FFCC00', '#000DCC'], :width => 600, :min_value => 0, :max_value => 500, :consortium => ''})
+  def draw_graph(graph = {:title => '', :legend => ['',''], :pointer_marker => [], :data => {}, :line_data_plots => [], :area_data_plots => [], }, render = {:name => '', :colour => ['#009966', '#6886B4','#662266', '#FFCC00', '#000DCC'], :width => 600, :min_value => 0, :max_value => 500, :consortium => ''})
     format = 'jpeg'
 
     mi_graph = Scruffy::Graph.new(:title => graph[:title], :point_markers => graph[:pointer_marker], :point_markers_rotation => 90)
     mi_graph.theme = Scruffy::Themes::Base.new({:background => [:white, :white], :colors => render[:colour], :marker => :black})
     layers = []
     layer_count = 0
-    graph[:line_data_plots].each do |data2|
-      layers << mi_graph.add(:line, graph[:legend][layer_count], graph[:data][data2], :stroke_width => 4, :dots => true)
-      layer_count += 1
+    if graph.has_key?(:line_data_plots)
+      graph[:line_data_plots].each do |data2|
+        layers << mi_graph.add(:line, graph[:legend][layer_count], graph[:data][data2], :stroke_width => 3, :dots => true)
+        layer_count += 1
+      end
     end
-    graph[:area_data_plots].each do |data2|
-      layers << mi_graph.add(:area, graph[:legend][layer_count], graph[:data][data2])
-      layer_count += 1
+    if graph.has_key?(:area_data_plots)
+      graph[:area_data_plots].each do |data2|
+        layers << mi_graph.add(:area, graph[:legend][layer_count], graph[:data][data2])
+        layer_count += 1
+      end
     end
     mi_graph.renderer = Scruffy::Renderers::Base.new
     mi_graph.renderer.components << Scruffy::Components::Title.new(:title, :position => [5, 0], :size => [90, 7])

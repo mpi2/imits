ea129307d1a5b3613126cbf72cad968377ab9264
Peter Matthews <pm9@sanger.ac.uk>
2013-10-22 17:13:19 +0100

    relaxed validation for allele

--- app/validators/consistent_allele_validator.rb
+++ app/validators/consistent_allele_validator.rb
@@ -9,16 +9,16 @@ class ConsistentAlleleValidator < ActiveModel::EachValidator
       targ_vec_allele = record.targeting_vector.allele
 
       valid = targ_vec_allele.id == my_allele.id || (
-        my_allele.mgi_accession_id    == targ_vec_allele.mgi_accession_id   and
-        my_allele.project_design_id   == targ_vec_allele.project_design_id  and
-        ['crd', 'tnc'].include?(my_allele.mutation_type.code)               and
-        ['crd', 'tnc'].include?(targ_vec_allele.mutation_type.code)         and
-        my_allele.cassette            == targ_vec_allele.cassette           and
-        my_allele.backbone            == targ_vec_allele.backbone           and
-        my_allele.homology_arm_start  == targ_vec_allele.homology_arm_start and
-        my_allele.homology_arm_end    == targ_vec_allele.homology_arm_end   and
-        my_allele.cassette_start      == targ_vec_allele.cassette_start     and
-        my_allele.cassette_end        == targ_vec_allele.cassette_end
+        my_allele.mgi_accession_id          == targ_vec_allele.mgi_accession_id         and
+        my_allele.project_design_id         == targ_vec_allele.project_design_id        and
+        ['crd', 'tnc'].include?(my_allele.mutation_type.code)                           and
+        ['crd', 'tnc'].include?(targ_vec_allele.mutation_type.code)                     and
+        my_allele.cassette.gsub('_cre','')  == targ_vec_allele.cassette.gsub('_cre','') and
+        my_allele.backbone                  == targ_vec_allele.backbone                 and
+        my_allele.homology_arm_start        == targ_vec_allele.homology_arm_start       and
+        my_allele.homology_arm_end          == targ_vec_allele.homology_arm_end         and
+        my_allele.cassette_start            == targ_vec_allele.cassette_start           and
+        my_allele.cassette_end              == targ_vec_allele.cassette_end
       )
 
    rescue => e

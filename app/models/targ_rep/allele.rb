cdf0abbd9fc5f9b1100f5d165df42c929da96b86
Richard Easty <re4@sanger.ac.uk>
2013-11-26 17:35:25 +0000

    Add ikmc_project_pipeline.

--- app/models/targ_rep/allele.rb
+++ app/models/targ_rep/allele.rb
@@ -30,12 +30,14 @@ class TargRep::Allele < ActiveRecord::Base
           :mgi_allele_symbol_superscript => es_cell.mgi_allele_symbol_superscript,
           :ikmc_project_id => es_cell.ikmc_project_id.to_s,
           :ikmc_project_status_name => '',
-          :ikmc_project_name => ''
+          :ikmc_project_name => '',
+          :ikmc_project_pipeline => ''
         }
 
         if es_cell.ikmc_project && es_cell.ikmc_project.status && es_cell.ikmc_project.status.name
           key[:ikmc_project_name] = es_cell.ikmc_project.name
           key[:ikmc_project_status_name] = es_cell.ikmc_project.status.name
+          key[:ikmc_project_pipeline] = es_cell.ikmc_project.pipeline.name
         end
 
         info_map[key] ||= {:pipelines => []}
@@ -386,4 +388,3 @@ end
 #  intron              :integer
 #  type                :string(255)     default("TargRep::TargetedAllele")
 #
-

8b6657636c963623b7b2e95801824e61916a90ec
Richard Easty <re4@sanger.ac.uk>
2013-12-09 16:21:58 +0000

    Remove commented code.

--- db/migrate/20131209100237_add_qc_fields_to_phenotype_attempt.rb
+++ db/migrate/20131209100237_add_qc_fields_to_phenotype_attempt.rb
@@ -1,24 +1,5 @@
 class AddQcFieldsToPhenotypeAttempt < ActiveRecord::Migration
 
-  #QC_FIELDS = [
-  #  :qc_southern_blot,
-  #  :qc_five_prime_lr_pcr,
-  #  :qc_five_prime_cassette_integrity,
-  #  :qc_tv_backbone_assay,
-  #  :qc_neo_count_qpcr,
-  #  :qc_lacz_count_qpcr,
-  #  :qc_neo_sr_pcr,
-  #  :qc_loa_qpcr,
-  #  :qc_homozygous_loa_sr_pcr,
-  #  :qc_lacz_sr_pcr,
-  #  :qc_mutant_specific_sr_pcr,
-  #  :qc_loxp_confirmation,
-  #  :qc_three_prime_lr_pcr,
-  #  :qc_critical_region_qpcr,
-  #  :qc_loxp_srpcr,
-  #  :qc_loxp_srpcr_and_sequencing
-  #]
-
   QC_FIELDS = [
     :qc_southern_blot_id,
     :qc_five_prime_lr_pcr_id,
@@ -49,10 +30,6 @@ class AddQcFieldsToPhenotypeAttempt < ActiveRecord::Migration
   end
 
   def down
-    #QC_FIELDS.each do |qc_field|
-    #  remove_foreign_key :phenotype_attempts, :qc_result_phenotypes
-    #end
-
     QC_FIELDS.each do |qc_field|
       remove_column :phenotype_attempts, qc_field
     end

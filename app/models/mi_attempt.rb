5ee24b197b545d0e7b9e4916bd5b1b0fe6601334
Richard Easty <re4@sanger.ac.uk>
2013-12-09 16:22:55 +0000

    Tidy annotation.

--- app/models/mi_attempt.rb
+++ app/models/mi_attempt.rb
@@ -407,11 +407,6 @@ class MiAttempt < ApplicationModel
 
 end
 
-
-
-
-
-
 # == Schema Information
 #
 # Table name: mi_attempts
@@ -482,76 +477,3 @@ end
 #
 #  index_mi_attempts_on_colony_name  (colony_name) UNIQUE
 #
-
-
-# == Schema Information
-#
-# Table name: mi_attempts
-#
-#  id                                              :integer         not null, primary key
-#  es_cell_id                                      :integer         not null
-#  mi_date                                         :date            not null
-#  status_id                                       :integer         not null
-#  colony_name                                     :string(125)
-#  updated_by_id                                   :integer
-#  blast_strain_id                                 :integer
-#  total_blasts_injected                           :integer
-#  total_transferred                               :integer
-#  number_surrogates_receiving                     :integer
-#  total_pups_born                                 :integer
-#  total_female_chimeras                           :integer
-#  total_male_chimeras                             :integer
-#  total_chimeras                                  :integer
-#  number_of_males_with_0_to_39_percent_chimerism  :integer
-#  number_of_males_with_40_to_79_percent_chimerism :integer
-#  number_of_males_with_80_to_99_percent_chimerism :integer
-#  number_of_males_with_100_percent_chimerism      :integer
-#  colony_background_strain_id                     :integer
-#  test_cross_strain_id                            :integer
-#  date_chimeras_mated                             :date
-#  number_of_chimera_matings_attempted             :integer
-#  number_of_chimera_matings_successful            :integer
-#  number_of_chimeras_with_glt_from_cct            :integer
-#  number_of_chimeras_with_glt_from_genotyping     :integer
-#  number_of_chimeras_with_0_to_9_percent_glt      :integer
-#  number_of_chimeras_with_10_to_49_percent_glt    :integer
-#  number_of_chimeras_with_50_to_99_percent_glt    :integer
-#  number_of_chimeras_with_100_percent_glt         :integer
-#  total_f1_mice_from_matings                      :integer
-#  number_of_cct_offspring                         :integer
-#  number_of_het_offspring                         :integer
-#  number_of_live_glt_offspring                    :integer
-#  mouse_allele_type                               :string(3)
-#  qc_southern_blot_id                             :integer
-#  qc_five_prime_lr_pcr_id                         :integer
-#  qc_five_prime_cassette_integrity_id             :integer
-#  qc_tv_backbone_assay_id                         :integer
-#  qc_neo_count_qpcr_id                            :integer
-#  qc_neo_sr_pcr_id                                :integer
-#  qc_loa_qpcr_id                                  :integer
-#  qc_homozygous_loa_sr_pcr_id                     :integer
-#  qc_lacz_sr_pcr_id                               :integer
-#  qc_mutant_specific_sr_pcr_id                    :integer
-#  qc_loxp_confirmation_id                         :integer
-#  qc_three_prime_lr_pcr_id                        :integer
-#  report_to_public                                :boolean         default(TRUE), not null
-#  is_active                                       :boolean         default(TRUE), not null
-#  is_released_from_genotyping                     :boolean         default(FALSE), not null
-#  comments                                        :text
-#  created_at                                      :datetime
-#  updated_at                                      :datetime
-#  mi_plan_id                                      :integer         not null
-#  genotyping_comment                              :string(512)
-#  legacy_es_cell_id                               :integer
-#  qc_lacz_count_qpcr_id                           :integer         default(1)
-#  qc_critical_region_qpcr_id                      :integer         default(1)
-#  qc_loxp_srpcr_id                                :integer         default(1)
-#  qc_loxp_srpcr_and_sequencing_id                 :integer         default(1)
-#  cassette_transmission_verified                  :date
-#  cassette_transmission_verified_auto_complete    :boolean
-#
-# Indexes
-#
-#  index_mi_attempts_on_colony_name  (colony_name) UNIQUE
-#
-

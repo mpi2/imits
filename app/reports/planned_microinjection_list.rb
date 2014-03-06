694b40fe2362543b831fc25b826ed6d050e44968
Richard Easty <re4@sanger.ac.uk>
2014-03-06 13:29:01 +0000

    Add changes for crisprs.

--- app/reports/planned_microinjection_list.rb
+++ app/reports/planned_microinjection_list.rb
@@ -1,4 +1,6 @@
-class PlannedMicroinjectionList
+require 'pp'
+
+class PlannedMicroinjectionList #< Reports::Base
 
   attr_accessor :mi_plan_summary
   attr_accessor :pretty_print_non_assigned_mi_plans
@@ -9,31 +11,41 @@ class PlannedMicroinjectionList
   attr_accessor :gene_pretty_prints
 
   def mi_plan_summary(consortium = nil, crisprs = false)
+    @crisprs = crisprs
     @mi_plan_summary = ActiveRecord::Base.connection.execute(self._mi_plan_summary(consortium, crisprs))
   end
 
   def pretty_print_non_assigned_mi_plans
-    Gene.pretty_print_non_assigned_mi_plans_in_bulk(nil, gene_pretty_prints['non assigned plans'])
+   # puts "#### pretty_print_non_assigned_mi_plans: @crisprs: #{@crisprs}"
+    non_assigned_mi_plans = Gene.pretty_print_non_assigned_mi_plans_in_bulk(nil, gene_pretty_prints['non assigned plans'], @crisprs)
+   # puts "#### non_assigned_mi_plans:"
+    #pp non_assigned_mi_plans
+    non_assigned_mi_plans
   end
 
   def pretty_print_assigned_mi_plans
-    Gene.pretty_print_assigned_mi_plans_in_bulk(nil, gene_pretty_prints['assigned plans'])
+  #  puts "#### pretty_print_assigned_mi_plans: @crisprs: #{@crisprs}"
+    Gene.pretty_print_assigned_mi_plans_in_bulk(nil, gene_pretty_prints['assigned plans'], @crisprs)
   end
 
   def pretty_print_aborted_mi_attempts
-    Gene.pretty_print_mi_attempts_in_bulk_helper(nil, nil, nil, gene_pretty_prints['aborted mi attempts'])
+   # puts "#### pretty_print_aborted_mi_attempts: @crisprs: #{@crisprs}"
+    Gene.pretty_print_mi_attempts_in_bulk_helper(nil, nil, nil, gene_pretty_prints['aborted mi attempts'], @crisprs)
   end
 
   def pretty_print_mi_attempts_in_progress
-    Gene.pretty_print_mi_attempts_in_bulk_helper(nil, nil, nil, gene_pretty_prints['in progress mi attempts'])
+   # puts "#### pretty_print_mi_attempts_in_progress: @crisprs: #{@crisprs}"
+    Gene.pretty_print_mi_attempts_in_bulk_helper(nil, nil, nil, gene_pretty_prints['in progress mi attempts'], @crisprs)
   end
 
   def pretty_print_mi_attempts_genotype_confirmed
-    Gene.pretty_print_mi_attempts_in_bulk_helper(nil, nil, nil, gene_pretty_prints['genotype confirmed mi attempts'])
+   # puts "#### pretty_print_mi_attempts_genotype_confirmed: @crisprs: #{@crisprs}"
+    Gene.pretty_print_mi_attempts_in_bulk_helper(nil, nil, nil, gene_pretty_prints['genotype confirmed mi attempts'], @crisprs)
   end
 
   def gene_pretty_prints
-    @gene_pretty_prints ||= Gene.gene_production_summary
+   # puts "#### pretty_print_mi_attempts_genotype_confirmed: @crisprs: #{@crisprs}"
+    @gene_pretty_prints ||= Gene.gene_production_summary nil, nil, nil, @crisprs
   end
 
   def _mi_plan_summary(consortium = nil, crisprs = false)
@@ -41,9 +53,10 @@ class PlannedMicroinjectionList
     sql = <<-EOF
     WITH mi_attempt_counts AS (
       SELECT mi_plans.id AS plan_id, SUM(CASE WHEN mi_attempt_statuses.name = 'Micro-injection aborted' THEN 1 ELSE 0 END) AS plan_aborted_count,
-             max(CASE WHEN mi_attempt_statuses.name = 'Micro-injection aborted' THEN mi_attempt_status_stamps.created_at ELSE NULL END ) AS plan_aborted_max_date
+             max(CASE WHEN mi_attempt_statuses.name = 'Micro-injection aborted' THEN mi_attempt_status_stamps.created_at ELSE NULL END ) AS plan_aborted_max_date --,
+             --mi_plans.mutagenesis_via_crispr_cas9 as mutagenesis_via_crispr_cas9
         FROM mi_plans
-        JOIN consortia ON consortia.id = mi_plans.consortium_id #{consortium.nil? ? "" : "AND consortia.name = '#{consortium}'"}
+        JOIN consortia ON consortia.id = mi_plans.consortium_id #{consortium.nil? ? "" : "AND consortia.name = '#{consortium}'"} #{crisprs ? 'and mi_plans.mutagenesis_via_crispr_cas9 is true' : ''}
         LEFT JOIN (mi_attempts JOIN mi_attempt_statuses ON mi_attempts.status_id = mi_attempt_statuses.id
                                JOIN mi_attempt_status_stamps ON mi_attempt_status_stamps.status_id = mi_attempt_statuses.id AND mi_attempt_status_stamps.mi_attempt_id = mi_attempts.id
                   ) ON mi_attempts.mi_plan_id = mi_plans.id
@@ -98,8 +111,8 @@ class PlannedMicroinjectionList
       mi_attempt_counts.plan_aborted_count AS plan_aborted_count,
       to_char(mi_attempt_counts.plan_aborted_max_date, 'dd/mm/yyyy') AS plan_aborted_max_date
     FROM mi_attempt_counts
-      JOIN mi_plans ON mi_plans.id = mi_attempt_counts.plan_id CRISPR_SUBS
-      JOIN new_intermediate_report ON new_intermediate_report.mi_plan_id = mi_attempt_counts.plan_id
+      JOIN mi_plans ON mi_plans.id = mi_attempt_counts.plan_id #{crisprs ? 'and mi_plans.mutagenesis_via_crispr_cas9 is true' : ''}
+      JOIN new_intermediate_report ON new_intermediate_report.mi_plan_id = mi_attempt_counts.plan_id #{crisprs ? 'and new_intermediate_report.mutagenesis_via_crispr_cas9 is true' : ''}
       JOIN mi_plan_statuses ON mi_plan_statuses.name = new_intermediate_report.mi_plan_status
       JOIN mi_plan_status_stamps ON mi_plan_status_stamps.mi_plan_id = new_intermediate_report.mi_plan_id AND mi_plan_status_stamps.status_id = mi_plan_statuses.id
       LEFT JOIN status_conflict_options ON new_intermediate_report.mi_plan_id != status_conflict_options.mi_plan_id AND mi_plan_statuses.name = status_conflict_options.possible_conflict AND new_intermediate_report.gene = status_conflict_options.marker_symbol
@@ -135,11 +148,17 @@ class PlannedMicroinjectionList
 
     EOF
 
-    if crisprs
-      sql.gsub!(/CRISPR_SUBS/, 'and mi_plans.mutagenesis_via_crispr_cas9 is true')
-    else
-      sql.gsub!(/CRISPR_SUBS/, '')
-    end
+    #if crisprs
+    #  #sql.gsub!(/CRISPR_SUBS1/, 'and mi_plans.mutagenesis_via_crispr_cas9 is true and mi_attempt_counts.mutagenesis_via_crispr_cas9 is true')
+    #  #sql.gsub!(/CRISPR_SUBS0/, 'and mi_plans.mutagenesis_via_crispr_cas9 is true')
+    #  sql.gsub!(/CRISPR_SUBS0/, '')
+    #  sql.gsub!(/CRISPR_SUBS1/, 'and mi_plans.mutagenesis_via_crispr_cas9 is true')
+    #  sql.gsub!(/CRISPR_SUBS2/, 'and new_intermediate_report.mutagenesis_via_crispr_cas9 is true')
+    #else
+    #  sql.gsub!(/CRISPR_SUBS0/, '')
+    #  sql.gsub!(/CRISPR_SUBS1/, '')
+    #  sql.gsub!(/CRISPR_SUBS2/, '')
+    #end
 
     sql
 

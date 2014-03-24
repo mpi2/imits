9b6322c2ef600ab5a633d5d0bda063dddbef1a32
Richard Easty <re4@sanger.ac.uk>
2014-03-24 14:37:13 +0000

    Rename _mgi_accession_id.

--- db/migrate/20140320152942_add_allele_name_and_mgi_accession_id_to_phenotype_attempt.rb
+++ db/migrate/20140320152942_add_allele_name_and_mgi_accession_id_to_phenotype_attempt.rb
@@ -1,6 +1,6 @@
 class AddAlleleNameAndMgiAccessionIdToPhenotypeAttempt < ActiveRecord::Migration
   def change
     add_column :phenotype_attempts, :allele_name, :string
-    add_column :phenotype_attempts, :mgi_accession_id, :string
+    add_column :phenotype_attempts, :jax_mgi_accession_id, :string
   end
 end

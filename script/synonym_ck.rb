39d0c5a749bc3db469d80076f31b830fc2898b9b
Richard Easty <re4@sanger.ac.uk>
2014-04-06 13:04:45 +0100

    Tidy.

--- script/synonym_ck.rb
+++ script/synonym_ck.rb
@@ -3,6 +3,8 @@
 require 'pp'
 require 'open-uri'
 
+# see http://www.geneontology.org/GO.format.gaf-2_0.shtml#fields
+
 #Column 	Content 	              Required?   Cardinality 	Example
 #1 	        DB 	                      required 	  1 	        UniProtKB
 #2 	        DB Object ID 	              required 	  1 	        P12345
@@ -23,7 +25,9 @@ require 'open-uri'
 #17 	        Gene Product Form ID 	      optional 	  0 or 1 	UniProtKB:P12345-2
 
 ALL_COLUMNS = false
-USE_FTP = true
+USE_FTP = false
+FTP_LOCATION = 'ftp://ftp.informatics.jax.org/pub/reports/gene_association.mgi'
+LOCAL_FILE_LOCATION = 'gene_association.mgi.original'
 
 @genes_data = {}
 @genes_data_array = []
@@ -35,46 +39,13 @@ USE_FTP = true
 
 @columns_hash = Hash[@columns.map.with_index.to_a]
 
-#def simple
-#	puts 'Here comes the code block!'
-#	yield
-#	puts 'There was the code block!'
-#end
-#simple { puts 'Hooray! The code block is here!' } »
-#Here comes the code block!
-#Hooray! The code block is here!
-#There was the code block!
+def do_ftp; open(FTP_LOCATION, :proxy => nil) { |file| file.each_line { |line| process_line line } }; end
 
-#def test(&block)
-#   block.call
-#end
-
-def do_ftp    #(&block)
-  url = 'ftp://ftp.informatics.jax.org/pub/reports/gene_association.mgi'
-  open(url, :proxy => nil) do |file|
-    file.each_line do |line|
-      #yield line
-      #block.call(line)
-      process_line line
-    end
-  end
-end
-
-def do_local    #(&block)
-  File.open("gene_association.mgi.original", "r") do |f|
-    f.each_line do |line|
-      #yield line
-      #block.call(line)
-      process_line line
-    end
-  end
-end
+def do_local; File.open(LOCAL_FILE_LOCATION, "r") { |f| f.each_line { |line| process_line line } }; end
 
 def process_line line
   return if line[0] == '!'
-
   row = line.strip.split("\t")
-
   product = {}
 
   if ALL_COLUMNS
@@ -98,52 +69,19 @@ end
 
 def get_db
   rows = ActiveRecord::Base.connection.execute("select id, marker_symbol, mgi_accession_id from genes")
-  rows.each { |row| @genes_data_array.push(@genes_data[row['mgi_accession_id']].merge({ 'Marker symbol' => row['marker_symbol']})) if @genes_data.has_key?(row['mgi_accession_id']) }
+  rows.each do |row|
+    if @genes_data.has_key?(row['mgi_accession_id'])
+      @genes_data[row['mgi_accession_id']]['Marker symbol'] = row['marker_symbol']
+      @genes_data_array.push(@genes_data[row['mgi_accession_id']].merge({ 'Marker symbol' => row['marker_symbol']}))
+    end
+  end
 end
+
+puts "#### using #{USE_FTP ? 'ftp' : 'local file'}"
 
 do_ftp if USE_FTP
 do_local if ! USE_FTP
 get_db
 save_csv "#{Dir.home}/Desktop/synonym_ck.csv", @genes_data_array
 
-#url = 'ftp://ftp.informatics.jax.org/pub/reports/gene_association.mgi'
-#open(url, :proxy => nil) do |file|
-#  headers = file.readline.strip.split("\t")
-
-#File.open("gene_association.mgi.original", "r") do |f|
-#  f.each_line do |line|
-#
-#    next if line[0] == '!'
-#
-#    row = line.strip.split("\t")
-#
-#    product = {}
-#    #@columns.each do |column|
-#    #  product[column] = row[@columns_hash[column]]
-#    #end
-#
-#    product['DB Object ID'] = row[@columns_hash['DB Object ID']]
-#    product['DB Object Synonym'] = row[@columns_hash['DB Object Synonym']]
-#
-#    @genes_data[row[@columns_hash['DB Object ID']]] = product if ! row[@columns_hash['DB Object Synonym']].to_s.empty?
-#
-#    # @genes_data_array.push product if ! row[@columns_hash['DB Object Synonym']].to_s.empty?
-#  end
-#end
-#
-#rows = ActiveRecord::Base.connection.execute("select id, marker_symbol, mgi_accession_id from genes")
-#rows.each do |row|
-#  if @genes_data.has_key?(row['mgi_accession_id'])
-#    @genes_data_array.push(@genes_data[row['mgi_accession_id']].merge({ 'marker_symbol' => row['marker_symbol']}))
-#  end
-#end
-#
-#home = Dir.home
-#filename = "#{home}/Desktop/synonym_ck.csv"
-#
-#CSV.open(filename, "wb") do |csv|
-#  csv << @genes_data_array.first.keys
-#  @genes_data_array.each do |hash|
-#    csv << hash.values
-#  end
-#end
+puts "#### done!"

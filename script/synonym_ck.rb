75844d7261ff2120d498785c0ced55fdb2daf305
Richard Easty <re4@sanger.ac.uk>
2014-04-06 11:42:22 +0100

    Add routines.

--- script/synonym_ck.rb
+++ script/synonym_ck.rb
@@ -22,69 +22,128 @@ require 'open-uri'
 #16 	        Annotation Extension 	      optional 	  0 or greater 	part_of(CL:0000576)
 #17 	        Gene Product Form ID 	      optional 	  0 or 1 	UniProtKB:P12345-2
 
-genes_data = {}
-genes_data_array = []
+ALL_COLUMNS = false
+USE_FTP = true
 
-columns = [
-  'DB',
-  'DB Object ID',
-  'DB Object Symbol',
-  'Qualifier',
-  'GO ID',
-  'DB Reference',
-  'Evidence Code',
-  'With From',
-  'Aspect',
-  'DB Object Name',
-  'DB Object Synonym',
-  'DB Object Type',
-  'Taxon',
-  'Date',
-  'Assigned By',
-  'Annotation Extension',
-  'Gene Product Form ID'
+@genes_data = {}
+@genes_data_array = []
+
+@columns = [
+  'DB', 'DB Object ID', 'DB Object Symbol', 'Qualifier', 'GO ID', 'DB Reference', 'Evidence Code', 'With From', 'Aspect',
+  'DB Object Name', 'DB Object Synonym', 'DB Object Type', 'Taxon', 'Date', 'Assigned By', 'Annotation Extension', 'Gene Product Form ID'
 ]
 
-columns_hash = Hash[columns.map.with_index.to_a]
+@columns_hash = Hash[@columns.map.with_index.to_a]
+
+#def simple
+#	puts 'Here comes the code block!'
+#	yield
+#	puts 'There was the code block!'
+#end
+#simple { puts 'Hooray! The code block is here!' } »
+#Here comes the code block!
+#Hooray! The code block is here!
+#There was the code block!
+
+#def test(&block)
+#   block.call
+#end
+
+def do_ftp    #(&block)
+  url = 'ftp://ftp.informatics.jax.org/pub/reports/gene_association.mgi'
+  open(url, :proxy => nil) do |file|
+    file.each_line do |line|
+      #yield line
+      #block.call(line)
+      process_line line
+    end
+  end
+end
+
+def do_local    #(&block)
+  File.open("gene_association.mgi.original", "r") do |f|
+    f.each_line do |line|
+      #yield line
+      #block.call(line)
+      process_line line
+    end
+  end
+end
+
+def process_line line
+  return if line[0] == '!'
+
+  row = line.strip.split("\t")
+
+  product = {}
+
+  if ALL_COLUMNS
+    @columns.each { |column| product[column] = row[@columns_hash[column]] }
+  else
+    product['DB Object ID'] = row[@columns_hash['DB Object ID']]
+    product['DB Object Synonym'] = row[@columns_hash['DB Object Synonym']]
+  end
+
+  @genes_data[row[@columns_hash['DB Object ID']]] = product if ! row[@columns_hash['DB Object Synonym']].to_s.empty?
+end
+
+def save_csv filename, data
+  CSV.open(filename, "wb") do |csv|
+    csv << data.first.keys
+    data.each do |hash|
+      csv << hash.values
+    end
+  end
+end
+
+def get_db
+  rows = ActiveRecord::Base.connection.execute("select id, marker_symbol, mgi_accession_id from genes")
+  rows.each { |row| @genes_data_array.push(@genes_data[row['mgi_accession_id']].merge({ 'Marker symbol' => row['marker_symbol']})) if @genes_data.has_key?(row['mgi_accession_id']) }
+end
+
+do_ftp if USE_FTP
+do_local if ! USE_FTP
+get_db
+save_csv "#{Dir.home}/Desktop/synonym_ck.csv", @genes_data_array
 
 #url = 'ftp://ftp.informatics.jax.org/pub/reports/gene_association.mgi'
 #open(url, :proxy => nil) do |file|
 #  headers = file.readline.strip.split("\t")
 
-File.open("gene_association.mgi.original", "r") do |f|
-  f.each_line do |line|
-
-    next if line[0] == '!'
-
-    row = line.strip.split("\t")
-
-    product = {}
-    #columns.each do |column|
-    #  product[column] = row[columns_hash[column]]
-    #end
-
-    product['DB Object ID'] = row[columns_hash['DB Object ID']]
-    product['DB Object Synonym'] = row[columns_hash['DB Object Synonym']]
-
-    genes_data[row[columns_hash['DB Object ID']]] = product if ! row[columns_hash['DB Object Synonym']].to_s.empty?
-
-   # genes_data_array.push product if ! row[columns_hash['DB Object Synonym']].to_s.empty?
-  end
-end
-
-rows = ActiveRecord::Base.connection.execute("select id, marker_symbol, mgi_accession_id from genes")
-rows.each do |row|
-  if genes_data.has_key?(row['mgi_accession_id'])
-    genes_data_array.push(genes_data[row['mgi_accession_id']].merge({ 'marker_symbol' => row['marker_symbol']}))
-  end
-end
-
-home = Dir.home
-filename = "#{home}/Desktop/synonym_ck.csv"
-
-CSV.open(filename, "wb") do |csv|
-  csv << genes_data_array.first.keys
-  genes_data_array.each do |hash|
-    csv << hash.values
-  end
-end
+#File.open("gene_association.mgi.original", "r") do |f|
+#  f.each_line do |line|
+#
+#    next if line[0] == '!'
+#
+#    row = line.strip.split("\t")
+#
+#    product = {}
+#    #@columns.each do |column|
+#    #  product[column] = row[@columns_hash[column]]
+#    #end
+#
+#    product['DB Object ID'] = row[@columns_hash['DB Object ID']]
+#    product['DB Object Synonym'] = row[@columns_hash['DB Object Synonym']]
+#
+#    @genes_data[row[@columns_hash['DB Object ID']]] = product if ! row[@columns_hash['DB Object Synonym']].to_s.empty?
+#
+#    # @genes_data_array.push product if ! row[@columns_hash['DB Object Synonym']].to_s.empty?
+#  end
+#end
+#
+#rows = ActiveRecord::Base.connection.execute("select id, marker_symbol, mgi_accession_id from genes")
+#rows.each do |row|
+#  if @genes_data.has_key?(row['mgi_accession_id'])
+#    @genes_data_array.push(@genes_data[row['mgi_accession_id']].merge({ 'marker_symbol' => row['marker_symbol']}))
+#  end
+#end
+#
+#home = Dir.home
+#filename = "#{home}/Desktop/synonym_ck.csv"
+#
+#CSV.open(filename, "wb") do |csv|
+#  csv << @genes_data_array.first.keys
+#  @genes_data_array.each do |hash|
+#    csv << hash.values
+#  end
+#end

b37c10f32d59d44ab9f1e677bf7212b92bb9867b
Richard Easty <re4@sanger.ac.uk>
2014-04-23 13:10:21 +0100

    Fix test errors.

--- test/unit/solr_update/observer_test.rb
+++ test/unit/solr_update/observer_test.rb
@@ -22,23 +22,23 @@ class SolrUpdate::ObserverTest < ActiveSupport::TestCase
 
   context 'SolrUpdate::Observer::PhenotypeAttempt' do
     should 'tell the enqueuer that a PhenotypeAttempt is changed' do
-      return if ! Rails.configuration.enable_solr_phenotype_attempt
+      if Rails.configuration.enable_solr_phenotype_attempt
+        pa = stub('phenotype_attempt')
+        SolrUpdate::Enqueuer.any_instance.expects(:phenotype_attempt_updated).with(pa)
 
-      pa = stub('phenotype_attempt')
-      SolrUpdate::Enqueuer.any_instance.expects(:phenotype_attempt_updated).with(pa)
-
-      o = SolrUpdate::Observer::PhenotypeAttempt.new
-      o.after_save(pa)
+        o = SolrUpdate::Observer::PhenotypeAttempt.new
+        o.after_save(pa)
+      end
     end
 
     should 'tell the enqueuer that a PhenotypeAttempt is deleted' do
-      return if ! Rails.configuration.enable_solr_phenotype_attempt
+      if Rails.configuration.enable_solr_phenotype_attempt
+        pa = stub('phenotype_attempt')
+        SolrUpdate::Enqueuer.any_instance.expects(:phenotype_attempt_destroyed).with(pa)
 
-      pa = stub('phenotype_attempt')
-      SolrUpdate::Enqueuer.any_instance.expects(:phenotype_attempt_destroyed).with(pa)
-
-      o = SolrUpdate::Observer::PhenotypeAttempt.new
-      o.after_destroy(pa)
+        o = SolrUpdate::Observer::PhenotypeAttempt.new
+        o.after_destroy(pa)
+      end
     end
   end
 

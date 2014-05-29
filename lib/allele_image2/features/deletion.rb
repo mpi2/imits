aa76cd7d606be3ed06092922af26012aee20262a
as28 <as28@sanger.ac.uk>
2014-05-29 17:05:47 +0100

    Fix for LoxP missing in image after deletion

--- lib/allele_image2/features/deletion.rb
+++ lib/allele_image2/features/deletion.rb
@@ -60,6 +60,24 @@ class AlleleImage2::Features::Deletion < AlleleImage2::Features::DefaultFeature
     
     drawing.line( dslash_width, dslash_top_y, renderer.x + feature.render_options[:text_width], dslash_btm_y )
     drawing.draw( image )
+
+    if feature.del_bp && feature.del_exons
+
+      font_colour = feature.render_options[:font_colour]
+
+      del_txt_start_x = renderer.x - 15
+      del_txt_start_y = ( feature.render_options[:height] + feature.render_options[:bottom_margin] ) - 8
+
+      del_txt         = feature.del_bp + "\n" + feature.del_exons
+      
+      drawing.annotate( image, feature.render_options[:width], feature.render_options[:text_height] * 2, del_txt_start_x, del_txt_start_y, del_txt ) do
+        self.fill        = font_colour
+        self.gravity     = Magick::CenterGravity
+        self.font_weight = Magick::BoldWeight
+        self.font_style  = Magick::ItalicStyle
+        self.pointsize   = 13
+      end
+    end
   end
 
 end

381d2121f799df97e12671cedf0372a08a271567
Peter Matthews <pm9@sanger.ac.uk>
2014-07-23 15:15:14 +0100

    generalise table row insertion and deletion

--- public/javascripts/mi_attempts/show.js
+++ public/javascripts/mi_attempts/show.js
@@ -1,15 +1,18 @@
 function addHideRowLinks() {
-    var parentEl = Ext.fly('distribution_centres_table');
-    if (parentEl) {
-        parentEl.on('click', function(event, target, options) {
-            event.preventDefault();
-            var inputField = Ext.get(target).prev('.destroy-field');
-            inputField.set({value: true});
-            row = Ext.get(target).parent().parent();
-            row.setVisibilityMode(Ext.Element.DISPLAY);
-            row.hide();
-        }, this, {delegate: 'a'});
-    }
+    var addDeleteRowArray = ['distribution_centres_table', 'colonies_table']
+    addDeleteRowArray.forEach(function(table_name) {
+      var parentEl = Ext.fly(table_name);
+      if (parentEl) {
+          parentEl.on('click', function(event, target, options) {
+              event.preventDefault();
+              var inputField = Ext.get(target).prev('.destroy-field');
+              inputField.set({value: true});
+              row = Ext.get(target).parent().parent();
+              row.setVisibilityMode(Ext.Element.DISPLAY);
+              row.hide();
+          }, this, {delegate: 'a'});
+      }
+    })
 }
 
 Ext.onReady(addHideRowLinks);
@@ -19,18 +22,19 @@ Ext.select('form .add-row').on("click", function(event){
 
   var data = Ext.get(this).getAttribute('data-fields');
   var id = Ext.get(this).getAttribute('data-object-id');
+  var table_id = Ext.get(this).getAttribute('data-table-id');
   var time = new Date().getTime();
   regexp = new RegExp(id, 'g');
 
   data = data.replace(regexp, time);
 
-  Ext.select("#distribution_centres_table:not(td > table) > tbody > tr:last").insertSibling(data, 'after');
+  Ext.select("#" + table_id + ":not(td > table) > tbody > tr:last").insertSibling(data, 'after');
 
-  Ext.select("#distribution_centres_table tr:last a.remove-row").on("click", function(e){
+  Ext.select("#" + table_id + " tr:last a.remove-row").on("click", function(e){
         e.preventDefault();
         Ext.get(this).up('tr').remove();
   });
-  Ext.select('#distribution_centres_table tr:last .date-field').each(function(field) {
+  Ext.select("#" + table_id + " tr:last .date-field").each(function(field) {
         var name = field.dom.name;
         var defaultValue = field.dom.value;
         var renderDiv = new Ext.Element(Ext.core.DomHelper.createDom({tag: 'div'}));

d1bba388888d845f8a319bb32432fe9ff2c2b36d
Richard Easty <re4@sanger.ac.uk>
2014-08-04 14:43:50 +0100

    Add Colony::Observer.

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,8 @@ module TarMits
       'SolrUpdate::Observer::EsCell',
       'SolrUpdate::Observer::MiPlan',
       'SolrUpdate::Observer::Gene',
-      'SolrUpdate::Observer::IkmcProject'
+      'SolrUpdate::Observer::IkmcProject',
+      #'Colony::Observer'
     ]
 
     config.active_record.observers += ['SolrUpdate::Observer::PhenotypeAttempt'] if config.enable_solr_phenotype_attempt

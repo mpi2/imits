a27b417e9a53c7dba2b76fa20da88c9b11e696c1
Richard Easty <re4@sanger.ac.uk>
2014-11-11 10:32:47 +0000

    remove solr_bulk

--- script/solr_bulk/drop_main.sql
+++
@@ -1,7 +0,0 @@
--- http://www.postgresql.org/message-id/14658.1175879477@sss.pgh.pa.us
-SET client_min_messages=WARNING;
-
-DROP TABLE IF EXISTS solr_phenotype_attempts CASCADE;
-DROP TABLE IF EXISTS solr_mi_attempts CASCADE;
-DROP TABLE IF EXISTS solr_alleles CASCADE;
-DROP TABLE IF EXISTS solr_genes CASCADE;

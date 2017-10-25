c67103d58683176179e19ab57d4e1bbaf18aea89
Peter Matthews <pm9@ebi.ac.uk>
2017-10-25 10:36:06 +0100

    remove genes not in the idg and cmg files

--- app/models/gene/private_annotation.rb
+++ app/models/gene/private_annotation.rb
@@ -3,25 +3,71 @@ class Gene::PrivateAnnotation < ActiveRecord::Base
   belongs_to :gene
 
   def self.load_annoations
-    idg_config = YAML.load_file("#{Rails.root}/config/idg_symbols.yml")
-    cmg_tier1_config = YAML.load_file("#{Rails.root}/config/cmg_tier1_symbols.yml")
-    cmg_tier2_config = YAML.load_file("#{Rails.root}/config/cmg_tier2_symbols.yml")
+    idg_config = "#{Rails.root}/config/idg_symbols.yml"
+    cmg_tier1_config = "#{Rails.root}/config/cmg_tier1_symbols.yml"
+    cmg_tier2_config = "#{Rails.root}/config/cmg_tier2_symbols.yml"
 
     genes = {}
 
-    idg_config.each do |gene|
-      genes[gene] = {} unless genes.has_key?(gene)
-      genes[gene][:idg] = true
+    if File.file?(idg_config)
+      idg_gene_list = YAML.load_file("#{Rails.root}/config/idg_symbols.yml")
+      sql = 'SELECT genes.marker_symbol FROM genes JOIN gene_private_annotations gpa ON gpa.gene_id = genes.id WHERE gpa.idg = true'
+      remove_idg_genes = ActiveRecord::Base.connection.execute(sql).map{|g| g['marker_symbol']}
+
+      if !idg_gene_list.blank?
+        idg_gene_list.each do |gene|
+          genes[gene] = {} unless genes.has_key?(gene)
+          genes[gene][:idg] = true
+          remove_idg_genes.delete(gene)
+        end
+      end
+
+      remove_idg_genes.each do |gene|
+        genes[gene] = {} unless genes.has_key?(gene)
+        genes[gene][:idg] = false
+      end
+    else
+        puts "IDG File not found"
+    end
+  
+    if File.file?(cmg_tier1_config)
+      cmg_tier1_gene_list = YAML.load_file("#{Rails.root}/config/cmg_tier1_symbols.yml")
+      sql = 'SELECT genes.marker_symbol FROM genes JOIN gene_private_annotations gpa ON gpa.gene_id = genes.id WHERE gpa.cmg_tier1 = true'
+      remove_cmg_tier1_genes = ActiveRecord::Base.connection.execute(sql).map{|g| g['marker_symbol']}
+
+      if !cmg_tier1_gene_list.blank?
+        cmg_tier1_gene_list.each do |gene|
+          genes[gene] = {} unless genes.has_key?(gene)
+          genes[gene][:cmg_tier1] = true
+        end
+      end
+
+      remove_cmg_tier1_genes.each do |gene|
+        genes[gene] = {} unless genes.has_key?(gene)
+        genes[gene][:cmg_tier1] = false
+      end
+    else
+        puts "CMG tier1 File not found"
     end
 
-    cmg_tier1_config.each do |gene|
-      genes[gene] = {} unless genes.has_key?(gene)
-      genes[gene][:cmg_tier1] = true
-    end
+    if File.file?(cmg_tier2_config)
+      cmg_tier2_gene_list = YAML.load_file("#{Rails.root}/config/cmg_tier2_symbols.yml")
+      sql = 'SELECT genes.marker_symbol FROM genes JOIN gene_private_annotations gpa ON gpa.gene_id = genes.id WHERE gpa.cmg_tier2 = true'
+      remove_cmg_tier2_genes = ActiveRecord::Base.connection.execute(sql).map{|g| g['marker_symbol']}
 
-    cmg_tier2_config.each do |gene|
-      genes[gene] = {} unless genes.has_key?(gene)
-      genes[gene][:cmg_tier2] = true
+      if !cmg_tier2_gene_list.blank?
+        cmg_tier2_gene_list.each do |gene|
+          genes[gene] = {} unless genes.has_key?(gene)
+          genes[gene][:cmg_tier2] = true
+        end
+      end
+
+      remove_cmg_tier2_genes.each do |gene|
+        genes[gene] = {} unless genes.has_key?(gene)
+        genes[gene][:cmg_tier2] = false
+      end
+    else
+        puts "CMG tier2 File not found"
     end
 
     genes.each do |gene, params|

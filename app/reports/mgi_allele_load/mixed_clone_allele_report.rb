f54e6650fee3de0820efa412732f80da5123d249
Peter Matthews <pm9@ebi.ac.uk>
2018-03-08 13:17:46 +0000

    fix MGI reports

--- app/reports/mgi_allele_load/mixed_clone_allele_report.rb
+++ app/reports/mgi_allele_load/mixed_clone_allele_report.rb
@@ -30,25 +30,27 @@ class MgiAlleleLoad::MixedCloneAlleleReport
       SELECT
         genes.marker_symbol AS marker_symbol,
         genes.mgi_accession_id AS mgi_accession_id,
-        colonies.name AS colony_name, colonies.allele_type AS mutation_type, colonies.mgi_allele_id AS mgi_allele_accession, colonies.mgi_allele_symbol_superscript AS mgi_allele_name,
+        colonies.name AS colony_name, 
+        colony_allele.allele_type AS mutation_type,
+        colony_allele.mgi_allele_accession_id AS mgi_allele_accession,
+        colony_allele.mgi_allele_symbol_superscript AS mgi_allele_name,
         background_strains.name AS colony_background_strain,
         centres.name AS production_centre,
-        colonies.allele_type AS mutation_type,
         targ_rep_es_cells.name AS mi_attempt_es_cell_name,
-        targ_rep_es_cells.mgi_allele_symbol_superscript AS mi_attempt_es_cell_allele_symbol,
-        targ_rep_es_cells.mgi_allele_id AS mi_attempt_es_cell_mgi_allele_accession,
-        targ_rep_es_cells.parental_cell_line AS mi_attempt_es_cell_line,
-        colonies.mgi_allele_id AS mgi_allele_accession,
-        colonies.mgi_allele_symbol_superscript AS mgi_allele_name
+        es_cell_allele.mgi_allele_symbol_superscript AS mi_attempt_es_cell_allele_symbol,
+        es_cell_allele.mgi_allele_accession_id AS mi_attempt_es_cell_mgi_allele_accession,
+        targ_rep_es_cells.parental_cell_line AS mi_attempt_es_cell_line
 
       FROM mi_attempts
-        JOIN colonies ON colonies.mi_attempt_id = mi_attempts.id AND colonies.allele_type IS NOT NULL
+        JOIN colonies ON colonies.mi_attempt_id = mi_attempts.id
+        JOIN alleles colony_allele ON colony_allele.colony_id = colonies.id AND colony_allele.allele_type IS NOT NULL
         LEFT JOIN strains background_strains ON background_strains.id = colonies.background_strain_id
         JOIN targ_rep_es_cells ON targ_rep_es_cells.id = mi_attempts.es_cell_id
+        JOIN alleles es_cell_allele ON es_cell_allele.es_cell_id = targ_rep_es_cells.id
         JOIN mi_plans ON mi_plans.id = mi_attempts.mi_plan_id
         JOIN genes ON genes.id = mi_plans.gene_id
         JOIN centres ON centres.id = mi_plans.production_centre_id
-      WHERE colonies.allele_type != targ_rep_es_cells.allele_type AND colonies.genotype_confirmed = true
+      WHERE colony_allele.allele_type != es_cell_allele.allele_type AND colonies.genotype_confirmed = true
       ORDER BY mgi_accession_id
       EOF
     end

fb2cb255451a8c89a50874d2cea2e5b9091171cd
albags <gomezsegura.alba@gmail.com>
2018-10-30 15:09:19 +0000

    Allele description corrections

--- app/models/allele.rb
+++ app/models/allele.rb
@@ -271,7 +271,6 @@ class Allele < ApplicationModel
         key = {}
         bcsq_transcripts.each do |tran|
           tran_fields = tran.split("|")
-          tran_fields = tran.split(",")
 #         first 4 fields always exist
           next if tran_fields[ index_bcsq['biotype'] ] == 'NMD'
 
@@ -551,9 +550,9 @@ class Allele < ApplicationModel
 
     nuclease = 'Cas9'
     if !mi_attempt.protein_nuclease.blank?
-      nuclease = "#{mi_attempt.protein_nuclease} Protein"
+      nuclease = "#{mi_attempt.protein_nuclease.capitalize} protein"
     elsif !mi_attempt.mrna_nuclease.blank?
-      nuclease = "#{mi_attempt.mrna_nuclease} RNA"
+      nuclease = "#{mi_attempt.mrna_nuclease.capitalize} RNA"
     end
 
     if crisprs.length > 1
@@ -608,21 +607,16 @@ class Allele < ApplicationModel
 
     (del['exonic'] + ins['exonic'] + inv['exonic']).each do |a|
       start = !a.linked_concequence.blank? ? a.linked_concequence : a.start
-      mutations[start] = [{'coding_sequence' => false, 'splice_acceptor' => false, 'splice_donor' => false, 'frameshift' => false},[]] if !mutations.has_key?(a.start)
-      mutations[start][0]['coding_sequence'] = true if a.protein_coding_region == true
-      mutations[start][0]['splice_acceptor'] = true if a.splice_acceptor == true
-      mutations[start][0]['splice_donor'] = true if a.splice_donor == true
-      mutations[start][0]['frameshift'] = true if a.frameshift == true
-      
-      puts 
-      puts "mutations => #{mutations}"
-      puts 
-
-      # mutations[a.start][1] << {'type' => mapping[a.mod_type], 'size' => (a.end - a.start).abs, 'sequence' => a.ref_seq.length < a.alt_seq.length ? a.alt_seq[( a.ref_seq.length)..-1] : a.ref_seq[( a.alt_seq.length)..-1], 'start' => a.start, 'end' => a.end, 'chr' => a.chr}
+      mutations[start.to_i] = [{'coding_sequence' => false, 'splice_acceptor' => false, 'splice_donor' => false, 'frameshift' => false},[]] if !mutations.has_key?(a.start)
+      mutations[start.to_i][0]['coding_sequence'] = true if a.protein_coding_region == true
+      mutations[start.to_i][0]['splice_acceptor'] = true if a.splice_acceptor == true
+      mutations[start.to_i][0]['splice_donor'] = true if a.splice_donor == true
+      mutations[start.to_i][0]['frameshift'] = true if a.frameshift == true
+    
+      mutations[start.to_i][1] << {'type' => mapping[a.mod_type], 'size' => (a.end - a.start + 1).abs, 'sequence' => a.ref_seq.length < a.alt_seq.length ? a.alt_seq[( a.ref_seq.length)..-1] : a.ref_seq[( a.alt_seq.length)..-1], 'start' => a.start, 'end' => a.end, 'chr' => a.chr}
     end
 
     puts "Keys => #{mutations.keys}"
-    puts
     
     if !mutations.keys.blank?
       region_start = mutations.keys.min 
@@ -640,6 +634,7 @@ class Allele < ApplicationModel
     
     compiling_mut_arr = []
     mutations.each do |key, value|
+      mut_type = []
       mut_arr = []
       consequence = []
       consequence << "cause a frameshift" if value[0]['frameshift'] == true
@@ -648,32 +643,33 @@ class Allele < ApplicationModel
       consequence << "remove the splice donor" if value[0]['splice_donor'] == true && value[1].any?{|m| m['type'] == 'deletion'}
       mutation_description = value[1]
       mutation_description.each do |md|
-        mut_arr << "#{md['size']} bp deletion beginning at Chromosome #{md['chr']} position #{md['start']} (GRCm38/mm10)" if md['size'] > 10
+        mut_arr << "in a #{md['size']}-bp #{md['type']} beginning at Chromosome #{md['chr']} position #{md['start']} (GRCm38/mm10)" # if md['size'] > 10
       end
+      
       mut_str = mut_arr.to_sentence
-      mut_str << " that is predicted to #{consequence.to_sentence}" unless consequence.blank?
-      compiling_mut_arr << mut_str
+      mut_str << ", which is predicted to #{consequence.to_sentence}" unless consequence.blank?
+      compiling_mut_arr << ". This process resulted #{mut_str}"
     end
 
     ind_arr = []
     inside.each do |ind|
-      ind_arr << "#{ind['size']} bp #{ind['type']} at position #{ind['start']}"
+      ind_arr << "#{ind['size']}-bp #{ind['type']} at position #{ind['start']}"
     end
-    ins_str = ind_arr.blank? ? "" : " In addition there is a #{ind_arr.to_sentence} in the intron sequence, which will not affect the exon deletion."
+    ins_str = ind_arr.blank? ? "" : " In addition there is a #{ind_arr.to_sentence} in the intron sequence, which will not affect the exon."
 
     upstream_arr = []
     upstream.each do |ind|
-      upstream_arr << "#{ind['size']} bp #{ind['type']} at position #{ind['start']}"
+      upstream_arr << "#{ind['size']}-bp #{ind['type']} at position #{ind['start']}"
     end
-    upstream_str = upstream_arr.blank? ? "" : " Upstream of the deletion there is a #{upstream_arr.to_sentence}, which will not affect the exon deletion."
+    upstream_str = upstream_arr.blank? ? "" : " Upstream there is a #{upstream_arr.to_sentence}, which will not affect the exon."
 
     downstream_arr = []
     downstream.each do |ind|
-      downstream_arr << "#{ind['size']} bp #{ind['type']} at position #{ind['start']}"
+      downstream_arr << "#{ind['size']}-bp #{ind['type']} at position #{ind['start']}"
     end
-    downstream_str = downstream_arr.blank? ? "" : " Downstream of the deletion there is a #{downstream_arr.to_sentence}, which will not affect the exon deletion."
+    downstream_str = downstream_arr.blank? ? "" : " Downstream there is a #{downstream_arr.to_sentence}, which will not affect the exon."
 
-    allele_description = "This allele from IMPC was generated at #{centre_name} by injecting #{mutagenesis_factor_array.to_sentence}, which resulted in a #{compiling_mut_arr.to_sentence}. #{ins_str}#{upstream_str}#{downstream_str}"
+    allele_description = "This IMPC allele was generated at #{centre_name} by injecting #{mutagenesis_factor_array.to_sentence}#{compiling_mut_arr.to_sentence}. #{ins_str}#{upstream_str}#{downstream_str}"
   end
 end
 

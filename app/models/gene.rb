22f61a0187bdc071907c5fcdae3fa86137619c17
albags <gomezsegura.alba@gmail.com>
2018-11-07 16:27:08 +0000

    Update gene.rb
    MGI ids updated

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -754,7 +754,7 @@ class Gene < ActiveRecord::Base
       gene_data = genes_data.delete(gene.mgi_accession_id)
       if gene_data.blank?
         logger.info "MGI accession does not exist: #{gene.mgi_accession_id}"
-        if gene.allele.count == 0 and gene.mi_plans.count == 0 and ! gene.mgi_accession_id =~ /CGI/
+        if gene.allele.count == 0 and gene.mi_plans.count == 0 and ! (gene.mgi_accession_id =~ /CGI/) and gene.notifications.blank?
           gene.delete
           logger.info "Deleted MGI accession: #{gene.mgi_accession_id}"
         else

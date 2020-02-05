2e3b7dcc2a0ab08964ef8b580d711397566f32bd
albags <gomezsegura.alba@gmail.com>
2020-02-05 11:57:47 +0000

    update is_active and phenotype_started fields in phenotyping_productions_report, t = true and f = false

--- app/reports/mp2_load/phenotyping_productions_report.rb
+++ app/reports/mp2_load/phenotyping_productions_report.rb
@@ -35,16 +35,16 @@ class Mp2Load::PhenotypingProductionsReport
 
     def phenotyping_productions_sql
       <<-EOF
-        SELECT pp.id, pp.colony_name, strain.name AS colony_background_strain_name, parent_strain.name AS parent_colony_background_strain_name, phenotyping_centre.name AS phenotyping_centre_name, cohort_centre.name AS cohort_production_centre_name, a.mgi_allele_symbol_superscript AS mouse_allele_symbol, g.mgi_accession_id, g.marker_symbol, pp.phenotype_attempt_id, p.id AS mi_plan_id, pp.phenotyping_started, pp.is_active
+        SELECT pp.id, pp.colony_name, strain.name AS colony_background_strain_name, parent_strain.name AS parent_colony_background_strain_name, phenotyping_centre.name AS phenotyping_centre_name, cohort_centre.name AS cohort_production_centre_name, a.mgi_allele_symbol_superscript AS mouse_allele_symbol, g.mgi_accession_id, g.marker_symbol, pp.phenotype_attempt_id, p.id AS mi_plan_id, CASE WHEN pp.phenotyping_started = true THEN 'true' ELSE 'false' END AS phenotyping_started, CASE WHEN pp.is_active = true THEN 'true' ELSE 'false' END AS is_active
 
         FROM phenotyping_productions pp INNER JOIN colonies c ON c.id = pp.parent_colony_id 
           LEFT JOIN phenotyping_production_statuses ps ON ps.id = pp.status_id
           LEFT JOIN strains parent_strain ON c.background_strain_id = parent_strain.id
           LEFT JOIN strains strain ON pp.colony_background_strain_id = strain.id
-          LEFT JOIN mi_plans p ON p.id = pp.mi_plan_id
+          JOIN mi_plans p ON p.id = pp.mi_plan_id
           LEFT JOIN centres phenotyping_centre ON phenotyping_centre.id = p.production_centre_id
           LEFT JOIN centres cohort_centre ON cohort_centre.id = pp.cohort_production_centre_id
-          LEFT JOIN genes g ON g.id = p.gene_id
+          JOIN genes g ON g.id = p.gene_id
           LEFT JOIN alleles a ON c.id = a.colony_id
 
         WHERE (ps.name != 'Rederivation Started' AND c.genotype_confirmed = true);

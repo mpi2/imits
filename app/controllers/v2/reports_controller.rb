c41ba8be9746c4ae6af7d843a37a5b3d491e5d63
albags <gomezsegura.alba@gmail.com>
2021-03-19 16:31:25 +0000

    Add function to controller for new phenotyping_colonies_report.

--- app/controllers/v2/reports_controller.rb
+++ app/controllers/v2/reports_controller.rb
@@ -2,7 +2,7 @@ class V2::ReportsController < ApplicationController
 
   helper :reports
 
-  before_filter :authenticate_user!, :except => [:komp_project, :idcc_master_genelist, :mgi_modification_allele_report, :mgi_es_cell_allele_report, :mgi_mixed_allele_report, :mgi_crispr_allele_report, :mp2_load_phenotyping_colonies_report, :mp2_load_phenotyping_productions_report_crawler, :mp2_load_gene_interest_report, :mp2_load_gene_interest_report_es_cell, :mp2_load_gene_contact_report, :mp2_load_gene_contact_sent_report, :emma_distribution_report]
+  before_filter :authenticate_user!, :except => [:komp_project, :idcc_master_genelist, :mgi_modification_allele_report, :mgi_es_cell_allele_report, :mgi_mixed_allele_report, :mgi_crispr_allele_report, :mp2_load_phenotyping_colonies_report, :mp2_load_phenotyping_colonies_report_es_cell, :mp2_load_phenotyping_productions_report_crawler, :mp2_load_gene_interest_report, :mp2_load_gene_interest_report_es_cell, :mp2_load_gene_contact_report, :mp2_load_gene_contact_sent_report, :emma_distribution_report]
 
   before_filter do
     if params[:format] == 'csv'
@@ -86,6 +86,14 @@ class V2::ReportsController < ApplicationController
     @phenotyping_colonies = @report.phenotyping_colonies
     respond_to do |format|
       format.tsv {render :mp2_load_phenotyping_colonies_report}
+    end
+  end
+
+  def mp2_load_phenotyping_colonies_report_es_cell
+    @report = Mp2Load::PhenotypingColoniesReportEsCell.new
+    @phenotyping_colonies = @report.phenotyping_colonies
+    respond_to do |format|
+      format.tsv {render :mp2_load_phenotyping_colonies_report_es_cell}
     end
   end
 
